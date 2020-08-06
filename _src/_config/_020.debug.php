d7e34d7c3b0d679f6a9b7f594c1228a8f79b0e9a
Fabio Mosti <fabio.mosti@istricesrl.it>
2020-08-06 18:19:28 +0200

    aggiunti file _020.debug.php e _025.site.php

---
+++ _src/_config/_020.debug.php
@@ -0,0 +1,43 @@
+<?php
+
+    /**
+     * applicazione dei livelli di debug
+     *
+     * In questo file vengono applicati i livelli di debug impostati in
+     * _src/_config/_000.debug.php e src/config/000.debug.php.
+     *
+     * applicazione della configurazione
+     * =================================
+     * Proseguendo nella lettura dei commenti si troverà che spesso i file di configurazione del framework sono
+     * organizzati a coppie, il primo file che contiene soprattutto definizioni mentre il secondo che contiene il
+     * codice inteso ad applicare i valori definiti dal primo. Questa strategia consente di personalizzare i valori
+     * creando una copia custom del primo file, senza necessità di riscrivere le logiche contenute nel secondo.
+     *
+     * errori visualizzati da Apache
+     * -----------------------------
+     * Il livello degli errori visualizzati nell'output viene settato utilizzando la funzione ini_set()
+     * (http://it1.php.net/manual/it/function.ini-set.php) sulla variabile diaplay_errors alla quale
+     * viene assegnato il valore di $cf['debug']['lvl']['report'].
+     *
+     * la costante LOG_CURRENT_LEVEL
+     * -----------------------------
+     * @todo documentare questo paragrafo e rimandare al capitolo sulle costanti della documentazione tecnica
+     *
+     *
+     *
+     * @todo finire di documentare
+     *
+     * @file
+     *
+     */
+
+    // imposto la visualizzazione degli errori
+	ini_set( 'display_errors', $cf['debug'][ SITE_STATUS ]['*']['report']['lvl'] );
+
+    // imposto il livello di debug
+	error_reporting( $cf['debug'][ SITE_STATUS ]['*']['report']['lvl'] );
+
+    // costante che descrive il livello corrente di log
+	define( 'LOG_CURRENT_LEVEL'		, $cf['debug'][ SITE_STATUS ]['*']['log']['lvl'] );
+
+?>
---
+++ _src/_config/_025.site.php
@@ -0,0 +1,98 @@
+<?php
+
+    /**
+     * applicazione della configurazione base del sito
+     *
+     * Questo file applica le configurazioni fatte finora e crea alcune chiavi "brevi" per le variabili di uso comune.
+     *
+     * configurazione del sito
+     * =======================
+     * Lo scopo di questo file è ricavare dalla configurazione del sito i dati e le informazioni che vengono utilizzati
+     * più spesso in modo da facilitarne il reperimento nell'output space. Per un esame dettagliato delle chiavi e dei
+     * sotto array di $cf['site'] si veda il \ref variabili "capitolo dedicato alle variabili della documentazione
+     * tecnica".
+     *
+     * scorciatoie dell'array $cf['site']
+     * ----------------------------------
+     * Riportiamo qui di seguito le scorciatoie create nell'array $cf['site']; si tratta solo di chiavi pensate per
+     * l'accesso rapido alle informazioni e non contengono dati originali:
+     *
+     * variabile               | scorciatoia per...
+     * ------------------------|----------------------------------------------------------------
+     * $cf['site']['domain']   | $cf['site']['domains'][ $cf['site']['status'] ]
+     * $cf['site']['fqdn']     | $cf['site']['host'].'.'.$cf['site']['domain']
+     * $cf['site']['home']     | $cf['site']['homes'][ $cf['site']['status'] ]
+     * $cf['site']['host']     | $cf['site']['hosts'][ $cf['site']['status'] ]
+     * $cf['site']['ietf']     | $cf['localization']['language']['ietf']
+     * $cf['site']['root']     | '/' . $cf['site']['folders'][ $cf['site']['status'] ]
+     * $cf['site']['url']      | $cf['site']['urls'][ $cf['site']['status'] ]
+     *
+     * Si noti che $cf['site']['ietf'] viene dichiarata successivamente in _src/_config/_070.localization.php
+     *
+     *
+     *
+     *
+     *
+     *
+     * @todo creare una scorciatoia anche per $cf['localization']['language']['ietf'] tipo $cf['site']['ietf']
+     * @todo documentare $cf['site']['url'] e $cf['site']['root']
+     *
+     * @file
+     *
+     */
+
+    // multisito di default
+	if( ! defined( 'URL_WORD_SEPARATOR' ) ) {
+	    define( 'URL_WORD_SEPARATOR'		, '-' );
+	}
+
+    // gli URL del sito
+	foreach( array_keys( $site['domains'] ) as $status ) {
+
+	    $cf['site']['urls'][ $status ] =
+		$cf['site']['protocols'][ $status ] . '://' .
+		(
+		    ( ! empty( $cf['site']['hosts'][ $status ] ) )
+		    ? $cf['site']['hosts'][ $status ] . ( ( ! empty( $cf['site']['domains'][ $status ] ) ) ? '.' : NULL )
+		    : NULL 
+		).
+		$cf['site']['domains'][ $status ] . '/' .
+		( ( isset( $cf['site']['folders'][ $cf['site']['status'] ] ) ) ? $cf['site']['folders'][ $cf['site']['status'] ] : NULL );
+
+	}
+
+    // URL corrente del sito
+	$cf['site']['home']			= &$cf['site']['homes'][ $cf['site']['status'] ];
+
+    // URL corrente del sito
+	$cf['site']['url']			= &$cf['site']['urls'][ $cf['site']['status'] ];
+
+    // dominio corrente del sito
+	$cf['site']['domain']			= &$cf['site']['domains'][ $cf['site']['status'] ];
+
+    // host corrente del sito
+	$cf['site']['host']			= &$cf['site']['hosts'][ $cf['site']['status'] ];
+
+    // FQDN corrente del sito
+	$cf['site']['fqdn']			= $cf['site']['host'] . ( ( ! empty( $cf['site']['domain'] ) ) ? '.' . $cf['site']['domain'] : NULL );
+
+    // percorso della cartella root del sito
+	$cf['site']['root']			= '/' . ( ( isset( $cf['site']['folders'][ $cf['site']['status'] ] ) ) ? $cf['site']['folders'][ $cf['site']['status'] ] : NULL );
+
+    // pulisco la variabile REDIRECT_URL per far corrispondere la cartella base alla pagina home
+	if( isset( $_SERVER['REDIRECT_URL'] ) ) {
+	    $_SERVER['REDIRECT_URL']		= substr( $_SERVER['REDIRECT_URL'], strlen( $cf['site']['root'] ) );
+	}
+
+    // configurazione extra
+	if( isset( $cx['site'] ) ) {
+	    $cf['site'] = array_replace_recursive( $cf['site'], $cx['site'] );
+	}
+
+    // collegamento dell'array $ct
+	$ct['site']				= &$cf['site'];
+
+    // debug
+	// dieText( print_r( $cf['site'], true ) );
+
+?>
