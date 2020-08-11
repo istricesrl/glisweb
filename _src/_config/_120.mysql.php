384e69780bbe534fdea1bb67d1ce4648cc0cab0a
Fabio Mosti <fabio.mosti@istricesrl.it>
2020-08-11 20:05:24 +0200

    aggiunti file _12*.mysql.php

---
+++ _src/_config/_120.mysql.php
@@ -0,0 +1,80 @@
+<?php
+
+    /**
+     * server e profili MySQL
+     *
+     * In questo file sono impostati i profili di funzionamento di MySQL
+     * e i parametri di configurazione dei server disponibili.
+     *
+     * array di configurazione dei server
+     * ==================================
+     * L'array $cf['mysql']['servers'] contiene tutte le informazioni necessarie
+     * al framework per utilizzare uno o più server MySQL. Ogni chiave rappresenta un
+     * diverso server e deve puntare a un array associativo che riporti i seguenti dati:
+     *
+     * chiave           | dettagli
+     * -----------------|-----------------------------------------------------------------------
+     * address          | l'indirizzo (nome host o IP) del server
+     * port             | la porta del server (default 3306)
+     * username         | il nome utente per la connessione
+     * password         | la password per la connessione
+     * db               | il database da utilizzare sul server
+     *
+     * array di configurazione dei profili
+     * ===================================
+     * Ogni profilo di funzionamento del sito può avere associati server diversi; questo
+     * consente lo switch veloce da un profilo all'altro senza doversi preoccupare di
+     * modificare la configurazione del database. Ogni profilo è descritto da un sottoarray
+     * dell'array $cf['mysql']['profiles'] nel quale ogni chiave corrisponde a un diverso
+     * livello di funzionamento del framework. L'array di configurazione del profilo deve
+     * contenere i seguenti campi:
+     *
+     * chiave           | dettagli
+     * -----------------|-----------------------------------------------------------------------
+     * servers          | i server attivi per il profilo
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     * @todo implementare bilanciamento del carico
+     *
+     * @file
+     *
+     */
+
+    // server disponibili
+	$cf['mysql']['servers']				= array();
+
+    // profili di funzionamento
+	$cf['mysql']['profiles'][ DEVELOPEMENT ]	=
+	$cf['mysql']['profiles'][ TESTING ]		=
+	$cf['mysql']['profiles'][ PRODUCTION ]		= array();
+
+    // connessioni disponibili
+	$cf['mysql']['connections']			= array();
+
+    // link alla connessione corrente
+	$cf['mysql']['connection']			= NULL;
+
+    // configurazione extra
+	if( isset( $cx['mysql'] ) ) {
+	    $cf['mysql'] = array_replace_recursive( $cf['mysql'], $cx['mysql'] );
+	}
+
+    // link al profilo corrente
+	$cf['mysql']['profile']				= &$cf['mysql']['profiles'][ $cf['site']['status'] ];
+
+    // collegamento all'array $ct
+	$ct['mysql']					= &$cf['mysql'];
+
+    // debug
+	// echo $cf['site']['status'];
+	// die( print_r( $cf['mysql']['profile'], true ) );
+	// print_r( $cf['mysql'] );
+	// print_r( $cx['mysql'] );
+
+?>
---
+++ _src/_config/_125.mysql.php
@@ -0,0 +1,114 @@
+<?php
+
+    /**
+     * connessioni MySQL
+     *
+     * In questo file vengono effettuate le connessioni ai server MySQL configurati.
+     *
+     *
+     *
+     *
+     * @file
+     *
+     */
+
+    // debug
+	// die( $cf['site']['status'] );
+	// die( print_r( $cf['mysql'], true ) );
+
+    // verifico che sia presente alemno un server
+	if( isset( $cf['mysql']['profile']['servers'] ) && is_array( $cf['mysql']['profile']['servers'] ) ) {
+
+	    // verifico che sia presente almeno un server
+		if( count( $cf['mysql']['profile']['servers'] ) > 0 ) {
+
+		    // ciclo sui server attivi per lo stato corrente
+			foreach( $cf['mysql']['profile']['servers'] as $server ) {
+
+			    // log
+				logWrite( 'tento la connessione a: ' . $server, 'mysql' );
+
+			    // inizializzo la connessione
+				$cn = mysqli_init();
+
+			    // riduco il tempo massimo di connessione per evitare rallentamenti
+				mysqli_options( $cn, MYSQLI_OPT_CONNECT_TIMEOUT, 3 );
+
+			    // connessione
+				mysqli_real_connect(
+				    $cn,
+				    $cf['mysql']['servers'][ $server ]['address'],
+				    $cf['mysql']['servers'][ $server ]['username'],
+				    $cf['mysql']['servers'][ $server ]['password']
+				);
+
+			    // controllo errori
+				if( mysqli_connect_errno() ) {
+
+				    // log
+					logWrite( 'errore di connessione a ' . $server . ': ' . mysqli_connect_errno() . ' ' . mysqli_connect_error(), 'mysql', LOG_ERR );
+
+				} else {
+
+				    // versione del server
+					$cf['mysql']['servers'][ $server ]['version'] = mysqli_get_server_info( $cn );
+
+				    // selezione database
+					if( mysqli_select_db( $cn, $cf['mysql']['servers'][ $server ]['db'] ) ) {
+					    logWrite( 'database selezionato: ' . $cf['mysql']['servers'][ $server ]['db'], 'mysql' );
+					} else {
+					    logWrite( 'impossibile selezionare il database: ' . $cf['mysql']['servers'][ $server ]['db'], 'mysql', LOG_ERR );
+					}
+
+				    // character set
+#					mysqli_set_charset( $cn, 'utf8' );
+
+				    // collation
+					mysqlQuery( $cn, 'SET character_set_connection = utf8' );
+					mysqlQuery( $cn, 'SET collation_connection = utf8_general_ci' );
+
+				    // timezone
+					mysqlQuery( $cn, 'SET time_zone = ?', array( array( 's' => $cf['localization']['timezone']['name'] ) ) );
+
+				    // localizzazione
+					mysqlQuery( $cn, 'SET lc_time_names = ?', array( array( 's' => str_replace( '-', '_', $cf['localization']['language']['ietf'] ) ) ) );
+
+				    // log
+					logWrite( 'connessione stabilita: ' . $server, 'mysql' );
+					logWrite( 'dettagli: ' . mysqli_get_host_info( $cn ), 'mysql' );
+
+				    // aggiungo la connessione all'array
+					$cf['mysql']['connections'][ $server ] = $cn;
+
+				}
+
+			}
+
+		    // connessione di default
+			if( count( $cf['mysql']['connections'] ) ) {
+			    $keys = array_keys( $cf['mysql']['connections'] );
+			    $key = array_shift( $keys );
+			    $cf['mysql']['connection'] = &$cf['mysql']['connections'][ $key ];
+			    $cf['mysql']['server'] = &$cf['mysql']['servers'][ $key ];
+			}
+
+		} else {
+
+		    // log
+			logWrite( 'nessun server MySQL impostato per il livello di funzionamento corrente', 'mysql' );
+
+		}
+
+	} else {
+
+	    // log
+		logWrite( 'backend MySQL non configurato', 'mysql' );
+
+	}
+
+    // debug
+	// print_r( $cf['mysql']['profile'] );
+	// print_r( $cf['mysql']['connections'] );
+	// print_r( $cf['mysql'] );
+
+?>
