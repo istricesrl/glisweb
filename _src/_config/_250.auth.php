15aa8df3ffac5156cd423945f4429989310142ac
Fabio Mosti <fabio.mosti@istricesrl.it>
2020-08-13 12:30:40 +0200

    aggiunti file _25*.auth.php

---
+++ _src/_config/_250.auth.php
@@ -0,0 +1,171 @@
+<?php
+
+    /**
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     * @todo i permessi per le entità andrebbero dichiarati "più in basso" possibile, nei moduli che le usano
+     * @todo documentare
+     *
+     * @file
+     *
+     */
+
+    // array dei permessi
+	$cf['auth']['permissions'] = array(
+	    'account' => array(
+		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'account_gruppi' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'account_gruppi_attribuzione' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'anagrafica' => array(
+#		CONTROL_FULL => array( 'roots' ),
+#		CONTROL_FILTERED => array( 'staff' )
+#	    ),
+#	    'anagrafica_archiviati' => array(
+#		CONTROL_FULL => array( 'roots' ),
+#		CONTROL_FILTERED => array( 'staff' )
+#	    ),
+#	    'anagrafica_categorie' => array(
+#		CONTROL_FULL => array( 'roots' ),
+#		CONTROL_FILTERED => array( 'staff' )
+#	    ),
+#	    'anagrafica_cittadinanze' => array(
+#		CONTROL_FULL => array( 'roots' ),
+#		CONTROL_FILTERED => array( 'staff' )
+#	    ),
+#	    'anagrafica_clienti' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'anagrafica_ruoli' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'anagrafica_settori' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'attivita' => array(
+#		CONTROL_FULL => array( 'roots' ),
+#		CONTROL_FILTERED => array( 'staff' )
+#	    ),
+#	    'audio' => array(
+#		CONTROL_FULL => array( 'roots' ),
+#		CONTROL_FILTERED => array( 'staff' )
+#	    ),
+#	    'categorie_anagrafica' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'comuni' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'contenuti' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'cron' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'date' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'file' => array(
+#		CONTROL_FULL => array( 'roots' ),
+#		CONTROL_FILTERED => array( 'staff' )
+#	    ),
+#	    'gruppi' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'immagini' => array(
+#		CONTROL_FULL => array( 'roots' ),
+#		CONTROL_FILTERED => array( 'staff' )
+#	    ),
+#	    'immagini_anagrafica' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'indirizzi' => array(
+#		CONTROL_FULL => array( 'roots' ),
+#		CONTROL_FILTERED => array( 'staff' )
+#	    ),
+#	    'job' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'luoghi' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'mail' => array(
+#		CONTROL_FULL => array( 'roots' ),
+#		CONTROL_FILTERED => array( 'staff' )
+#	    ),
+#	    'mail_out' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'mail_sent' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'metadati' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'prezzi' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'recensioni' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'redirect' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'settori' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'sms_out' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'sms_sent' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'telefoni' => array(
+#		CONTROL_FULL => array( 'roots' ),
+#		CONTROL_FILTERED => array( 'staff' )
+#	    ),
+#	    'template_mail' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'test' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'tipologie_anagrafica' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'tipologie_attivita' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'tipologie_crm' => array(
+#		CONTROL_FULL => array( 'roots' )
+#	    ),
+#	    'video' => array(
+#		CONTROL_FULL => array( 'roots' )
+	    )
+	);
+
+?>
---
+++ _src/_config/_255.auth.php
@@ -0,0 +1,45 @@
+<?php
+
+    /**
+     *
+     *
+     *
+     *
+     *
+     *
+     * @todo visto che $_SESSION è permanente, questa cosa andrebbe fatta solo se c'è un login in corso!
+     * @todo verificare che questa cosa sia effettivamente utile
+     * @todo documentare
+     *
+     * @file
+     *
+     */
+
+    // TODO a cosa serve questa cosa?
+
+    // array dei permessi per l'utente connesso
+    // TODO questo è già valorizzato in _210.auth.php
+	// $_SESSION['account']['permissions'] = array();
+
+    // popolo l'array dei permessi
+	if( $cf['auth']['status'] == LOGIN_SUCCESS ) {
+	    if( isset( $_SESSION['account']['gruppi'] ) ) {
+		foreach( $cf['auth']['permissions'] as $t => $w ) {
+		    foreach( $w as $k => $j ) {
+			if( array_intersect( $_SESSION['account']['gruppi'], $j ) ) {
+			    $_SESSION['account']['permissions'][ $t ][] = $k;
+			}
+		    }
+		}
+	    }
+	} elseif( isset( $_SESSION['account'] ) && ! empty( $_SESSION['account'] ) ) {
+	    $cf['auth']['status'] = LOGIN_LOGGED;
+	}
+
+    // debug
+	// print_r( $_SESSION['account'] );
+	// print_r( $_SESSION['account']['permissions'] );
+	// print_r( $cf['auth'] );
+	// echo $cf['auth']['status'];
+
+?>
