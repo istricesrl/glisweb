5ff18f3dc6f08559bd47c0e749cd98ee2c13290c
Fabio Mosti <fabio.mosti@istricesrl.it>
2020-08-13 15:32:25 +0200

    aggiunto file con la pagina radice

---
+++ _src/_inc/_pages/_null.it-IT.php
@@ -0,0 +1,29 @@
+<?php
+
+    /**
+     * definizione della pagina di default per la lingua italiana
+     *
+     *
+     *
+     * @todo commentare
+     *
+     * @file
+     *
+     */
+
+    // lingua di questo file
+	$l = 'it-IT';
+
+    // pagina di default
+	$p[ NULL ] = array(
+	    'sitemap'		=> false,
+	    'headers'		=> array( 'Cache-Control: no-cache, must-revalidate' ),
+	    'title'		=> array( $l		=> 'pagina generata automaticamente' ),
+	    'h1'		=> array( $l		=> 'pagina generata automaticamente' ),
+	    'template'		=> array( 'path'	=> '_src/_templates/_default/', 'schema' => 'default.html' ),
+	    'parent'		=> array( 'id'		=> NULL ),
+	    'content'		=> array( $l		=> '<p>pagina generata il ' . date( 'Y-m-d H:i:s' ) . '</p>' ),
+	    'rewrited'		=> array( $l		=> NULL )
+	);
+
+?>
