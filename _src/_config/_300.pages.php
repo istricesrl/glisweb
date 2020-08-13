3e15957c0f793eb8633d0b68cb7e95999034e0a1
Fabio Mosti <fabio.mosti@istricesrl.it>
2020-08-13 12:33:30 +0200

    aggiunto file _300.pages.php

---
+++ _src/_config/_300.pages.php
@@ -0,0 +1,63 @@
+<?php
+
+    /**
+     * cache delle pagine
+     *
+     *
+     *
+     *
+     * cache dell'elaborazione dei contenuti
+     * =====================================
+     * In questo file vediamo in azione il meccanismo centrale di caching della parte più onerosa dell'esecuzione del
+     * framework, nella fattispecie il caricamento e l'elaborazione dell'albero delle pagine; poiché molti moduli
+     * creano, con i loro dati, delle pagine virtuali a loro volta, anche in essi si ritroverà lo stesso meccanismo.
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     * @todo finire di documentare
+     *
+     * @file
+     *
+     */
+
+    // log
+	logWrite( 'la costante MEMCACHE_REFRESH ' . ( ( defined( 'MEMCACHE_REFRESH' ) ) ? NULL : 'NON ' ) . 'È definita', 'speed' );
+
+    // elegibilità della cache
+	$cf['contents']['cached'] = ( ( defined( 'MEMCACHE_REFRESH' ) ) ? false : true );
+
+    // configurazione extra
+	if( isset( $cx['contents'] ) ) {
+	    $cf['contents'] = array_replace_recursive( $cf['contents'], $cx['contents'] );
+	}
+
+    // tento di leggere i valori dalla cache
+	$cf['contents']['updated']		= memcacheRead( $cf['memcache']['connection'], CONTENTS_PAGES_UPDATED );
+	$cf['contents']['pages']		= memcacheRead( $cf['memcache']['connection'], CONTENTS_PAGES_KEY );
+	$cf['contents']['tree']			= memcacheRead( $cf['memcache']['connection'], CONTENTS_TREE_KEY );
+	$cf['contents']['index']		= memcacheRead( $cf['memcache']['connection'], CONTENTS_INDEX_KEY );
+	$cf['contents']['shortcuts']		= memcacheRead( $cf['memcache']['connection'], CONTENTS_SHORTCUTS_KEY );
+
+    // elegibilità della cache
+	if( $cf['contents']['updated']		=== false
+	    || $cf['contents']['pages']		=== false
+	    || $cf['contents']['tree']		=== false
+	    || $cf['contents']['index']		=== false
+	    || $cf['contents']['shortcuts']	=== false
+	) {
+	    $cf['contents']['cached'] = false;
+	    logWrite( 'struttura delle pagine non presente in cache', 'performances', LOG_NOTICE );
+	} else {
+	    logWrite( 'struttura delle pagine presente in cache', 'performances' );
+	}
+
+    // debug
+	// var_dump( $cf['contents']['cached'] );
+
+?>
