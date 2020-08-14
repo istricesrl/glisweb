dd15aa3fae4befa919e833faba42e2b9599cc5ee
Fabio Mosti <fabio.mosti@istricesrl.it>
2020-08-14 13:31:01 +0200

    aggiunto file _420.pages.php

---
+++ _src/_config/_420.pages.php
@@ -0,0 +1,109 @@
+<?php
+
+    /**
+     * applicazione della configurazione della pagina corrente
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     * @todo documentare
+     *
+     * @file
+     *
+     */
+
+    // link alla pagina corrente
+	$cf['contents']['page']			= &$cf['contents']['pages'][ $page ];
+
+    // TODO fare questa cosa con un ciclo per tutte le chiavi di $cf['contents']
+    // in pratica $ct deve essere la scorciatoia per $cf['contents']
+
+    // NOTA è una brutta asimmetria, ma giustificata dalla brevità di scrittura?
+
+    // collego l'array delle pagine a $ct
+	$ct['pages']				= &$cf['contents']['pages'];
+
+    // collego la pagina corrente a $ct
+	$ct['page']				= &$cf['contents']['page'];
+
+    // versione canonica della pagina
+	if( ! in_array( $_SERVER['REQUEST_URI'], $cf['contents']['page']['path'] ) ) {
+	    $cf['contents']['page']['canonical'] = $cf['contents']['page']['id'];
+	}
+
+    // pulitura delle tab
+	if( isset( $ct['page']['etc']['tabs'] ) ) {
+	    foreach( $ct['page']['etc']['tabs'] as $key => $tab ) {
+		if( isset( $cf['contents']['pages'][ $tab ]['auth']['groups'] ) &&
+		    ( ! isset( $_SESSION['account']['gruppi'] ) ||
+			count(
+			    array_intersect(
+				$cf['contents']['pages'][ $tab ]['auth']['groups'],
+				$_SESSION['account']['gruppi']
+			    )
+			) == 0
+		    )
+		) {
+		    unset( $ct['page']['etc']['tabs'][ $key ] );
+		}
+	    }
+	}
+
+    // forzatura del template corrente per one-char parameter debug
+	if( isset( $_REQUEST['t'] ) ) {
+	    if( file_exists( DIRECTORY_BASE . 'src/templates/' . $_REQUEST['t'] . '/' ) ) {
+		$ct['page']['template']['path']	= 'src/templates/' . $_REQUEST['t'] . '/';
+	    } elseif( file_exists( DIRECTORY_BASE . '_src/_templates/_' . $_REQUEST['t'] . '/' ) ) {
+		$ct['page']['template']['path']	= '_src/_templates/_' . $_REQUEST['t'] . '/';
+	    }
+	    $ct['page']['template']['schema']	= 'default.html';
+	}
+
+    // forzatura dello schema corrente per one-char parameter debug
+	if( isset( $_REQUEST['s'] ) ) {
+	    $ct['page']['template']['schema']	= $_REQUEST['s'] . '.html';
+	}
+
+    // forzatura dei contenuti correnti per one-char parameter debug
+	if( isset( $_REQUEST['j'] ) ) {
+	    $ct['page']['content'][ $cf['localization']['language']['ietf'] ]	= implode( PHP_EOL, array_fill( 0, $_REQUEST['j'], '<p>'.$cf['common']['lorem'].'</p>' ) );
+	}
+
+    /*
+     * @todo prevedere la forzatura anche per il tema della pagina
+     */
+
+    // log
+	logWrite( 'pagina corrente: ' . $page, 'rewrite', LOG_INFO );
+
+    // debug
+	// print_r( $cf['contents']['pages']['licenza']['content'] );
+	// print_r( $cf['contents']['page']['path'] );
+	// print_r( $cf['contents']['page'] );
+	// print_r( $ct['page'] );
+	// var_dump( $_SERVER['REQUEST_URI'] );
+	// print_r( $ct['page']['etc']['tabs'] );
+	// print_r( $_SESSION['account']['gruppi'] );
+	// echo $cf['contents']['updated'];
+
+?>
