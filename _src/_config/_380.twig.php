e52f3f4d9144037104d513e9181164c89b0aaab8
Fabio Mosti <fabio.mosti@istricesrl.it>
2020-08-14 13:29:54 +0200

    aggiunti file _38*.twig.php

---
+++ _src/_config/_380.twig.php
@@ -0,0 +1,62 @@
+<?php
+
+    /**
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     * nota sulla struttura dei profili
+     * ================================
+     * Questo è un ottimo esempio di come la struttura standard dei profili operi correttamente anche in mancanza
+     * di alcuni elementi; Twig ad esempio funziona a profili ma non ha server né connessioni, quindi alcuni passaggi
+     * dello schema standard dei profili sono assenti, ma gli altri sono ancora al loro posto e nell'ordine consueto:
+     *
+     * -# dichiarazione dei server disponibili (NO)
+     * -# dichiarazione dei profili per ogni status
+     * -# inizializzazione dell'array delle connessioni (NO)
+     * -# applicazione della configurazione extra
+     * -# collegamento di $ct (NO)
+     *
+     * Nel secondo file, quello esecutivo, la sequenza è:
+     *
+     * -# collegamento del profilo corrente
+     * -# collegamento della connessione corrente (NO)
+     * -# collegamento del server corrente (NO)
+     *
+     *
+     *
+     *
+     * @todo documentare
+     *
+     * @file
+     *
+     */
+
+    // profili di funzionamento di Twig per DEV/TEST
+	$cf['twig']['profiles'][ DEVELOPEMENT ]		=
+	$cf['twig']['profiles'][ TESTING ]		= array(
+	    'debug' => true
+	);
+
+    // profilo di funzionamento di Twig per PROD
+	$cf['twig']['profiles'][ PRODUCTION ]		= array(
+	    'cache' => DIR_VAR_CACHE_TWIG
+	);
+
+    // configurazione extra
+	if( isset( $cx['twig'] ) ) {
+	    $cf['twig'] = array_replace_recursive( $cf['twig'], $cx['twig'] );
+	}
+
+?>
---
+++ _src/_config/_385.twig.php
@@ -0,0 +1,24 @@
+<?php
+
+    /**
+     *
+     *
+     *
+     *
+     *
+     *
+     * @todo documentare
+     *
+     * @file
+     *
+     */
+
+    // link al profilo corrente
+	$cf['twig']['profile']				= &$cf['twig']['profiles'][ $cf['site']['status'] ];
+
+    // controllo la cartella per la cache di Twig
+	if( isset( $cf['twig']['profile']['cache'] ) ) {
+	    checkFolder( $cf['twig']['profile']['cache'] );
+	}
+
+?>
