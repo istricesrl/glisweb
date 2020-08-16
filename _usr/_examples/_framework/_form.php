b1a9577b05d81b443317069b20ec7ede9148bf1c
Fabio Mosti <fabio.mosti@istricesrl.it>
2020-08-16 12:57:54 +0200

    aggiunti esempi form e login

---
+++ _usr/_examples/_framework/_form.php
@@ -0,0 +1,59 @@
+<?php
+
+    /**
+     * test delle variabili
+     *
+     *
+     *
+     * @todo documentare
+     *
+     * @file
+     *
+     */
+
+    // inclusione del framework
+	require '../../../_src/_config.php';
+
+    // ordinamento degli array per la scrittura
+	rksort( $cf );
+	rksort( $ct );
+
+    // nascondo le password
+	array2censored( $cf );
+
+    // output
+	$tx	= NULL;
+	$tx	.= '<html>';
+	$tx	.= '<head></head>';
+	$tx	.= '<body style="font-family: monospace;">';
+
+    // test
+	$tx	.= '<h1>test</h1>'
+		. '<form action="" method="post">'
+		. '<div><label for="test[nome]">nome</label></div>'
+		. '<div><input type="text" name="test[nome]" /></div>'
+		. '<div><label for="test[mail]">mail</label></div>'
+		. '<div><input type="text" name="test[mail]" /></div>'
+		. '<div><input type="submit" /></div>'
+		. '</form>';
+
+    // array $_REQUEST
+	$tx	.= '<h2>array $_REQUEST[\'__view__\']</h2>';
+	$tx	.= '<pre>' . print_r( $_REQUEST['__view__'], true ) . '</pre>';
+
+    // array $_REQUEST
+	$tx	.= '<h2>array $_REQUEST[\'__info__\']</h2>';
+	$tx	.= '<pre>' . print_r( $_REQUEST['__info__'], true ) . '</pre>';
+
+    // array $_REQUEST
+	$tx	.= '<h2>array $_REQUEST[\'__err__\']</h2>';
+	$tx	.= '<pre>' . print_r( $_REQUEST['__err__'], true ) . '</pre>';
+
+    // output
+	$tx	.= '</body>';
+	$tx	.= '</html>';
+
+    // output
+	buildHTML( $tx );
+
+?>
---
+++ _usr/_examples/_framework/_login.php
@@ -0,0 +1,61 @@
+<?php
+
+    /**
+     * test delle variabili
+     *
+     *
+     *
+     * @todo documentare
+     *
+     * @file
+     *
+     */
+
+    // inclusione del framework
+	require '../../../_src/_config.php';
+
+    // ordinamento degli array per la scrittura
+	rksort( $cf );
+	rksort( $ct );
+
+    // nascondo le password
+	array2censored( $cf );
+
+    // output
+	$tx	= NULL;
+	$tx	.= '<html>';
+	$tx	.= '<head></head>';
+	$tx	.= '<body style="font-family: monospace;">';
+
+    // test
+	if( $cf['auth']['status'] != LOGIN_LOGGED ) {
+	    $tx	.= '<h1>login</h1>'
+		. '<form action="" method="post">'
+		. '<div><input type="text" name="__login__[user]" /></div>'
+		. '<div><input type="password" name="__login__[pasw]" /></div>'
+		. '<div><input type="submit" /></div>'
+		. '</form>';
+	} else {
+	    $tx	.= '<h1>logout</h1>'
+		. '<form action="" method="post">'
+		. '<input type="hidden" name="__logout__" value="1" />'
+		. '<div><input type="submit" /></div>'
+		. '</form>';
+	}
+
+    // array $_SESSION
+	$tx	.= '<h2>array $ct[\'session\']</h2>';
+	$tx	.= '<pre>' . print_r( $ct['session'], true ) . '</pre>';
+
+    // array $cf
+	$tx	.= '<h2>array $cf[\'auth\'][\'status\']</h2>';
+	$tx	.= '<pre>' . print_r( $cf['auth']['status'], true ) . '</pre>';
+
+    // output
+	$tx	.= '</body>';
+	$tx	.= '</html>';
+
+    // output
+	buildHTML( $tx );
+
+?>
