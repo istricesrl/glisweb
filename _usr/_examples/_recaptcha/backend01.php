9ab55db8f09a0fadac322a057e2e8db964ae7839
Fabio Mosti <fabio.mosti@istricesrl.it>
2020-10-07 13:14:28 +0200

    rimossa chiave reCaptcha esposta in esempi recaptcha

--- _usr/_examples/_recaptcha/backend01.php
+++ _usr/_examples/_recaptcha/backend01.php
@@ -1,26 +1,15 @@
 <?php
 
-    function reCaptchaVerifyV3( $t ) {
-
-	$h = 'https://www.google.com/recaptcha/api/siteverify';
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $h);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_HEADER, false);
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
-	    'secret' => '6LeWItIZAAAAAOnXSW6ISp-Af8A2dzElj_r1HB-1',
-	    'response' => $t
-	)));
-	$output = curl_exec($ch);
-	curl_close($ch);
-
-	return json_decode( $output, true );
-
-    }
+    // inclusione del framework
+	require '../../../_src/_config.php';
 
     print_r( $_REQUEST );
 
-    print_r( reCaptchaVerifyV3($_REQUEST['g-recaptcha-response']));
+    print_r( 
+		reCaptchaVerifyV3(
+			$_REQUEST['g-recaptcha-response'],
+			$cf['google']['recaptcha']['profile']['secret']
+		)
+	);
 
 ?>
