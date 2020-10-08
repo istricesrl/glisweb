5710e51047b1c57fd9e6916821a37e8cd368378f
Fabio Mosti <fabio.mosti@istricesrl.it>
2020-10-08 18:32:21 +0200

    debug e correzioni anagrafica form amministrazione

--- _src/_inc/_macro/_anagrafica.form.amministrazione.php
+++ _src/_inc/_macro/_anagrafica.form.amministrazione.php
@@ -22,7 +22,22 @@
     // tabella gestita
 	$ct['form']['table'] = 'anagrafica';
 	
-	//mettere la tendina delle tipologie di fatturazione elettronica
+	// tendina regimi fiscali
+	$ct['etc']['select']['regimi_fiscali'] = mysqlCachedIndexedQuery(
+	    $cf['cache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM regimi_fiscali_view'
+	);
+
+	// tendina PEC
+	$ct['etc']['select']['pec'] = mysqlCachedIndexedQuery(
+	    $cf['cache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+        'SELECT id, __label__ FROM mail_view WHERE id_anagrafica = ? AND se_pec = 1',
+        array( array( 's' => $_REQUEST['anagrafica']['id'] ) )
+	);
 
 	// macro di default
 	require DIR_SRC_INC_MACRO . '_default.form.php';
