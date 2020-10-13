37bf649ffcb039ba975919ad3b3bbc9d1a52642d
Fabio Mosti <fabio.mosti@istricesrl.it>
2020-10-13 18:11:24 +0200

    impostato terminale cassa

--- _mod/_6500.casse/_src/_inc/_pages/_casse.it-IT.php
+++ _mod/_6500.casse/_src/_inc/_pages/_casse.it-IT.php
@@ -11,12 +11,25 @@
 	    'sitemap'		=> false,
 	    'title'		=> array( $l		=> 'cassa' ),
 	    'h1'		=> array( $l		=> 'cassa' ),
-	    'template'		=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'dashboard.html' ),
-	    'macro'		=> array( $m . '_src/_inc/_macro/_dashboard.php' ),
+	    'template'		=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'dashboard.cassa.html' ),
+	    'macro'		=> array( $m . '_src/_inc/_macro/_dashboard.cassa.php' ),
 	    'parent'		=> array( 'id'		=> NULL ),
 	    'auth'		=> array( 'groups'	=> array(	'roots' ) ),
 	    'menu'		=> array( 'admin'	=> array(	'label'		=> array( $l => 'cassa' ),
 									'priority'	=> 650 ) )
+	);
+
+    // pannello della cassa
+	$p['pannello.cassa'] = array(
+	    'sitemap'		=> false,
+	    'title'		=> array( $l		=> 'terminale' ),
+	    'h1'		=> array( $l		=> 'terminale' ),
+	    'template'		=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'pannello.cassa.html' ),
+	    'macro'		=> array( $m . '_src/_inc/_macro/_pannello.cassa.php' ),
+	    'parent'		=> array( 'id'		=> 'dashboard.cassa' ),
+	    'auth'		=> array( 'groups'	=> array(	'roots' ) ),
+	    'menu'		=> array( 'admin'	=> array(	'label'		=> array( $l => 'terminale' ),
+									'priority'	=> 100 ) )
 	);
 
     // debug
