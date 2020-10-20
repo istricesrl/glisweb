f118fbe2de3b68cddab35189464d7b988f27a10e
Fabio Mosti <fabio.mosti@istricesrl.it>
2020-10-20 22:47:44 +0200

    debug form multilingua

--- _mod/_3000.contenuti/_src/_inc/_macro/_pagine.form.default.php
+++ _mod/_3000.contenuti/_src/_inc/_macro/_pagine.form.default.php
@@ -3,9 +3,13 @@
     // integro le lingue mancanti
 	foreach( array_column( $cf['localization']['languages'], 'id' ) as $l ) {
 	    if(
-            ! isset( $_REQUEST[ $ct['form']['table'] ][ $ct['form']['subtable'] ] )
-            ||
-	    	! in_array( $l, array_column( $_REQUEST[ $ct['form']['table'] ][ $ct['form']['subtable'] ], 'id_lingua' ) ) ) {
+            in_array( $ct['form']['subtable'], array( 'contenuti' ) )
+            && (
+                ! isset( $_REQUEST[ $ct['form']['table'] ][ $ct['form']['subtable'] ] )
+                ||
+                ! in_array( $l, array_column( $_REQUEST[ $ct['form']['table'] ][ $ct['form']['subtable'] ], 'id_lingua' ) )
+            )
+        ) {
 
             // integro la lingua mancante
                 $_REQUEST[ $ct['form']['table'] ][ $ct['form']['subtable'] ][]['id_lingua'] = $l;
