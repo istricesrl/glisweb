b76395a9a76b49773aec3a6be09c637bd6904d9b
Fabio Mosti <fabio.mosti@istricesrl.it>
2020-10-26 11:41:07 +0100

    debug macro gruppi view

--- _src/_inc/_macro/_gruppi.view.php
+++ _src/_inc/_macro/_gruppi.view.php
@@ -15,24 +15,16 @@
     // pagina per la gestione degli oggetti esistenti
 	$ct['view']['open']['page'] = 'gruppi.form';
 
-
     // campi della vista
 	$ct['view']['cols'] = array(
 	    'id' => '#',
-	    '__label__' => 'pagina',
-	    'template' => 'template',
-	    'schema_html' => 'schema',
-	    'tipologia_pubblicazione' => 'pubblicazione'
+	    '__label__' => 'gruppo'
 	);
 
     // stili della vista
 	$ct['view']['class'] = array(
-	    '__label__' => 'text-left no-wrap',
-	    'template' => 'text-left',
-	    'schema_html' => 'text-left',
-	    'tipologia_pubblicazione' => 'text-left'
+	    '__label__' => 'text-left no-wrap'
 	);
-
 
     // gestione default
 	require DIR_SRC_INC_MACRO . '_default.view.php';
