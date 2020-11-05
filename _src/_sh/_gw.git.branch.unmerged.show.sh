ff363dd1a7f393095179c2ba7949f2c102a42560
Fabio Mosti <fabio.mosti@istricesrl.it>
2020-11-05 17:43:57 +0100

    aggiunto script per visualizzare le branch unmerged

---
+++ _src/_sh/_gw.git.branch.unmerged.show.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## livelli per la root del sito
+RL="../../"
+
+## directory corrente
+cd $(dirname "$0")
+cd $RL
+
+## informazioni
+echo "lavoro su: $(pwd)"
+
+## pulizia repository
+git branch --no-merged
