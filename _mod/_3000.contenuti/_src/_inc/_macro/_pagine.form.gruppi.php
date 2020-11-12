8a7e3daaeafda20845fbc66ca2d376d4bc16f065
sophie <sophie.lacordais@hotmail.it>
2020-11-12 14:35:14 +0100

    implementata subform pagine.gruppi

--- _mod/_3000.contenuti/_src/_inc/_macro/_pagine.form.gruppi.php
+++ _mod/_3000.contenuti/_src/_inc/_macro/_pagine.form.gruppi.php
@@ -28,22 +28,13 @@
     // pagina per la gestione degli oggetti esistenti
 	$ct['view']['open']['page'] = 'pagine.form';
     
-     // campi della vista
-	$ct['view']['cols'] = array(
-	    'id' => '#',
-	    'id_pagina' => 'pagina',
-	    'id_gruppo' => 'gruppi'
-	);
-
-    // stili della vista
-	$ct['view']['class'] = array(
-	    'id' => 'd-none d-md-table-cell',
-	    'id_pagina' => 'text-left',
-	    'id_gruppo' => 'text-left'
-	);
-
-    // preset filtro custom progetti aperti
-	//$ct['view']['__restrict__']['id_pagina']['LK'] = $_REQUEST[ $ct['form']['table'] ]['id'];
+    // tendina gruppi 
+	$ct['etc']['select']['gruppi'] = mysqlCachedIndexedQuery(
+	    $cf['cache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+        'SELECT id, __label__ FROM gruppi_view'
+    );   
 
 	// macro di default
 	require DIR_SRC_INC_MACRO . '_default.form.php';
