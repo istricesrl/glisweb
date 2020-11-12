3f35503406fbaff3d90b8eb0b712185db4b24e24
Fabio Mosti <fabio.mosti@istricesrl.it>
2020-11-12 22:01:23 +0100

    aggiunta libreria mysql utils

---
+++ _src/_lib/_mysql.utils.php
@@ -0,0 +1,13 @@
+<?php
+
+    function trovaIdComune( $c, $p = NULL ) {
+
+        global $cf;
+
+        $comuni = mysqlQuery(
+            $cf['mysql']['connection'],
+            'SELECT * FROM comuni WHERE nome = ?',
+            array( array( 's' => $c ) )
+        )
+
+    }
