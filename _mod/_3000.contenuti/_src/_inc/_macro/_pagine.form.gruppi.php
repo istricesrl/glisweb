4498a9cdca8e3677e382d4d52b4dbd596c066681
sophie <sophie.lacordais@hotmail.it>
2020-11-12 16:02:45 +0100

    corretti campi della subform pagine.gruppi

--- _mod/_3000.contenuti/_src/_inc/_macro/_pagine.form.gruppi.php
+++ _mod/_3000.contenuti/_src/_inc/_macro/_pagine.form.gruppi.php
@@ -22,12 +22,6 @@
      // tabella gestita
     $ct['form']['table'] = 'pagine';
     
-    // tabella della vista
-	$ct['view']['table'] = 'pagine_gruppi';
-
-    // pagina per la gestione degli oggetti esistenti
-	$ct['view']['open']['page'] = 'pagine.form';
-    
     // tendina gruppi 
 	$ct['etc']['select']['gruppi'] = mysqlCachedIndexedQuery(
 	    $cf['cache']['index'],
