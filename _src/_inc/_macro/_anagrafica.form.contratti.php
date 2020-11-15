a3185a372bb58a451247114dc5cfb8161bdbd5fd
Fabio Mosti <fabio.mosti@istricesrl.it>
2020-11-15 16:00:37 +0100

    aggiunta tab contratti in gestione anagrafica

---
+++ _src/_inc/_macro/_anagrafica.form.contratti.php
@@ -0,0 +1,32 @@
+<?php
+
+    /**
+     * macro form anagrafica
+     *
+     *
+     *
+     * -# definizione della tabella del modulo
+     * -# popolazione delle tendine
+     *
+     *
+     *
+     *
+     *
+     *
+     * @todo documentare
+     *
+     * @file
+     *
+     */
+
+    // tabella gestita
+    $ct['form']['table'] = 'anagrafica';
+
+
+    // macro di default per l'entità anagrafica
+	require DIR_SRC_INC_MACRO . '_anagrafica.form.default.php';
+
+	// macro di default
+	require DIR_SRC_INC_MACRO . '_default.form.php';
+
+?>
