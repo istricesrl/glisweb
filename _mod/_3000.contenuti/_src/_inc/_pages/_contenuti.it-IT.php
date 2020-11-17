6eb8d4b406cc28125c5551d78589ac968cf39ce7
sophie <sophie.lacordais@hotmail.it>
2020-11-17 17:37:42 +0100

    Eliminati file inerenti a popup dal modulo _3000.contenuti

--- _mod/_3000.contenuti/_src/_inc/_pages/_contenuti.it-IT.php
+++ _mod/_3000.contenuti/_src/_inc/_pages/_contenuti.it-IT.php
@@ -149,60 +149,6 @@
 	    'auth'		=> array( 'groups'	=> array(	'roots', 'staff' ) ),
 	    'etc'		=> array( 'tabs'	=> $p['redirect.form']['etc']['tabs'] )
     );
-
-	// vista popup
-	$p['popup.view'] = array(
-	    'sitemap'		=> false,
-	    'title'		=> array( $l		=> 'popup' ),
-	    'h1'		=> array( $l		=> 'popup' ),
-	    'parent'		=> array( 'id'		=> 'contenuti' ),
-	    'template'		=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'default.view.html' ),
-	    'macro'		=> array( $m . '_src/_inc/_macro/_popup.view.php' ),
-	    'auth'		=> array( 'groups'	=> array(	'roots', 'staff' ) ),
-	    'etc'		=> array( 'tabs'	=> array(	'popup.view',
-									'popup.tools' ) ),
-	    'menu'		=> array( 'admin'	=> array(	'label'		=> array( $l => 'popup' ),
-									'priority'	=> '100' ) )
-    );
-
-    // gestione popup
-	$p['popup.form'] = array(
-	    'sitemap'		=> false,
-	    'title'		=> array( $l		=> 'gestione' ),
-	    'h1'		=> array( $l		=> 'gestione' ),
-	    'parent'		=> array( 'id'		=> 'popup.view' ),
-	    'template'		=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'popup.form.html' ),
-	    'macro'		=> array( $m . '_src/_inc/_macro/_popup.form.php' ),
-	    'auth'		=> array( 'groups'	=> array(	'roots', 'staff' ) ),
-		'etc'		=> array( 'tabs'	=> array(	'popup.form',
-													'popup.form.pagine',
-													'popup.form.testo',
-													'popup.form.tools') )
-	);
-
-	// gestione pagine popup
-	$p['popup.form.pagine'] = array(
-	    'sitemap'		=> false,
-	    'title'		=> array( $l		=> 'pagine' ),
-	    'h1'		=> array( $l		=> 'pagine' ),
-	    'parent'		=> array( 'id'		=> 'popup.view' ),
-	    'template'		=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'popup.form.pagine.html' ),
-	    'macro'		=> array( $m . '_src/_inc/_macro/_popup.form.pagine.php' ),
-	    'auth'		=> array( 'groups'	=> array(	'roots', 'staff' ) ),
-		'etc'		=> array( 'tabs'	=> $p['popup.form']['etc']['tabs'] )
-	);
-
-	// gestione testo popup
-	$p['popup.form.testo'] = array(
-	    'sitemap'		=> false,
-	    'title'		=> array( $l		=> 'testo' ),
-	    'h1'		=> array( $l		=> 'testo' ),
-	    'parent'		=> array( 'id'		=> 'popup.view' ),
-	    'template'		=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'popup.form.testo.html' ),
-	    'macro'		=> array( $m . '_src/_inc/_macro/_popup.form.testo.php' ),
-	    'auth'		=> array( 'groups'	=> array(	'roots', 'staff' ) ),
-		'etc'		=> array( 'tabs'	=> $p['popup.form']['etc']['tabs'] )
-	);
 	
 /*
     // azioni sulle pagine
