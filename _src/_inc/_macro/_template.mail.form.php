1828cc99623ce7d40f641dcc45673851909333d5
sophie <sophie.lacordais@hotmail.it>
2020-11-23 10:54:35 +0100

    implementato schema template.mail.form.html

--- _src/_inc/_macro/_template.mail.form.php
+++ _src/_inc/_macro/_template.mail.form.php
@@ -12,6 +12,11 @@
     // tabella gestita
 	$ct['form']['table'] = 'template_mail';
 
+    // tendina types
+	$ct['etc']['select']['types'] = array(
+	    array( 'id' => 'twig', '__label__' => 'template manager Twig' )
+	);
+
     // macro di default
 	require DIR_SRC_INC_MACRO . '_default.form.php';
 
