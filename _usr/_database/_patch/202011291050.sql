3e6e98c96399bc9657cb0c1e6442fc0fc2c60ea8
Fabio Mosti <fabio.mosti@gmail.com>
2020-11-29 16:09:42 +0100

    distribuzione patch database 202011291050.sql

---
+++ _usr/_database/_patch/202011291050.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `anagrafica_modalita_pagamento` (
+`id` int(11) NOT NULL,
+  `id_anagrafica` int(11) NOT NULL,
+  `id_modalita` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
