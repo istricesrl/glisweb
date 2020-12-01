2c3165d60893cdc96724710e88ba56ce91a9da62
Fabio Mosti <fabio.mosti@istricesrl.it>
2020-12-01 11:33:31 +0100

    aggiunta la possibilità di creare un utente per il database in mysql install

--- _src/_sh/_gw.mysql.install.sh
+++ _src/_sh/_gw.mysql.install.sh
@@ -22,9 +22,9 @@ if [ -f "$FILE1" -a -f "$FILE2" ]; then
 
     read -p "porta del server: " SRVPORT
 
-    read -p "nome utente: " SRVUSER
+    read -p "nome utente AMMINISTRATORE: " SRVUSER
 
-    read -p "password: " SRVPASS
+    read -p "password utente AMMINISTRATORE: " SRVPASS
 
     read -p "database: " SRVDBNAME
 
@@ -32,8 +32,21 @@ if [ -f "$FILE1" -a -f "$FILE2" ]; then
         PASSC="-p$SRVPASS"
     fi
 
+    mysql -h $SRVADDR -u $SRVUSER $PASSC -e "CREATE DATABASE IF NOT EXISTS \`$SRVDBNAME\` CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
     mysql -h $SRVADDR -u $SRVUSER $PASSC $SRVDBNAME < $FILE1
     mysql -h $SRVADDR -u $SRVUSER $PASSC $SRVDBNAME < $FILE2
+
+    read -p "vuoi creare un utente per il database (s/n)? " SYN
+
+    if [ $YN == "s" ]; then
+
+        read -p "nome utente DATABASE: " SRVDBUSER
+        read -p "password utente DATABASE: " SRVDBPASS
+
+        mysql -h $SRVADDR -u $SRVUSER $PASSC -e "CREATE USER \`$SRVDBUSER\`@\`%\` IDENTIFIED BY '$SRVDBPASS';"
+        mysql -h $SRVADDR -u $SRVUSER $PASSC -e "GRANT ALL PRIVILEGES ON \`$SRVDBNAME\`.* TO \`$SRVDBUSER\`@\`%\`;"
+
+    fi
 
     mkdir -p usr/database/
     cp _usr/_database/mysql.schema.version usr/database/mysql.schema.version
