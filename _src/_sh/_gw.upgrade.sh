1822afedec2bfd590290cb721f58abc8c73aff83
Fabio Mosti <fabio.mosti@istricesrl.it>
2020-12-01 11:54:00 +0100

    completata prima versione dello script di aggiornamento

--- _src/_sh/_gw.upgrade.sh
+++ _src/_sh/_gw.upgrade.sh
@@ -16,10 +16,46 @@ GITNAME=$( basename $(git remote get-url origin) )
 ## se sto lavorando sul framework
 if [ -n "$( echo $GITNAME | grep 'glisweb' )" ]; then
 
-    echo "stai lavorando sul framework, utilizza git per rimanere aggiornato"
+    echo "stai lavorando sul framework ($GITNAME), utilizza git per rimanere aggiornato"
+    echo
+    echo "NOTA: potresti ricevere questo messaggio anche se sei su un repository custom che"
+    echo "però contiene 'glisweb' nel nome; rinomina il tuo repository affinché non contenga"
+    echo "la parola riservata 'glisweb' nel nome"
 
 else
 
+    # output
     echo "stai lavorando su $GITNAME, aggiorno il framework"
+
+    # se è specificata una branch di aggiornamento
+    if [[ -z $1 ]]; then
+        BRANCH=master
+    else
+        BRANCH=$1
+    fi
+
+    # scarico Glisweb
+    wget https://github.com/istricesrl/glisweb/archive/$BRANCH.zip
+
+    # scompatto Glisweb
+    unzip ./$BRANCH.zip
+
+    # elimino il vecchio framework
+    rm -rf ./_*
+
+    # installo la nuova versione
+    rsync -a ./glisweb-$BRANCH/* ./
+
+    # elimino la vecchia cartella
+    rm -rf ./glisweb-$BRANCH
+    rm -rf ./$BRANCH.zip
+
+    # installo il .gitignore se è presente un repository .git
+    if [ -f ./_usr/_deploy/_git/.gitignore -a -d ./.git ]; then
+        cp ./_usr/_deploy/_git/.gitignore ./.gitignore
+    fi
+
+    # aggiorno composer
+    composer update
 
 fi
