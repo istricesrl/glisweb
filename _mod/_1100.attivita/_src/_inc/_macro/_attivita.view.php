ad999f36802439240a35560b7b69ea5167c684de
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2020-12-10 10:45:40 +0100

    aggiunti i filtri alla vista attività e aggiunto edit alle selctbox

--- _mod/_1100.attivita/_src/_inc/_macro/_attivita.view.php
+++ _mod/_1100.attivita/_src/_inc/_macro/_attivita.view.php
@@ -21,7 +21,10 @@
 	// print_r( $_SESSION );
 
     // tabella della vista
-	$ct['view']['table'] = 'attivita';
+    $ct['view']['table'] = 'attivita';
+    
+    // id della vista
+    $ct['view']['id'] = md5( $ct['view']['table'] );
 
     // pagina per la gestione degli oggetti esistenti
 	$ct['view']['open']['page'] = 'attivita.form';
@@ -50,7 +53,43 @@
         'nome' => 'text-left',
         '__label__' => 'text-left',
         'testo' => 'text-left no-wrap'
-	);
+    );
+    
+    // inclusione filtri speciali
+	$ct['etc']['include']['filters'] = 'inc/attivita.view.filters.html';
+
+    // tendina mesi
+	foreach( range( 1, 12 ) as $mese ) {
+	    $ct['etc']['select']['mesi'][$mese] =  int2month( $mese ) ;
+	}
+
+    // tendina anni
+	foreach( range( date( 'Y' ) - 5,  date( 'Y' ) ) as $y ) {
+	    $ct['etc']['select']['anni'][$y] = $y ;
+	}
+
+    // tendina operatori
+	$ct['etc']['select']['operatori'] = mysqlCachedQuery(
+        $cf['memcache']['connection'], 
+        $cf['mysql']['connection'], 
+        'SELECT id, __label__ FROM anagrafica_view WHERE se_interno = 1 OR se_collaboratore = 1');
+
+    // tendina clienti
+	$ct['etc']['select']['clienti'] = mysqlCachedQuery(
+        $cf['memcache']['connection'], 
+        $cf['mysql']['connection'], 
+        'SELECT id, __label__ FROM anagrafica_view WHERE se_interno = 1 OR se_cliente = 1');
+
+    // preset filtri custom
+	if( ! isset( $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['giorno']['EQ'] ) && ! isset( $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['anno']['EQ'] ) && ! isset( $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['mese']['EQ'] ) ) {
+	    $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['mese']['EQ'] = date('m');
+	    $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['anno']['EQ'] = date('yy');
+	    $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['giorno']['EQ'] = date('d');
+	}
+
+	if( ! isset( $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['id_anagrafica']['EQ'] ) && isset($_SESSION['account']['id_anagrafica'] ) ){
+	    $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['id_anagrafica']['EQ'] = $_SESSION['account']['id_anagrafica'] ;
+	}
 
     // macro di default
 	require DIR_SRC_INC_MACRO . '_default.view.php';
