04b1bf9d06dbb114ab8b359ef8616551c11c908a
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2020-12-11 16:53:12 +0100

    modificata dashboard produzione da view a macro

--- _mod/_1000.produzione/_src/_inc/_pages/_produzione.it-IT.php
+++ _mod/_1000.produzione/_src/_inc/_pages/_produzione.it-IT.php
@@ -12,7 +12,7 @@
 	    'title'			=> array( $l		=> 'produzione' ),
 	    'h1'			=> array( $l		=> 'produzione' ),
 	    'parent'		=> array( 'id'		=> NULL ),
-	    'template'		=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'default.view.html' ),
+	    'template'		=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'dashboard.produzione.html' ),
 	    'macro'			=> array( $m . '_src/_inc/_macro/_dashboard.produzione.php' ),
 	    'auth'			=> array( 'groups'	=> array(	'roots', 'staff' ) ),
 	    'menu'			=> array( 'admin'	=> array(	'label'		=> array( $l => 'produzione' ),
