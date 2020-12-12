496fe98db5766fe9c6024837c18f131041b1a66c
Fabio Mosti <fabio.mosti@istricesrl.it>
2020-12-12 13:01:05 +0100

    corrette patch 202012121228,30,32

--- _usr/_database/_patch/202012121138.sql
+++ _usr/_database/_patch/202012121138.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `metadati` ADD FOREIGN KEY (`id_video`) REFERENCES `__test__`.`video`(`id`) ON DELETE SET NULL ON UPDATE SET NULL;
+ALTER TABLE `metadati` ADD FOREIGN KEY (`id_video`) REFERENCES `video`(`id`) ON DELETE SET NULL ON UPDATE SET NULL;
--- _usr/_database/_patch/202012121228.sql
+++ _usr/_database/_patch/202012121228.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `immagini` ADD FOREIGN KEY (`id_lingua`) REFERENCES `__test__`.`lingue`(`id`) ON DELETE SET NULL ON UPDATE SET NULL;
+ALTER TABLE `immagini` ADD FOREIGN KEY (`id_lingua`) REFERENCES `lingue`(`id`) ON DELETE SET NULL ON UPDATE SET NULL;
