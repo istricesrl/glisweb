1eb2a7239b42cbb6db6cf955ab319138a60dd4ed
silvia-df <silvia.defalco@istricesrl.it>
2020-12-20 18:50:44 +0100

    inserito logiche per gestione cartellini

--- _mod/_1120.cartellini/_src/_inc/_macro/_cartellini.php
+++ _mod/_1120.cartellini/_src/_inc/_macro/_cartellini.php
@@ -18,4 +18,71 @@
      */
 
 
+	// tendina mesi
+	foreach( range( 1, 12 ) as $mese ) {
+	    $ct['etc']['select']['mesi'][ $mese ] =  int2month( $mese ) ;
+	}
+
+    // tendina anni
+	foreach( range( date( 'Y' ) - 5,  date( 'Y' ) ) as $y ) {
+	    $ct['etc']['select']['anni'][ $y ] = $y ;
+	}
+
+	// preset filtri custom
+	if( ! isset( $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['anno']['EQ'] ) && ! isset( $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['mese']['EQ'] ) ) {
+	    $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['mese']['EQ'] = date('m');
+	    $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['anno']['EQ'] = date('yy');
+	}
+
+    // tendina operatori
+	$ct['etc']['select']['operatori'] = mysqlCachedQuery(
+        $cf['memcache']['connection'], 
+        $cf['mysql']['connection'], 
+        'SELECT id, __label__ FROM anagrafica_view WHERE se_interno = 1 OR se_collaboratore = 1');
+	
+	// elenco tipologie attività
+	$ct['etc']['tipologie_attivita'] = mysqlCachedQuery(
+        $cf['memcache']['connection'], 
+        $cf['mysql']['connection'], 
+        'SELECT id, __label__ FROM tipologie_attivita_view');
+
+	// costruzione della griglia
+	if( isset( $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['mese']['EQ'] ) && isset( $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['anno']['EQ'] ) && isset( $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['id_anagrafica']['EQ'] ) && !empty( $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['id_anagrafica']['EQ'] ) ) 
+	{
+
+		// costruisco l'elenco giorni partendo da mese e anno
+		$giorni = array();
+		$mese = intval( $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['mese']['EQ'] );
+		$anno = intval( $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['anno']['EQ'] );
+		
+		for( $d=1; $d<=31; $d++ )
+		{
+			$time=mktime(12, 0, 0, $mese, $d, $anno);          
+			if (date('m', $time) == $mese){   
+				$giorni[] = intval( date('d', $time) );
+			}
+		}
+
+		// elenco delle date per il mese e l'anno specificati
+		foreach( $giorni as $giorno ){
+			$ct['etc']['ore'][ $giorno ]['data'] = date( 'd/m/Y', strtotime("$anno-$mese-$giorno") );	
+		}
+
+		// ricavo il riepilogo attività presenti
+		$attivita = mysqlQuery( $cf['mysql']['connection'], 
+		'SELECT giorno, id_tipologia, sum(ore) as tot_ore FROM attivita_view WHERE anno = ? AND mese = ? and id_anagrafica = ? GROUP by data, id_tipologia',
+		array(
+			array( 's' => $anno ),
+			array( 's' => $mese ),
+			array( 's' => $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['id_anagrafica']['EQ'] )
+		)			
+		);
+		
+		if( !empty( $attivita ) ){
+			foreach( $attivita as $a ){
+				$ct['etc']['ore'][ $a['giorno'] ]['tipologie'][ $a['id_tipologia'] ] = $a['tot_ore'];
+			}
+		}
+		
+	}
    
--- _mod/_1120.cartellini/_src/_inc/_pages/_cartellini.it-IT.php
+++ _mod/_1120.cartellini/_src/_inc/_pages/_cartellini.it-IT.php
@@ -11,13 +11,13 @@
 	    'sitemap'		=> false,
 	    'title'		=> array( $l		=> 'cartellini' ),
 	    'h1'		=> array( $l		=> 'cartellini' ),
-	    'parent'		=> array( 'id'		=> NULL ),
+	    'parent'		=> array( 'id'		=> 'produzione' ),
 	    'template'		=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'cartellini.html' ),
 	    'macro'		=> array( $m . '_src/_inc/_macro/_cartellini.php' ),
 	    'auth'		=> array( 'groups'	=> array(	'roots', 'staff' ) ),
 	    'etc'		=> array( 'tabs'	=> array(	'cartellini' ) ),
 	    'menu'		=> array( 'admin'	=> array(	'label'		=> array( $l => 'cartellini' ),
-									'priority'	=> '205' ) )
+									'priority'	=> '105' ) )
     );
 
 
