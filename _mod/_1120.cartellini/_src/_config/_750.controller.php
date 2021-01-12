fc233de490e5c68e6b8ae6284d26332aa05e9d18
silvia-df <silvia.defalco@istricesrl.it>
2021-01-12 10:08:56 +0100

    cartellini, migliorie array giorni

--- _mod/_1120.cartellini/_src/_config/_750.controller.php
+++ _mod/_1120.cartellini/_src/_config/_750.controller.php
@@ -27,32 +27,33 @@ if( isset( $_REQUEST['__cartellini__'] ) ){
         appendToFile('mese: ' . $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['mese']['EQ'] . ', anno: ' .  $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['anno']['EQ'] . ', operatore: ' . $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['id_anagrafica']['EQ'] . PHP_EOL, 'var/log/cartellini.log');
         appendToFile( print_r( $_REQUEST['__cartellini__'], true ) . PHP_EOL, 'var/log/cartellini.log');
 
-        
-        foreach( $_REQUEST['__cartellini__'] as $k => $v ){
+        if( isset( $_REQUEST['__cartellini__']['giorni'] ) && is_array( $_REQUEST['__cartellini__']['giorni'] )  ){
+            foreach( $_REQUEST['__cartellini__']['giorni'] as $k => $v ){
 
-            // se ho un orario valido
-            if( $v > 0 ){
-                $s = explode("_",$k);
+                // se ho un orario valido
+                if( $v > 0 ){
+                    $s = explode("_",$k);
 
-                $giorno = $s[0];
-                $id_tipologia_inps = $s[1];
-                $mese = $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['mese']['EQ'];
-                $anno =  $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['anno']['EQ'];
-                $data = $anno . '/' . $mese . '/' . $giorno;
+                    $giorno = $s[0];
+                    $id_tipologia_inps = $s[1];
+                    $mese = $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['mese']['EQ'];
+                    $anno =  $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['anno']['EQ'];
+                    $data = $anno . '/' . $mese . '/' . $giorno;
 
-                appendToFile('data: ' . $data . ', id_tipologia_inps: ' . $id_tipologia_inps . ', ore: ' . $v . PHP_EOL, 'var/log/cartellini.log');
-        
-                mysqlQuery( $cf['mysql']['connection'], 'INSERT INTO attivita (data, id_anagrafica, id_tipologia_inps, ore, nome,  id_account_inserimento, timestamp_inserimento ) VALUES( ?, ?, ?, ?, ?, ?, ? )',
-                    array( 
-                        array( 's' =>  $data ),
-                        array( 's' =>  $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['id_anagrafica']['EQ'] ),
-                        array( 's' =>  $id_tipologia_inps ),
-                        array( 's' =>  str_replace( ",", ".", $v ) ),
-                        array( 's' => 'attività del '.$data),
-                        array( 's' => ( isset( $_SESSION['account']['id']) && is_numeric($_SESSION['account']['id']) ?  $_SESSION['account']['id'] : NULL ) ),
-                        array( 's' => time() )
-                    )
-                );
+                    appendToFile('data: ' . $data . ', id_tipologia_inps: ' . $id_tipologia_inps . ', ore: ' . $v . PHP_EOL, 'var/log/cartellini.log');
+            
+                    mysqlQuery( $cf['mysql']['connection'], 'INSERT INTO attivita (data, id_anagrafica, id_tipologia_inps, ore, nome,  id_account_inserimento, timestamp_inserimento ) VALUES( ?, ?, ?, ?, ?, ?, ? )',
+                        array( 
+                            array( 's' =>  $data ),
+                            array( 's' =>  $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['id_anagrafica']['EQ'] ),
+                            array( 's' =>  $id_tipologia_inps ),
+                            array( 's' =>  str_replace( ",", ".", $v ) ),
+                            array( 's' => 'attività del '.$data),
+                            array( 's' => ( isset( $_SESSION['account']['id']) && is_numeric($_SESSION['account']['id']) ?  $_SESSION['account']['id'] : NULL ) ),
+                            array( 's' => time() )
+                        )
+                    );
+                }
             }
         }
 
