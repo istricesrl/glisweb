c05324b55c7cf427c31938ac78c3d095d2aa0122
Fabio Mosti <fabio.mosti@istricesrl.it>
2021-01-14 13:41:13 +0100

    migliorato script aggiornamento db per includere anche i dati standard

--- _src/_sh/_gw.mysql.upgrade.sh
+++ _src/_sh/_gw.mysql.upgrade.sh
@@ -18,11 +18,12 @@ DSTDIR="usr/database/$(date '+%Y%m%d%H%M')/"
 mkdir -p $DSTDIR
 
 ## file di destinazione
-FILE1=$DSTDIR"mysql.schema.sql"
 FILE2=$DSTDIR"mysql.data.sql"
+FILE3=$DSTDIR"mysql.schema.sql"
 
 ## file sorgente
 FILE0="_usr/_database/mysql.schema.sql"
+FILE1="_usr/_database/mysql.data.sql"
 
 ## file temporaneo
 FILEC=$FILE1".clean"
@@ -46,16 +47,17 @@ if [ -f "$FILE0" ]; then
         PASSC="-p$SRVPASS"
     fi
 
-    mysqldump -h $SRVADDR -u $SRVUSER $PASSC --no-data --opt --routines --single-transaction --events $SRVDBNAME > $FILE1
-    mysqldump -h $SRVADDR -u $SRVUSER $PASSC --no-create-info --opt --single-transaction $SRVDBNAME > $FILE2
+    mysqldump -h $SRVADDR -u $SRVUSER $PASSC --no-create-info --replace --opt --single-transaction $SRVDBNAME > $FILE2
+    mysqldump -h $SRVADDR -u $SRVUSER $PASSC --no-data --opt --routines --single-transaction --events $SRVDBNAME > $FILE3
 
-    cat $FILE1 | sed -E 's/DEFINER=`[a-z]+`@`[a-z0-9\.%]+`/DEFINER=CURRENT_USER()/g' | sed 's/ AUTO_INCREMENT=[0-9]*\b//g' > $FILEC
-	mv $FILEC $FILE1
+    cat $FILE3 | sed -E 's/DEFINER=`[a-z]+`@`[a-z0-9\.%]+`/DEFINER=CURRENT_USER()/g' | sed 's/ AUTO_INCREMENT=[0-9]*\b//g' > $FILEC
+	mv $FILEC $FILE3
 
     mysql -h $SRVADDR -u $SRVUSER $PASSC -e "DROP DATABASE \`$SRVDBNAME\`;"
     mysql -h $SRVADDR -u $SRVUSER $PASSC -e "CREATE DATABASE \`$SRVDBNAME\` CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
     mysql -h $SRVADDR -u $SRVUSER $PASSC -e "GRANT ALL PRIVILEGES ON \`$SRVDBNAME\`.* TO \`$SRVDBUSER\`@\`%\`;"
     mysql -h $SRVADDR -u $SRVUSER $PASSC $SRVDBNAME < $FILE0
+    mysql -h $SRVADDR -u $SRVUSER $PASSC $SRVDBNAME < $FILE1
     mysql -h $SRVADDR -u $SRVUSER $PASSC $SRVDBNAME < $FILE2
 
 else
