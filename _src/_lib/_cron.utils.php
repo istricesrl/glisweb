85fb251b6503fbc260fc0c117f2d11841577beb9
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-01-15 18:52:13 +0100

    aggiunta funzione creazionePianificazione per la creazione di un array di date a partire da una data di inizio e dei criteri di pianificazione successiva

--- _src/_lib/_cron.utils.php
+++ _src/_lib/_cron.utils.php
@@ -190,3 +190,111 @@
         return $attivita;
 
     }
+
+
+    // funzione per la creazione di un'array di date pianificate in base a criteri specifici
+    function creazionePianificazione( $c, $data, $id_periodicita, $cadenza, $data_fine=NULL, $numero_ripetizioni=1, $giorni_settimana=NULL,$ripetizione_mese=1, $ripetizione_anno=1 ){ 
+
+        // TODO controlli
+               // la data inizio è successiva alla data fine
+
+        $number = ['first', 'second', 'third', 'fourth','fifth','sixth'];
+        $days = ['Monday', 'Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday' ];
+        $months = ['January','February','March','April','May','June','July','August','September','October','November','December' ];
+        $attivita[] = array();
+            // in base al tipo di periodicità della pianificazione vengono generate le attività
+        switch($id_periodicita){
+    
+            // l'attività non si ripete
+                case 0:
+                    $attivita[] = $data;
+    
+                break;
+    
+                // attività con ripetizione giornaliera
+                case 1:
+                    
+                    if ( empty($data_fine) || $data_fine === NULL ){ $data_fine = date('Y-m-d', strtotime($data. ' + '.$cadenza * ($numero_ripetizioni - 1).' days')); }
+                    do {
+                        $attivita[] = $data;
+                        // aggiorno la data con la successiva
+                        $data = date('Y-m-d', strtotime($data. ' + '.$cadenza.' days'));
+    
+                    } while ( $data <= $data_fine );
+                
+                break;
+    
+                // attività con ripetizione settimanale
+                case 2:
+                    // lunedì della settimana di inizio
+                    $d_inizio = date('Y-m-d',strtotime('monday this week ', strtotime($data) ));
+                    if ( empty($data_fine) || $data_fine === NULL ){ $data_fine = date('Y-m-d', strtotime($d_inizio. ' + '.($cadenza * $numero_ripetizioni ).' weeks -1 day')); }
+                    $giorni = explode(",",$giorni_settimana);
+                    foreach($giorni as $g){
+                        if( date('N', strtotime($data)) - 1 == $g ){
+                            $d = $data;
+                        } else {
+                            $d = date('Y-m-d',strtotime(' next '.$days[$g], strtotime($d_inizio) ));
+                        }
+                        do {
+                        if($d >= $data){
+                            $attivita[] =$d;
+                        }
+                        // aggiorno la data con la successiva
+                        $d = date('Y-m-d', strtotime($d. ' + '.$cadenza.' weeks'));
+                        
+                        } while ( $d <= $data_fine );
+                    }
+                break;
+    
+                // attività con ripetizione mensile
+                case 3:
+                    if ( empty($data_fine) || $data_fine === NULL ){ 
+                        $data_fine = date('Y-m-d', strtotime($data. ' + '.$cadenza * $numero_ripetizioni .' months ')); 
+                        $data_fine = date(date('Y',strtotime($data_fine))."-".date('m',strtotime($data_fine))."-01");}
+                                        
+                if( $ripetizione_mese != 1 ){
+                    $n_g = numOfDayInWeek($data, $days[ date('N', strtotime($data)) - 1 ]);
+                    while ( $data < $data_fine ){
+                        
+                        $attivita[] = $data;
+                        $data_temp = date("Y-m-d", strtotime("+ ".$cadenza." month", strtotime($data)));
+                        $data_temp = date(date('Y',strtotime($data_temp))."-".date('m',strtotime($data_temp))."-01");
+                        $data_temp = date("Y-m-d", strtotime($number[ $n_g -1 ]." ".$days[ date('N', strtotime($data))-1 ], strtotime($data_temp." -1 day")));
+                        
+                        if( date('m', strtotime($data_temp)) != ((date('m',  strtotime($data)) + $cadenza) % 12 ) ){
+                            $data_temp = date("Y-m-d", strtotime("last ".$days[ date('N', strtotime($data))-1 ], strtotime($data_temp)));   
+                        }
+                        
+                        $data = $data_temp;
+    
+                    };
+                } else {
+                    do {
+                        $attivita[] = $data;
+                        // aggiorno la data con la successiva
+                        $data = date('Y-m-d', strtotime($data. ' + '.$cadenza.' months'));
+    
+                    } while ( $data <= $data_fine );
+                }   
+                break;
+    
+                // attività con ripetizione annuale
+                // TODO gestione seconda tipologia di duplicazione data
+                case 4:
+                    if ( empty($data_fine) || $data_fine === NULL ){ $data_fine = date('Y-m-d', strtotime($data. ' + '.$cadenza * ($numero_ripetizioni - 1).' years')); }
+                    do {
+                        $attivita[] = $data;
+                        // aggiorno la data con la successiva
+                        $data = date('Y-m-d', strtotime($data. ' + '.$cadenza.' years'));
+    
+                    } while ( $data < $data_fine );
+    
+                break;
+    
+    
+            }
+    
+            return $attivita;
+    
+        }
