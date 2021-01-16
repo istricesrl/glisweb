95d123db47cb4bf20e6075d347718723f6578089
Fabio Mosti <fabio.mosti@istricesrl.it>
2021-01-16 16:51:38 +0100

    correzione patch database

--- _usr/_database/_patch/202001161648.sql
+++
@@ -1,1 +0,0 @@
-UPDATE `__glisweb__`.`ruoli_audio` SET `se_anagrafica` = '1', `se_contenuti` = '1', `se_categorie_prodotti` = '1' WHERE `ruoli_audio`.`id` = 1; UPDATE `__glisweb__`.`ruoli_audio` SET `se_contenuti` = '1' WHERE `ruoli_audio`.`id` = 2;
