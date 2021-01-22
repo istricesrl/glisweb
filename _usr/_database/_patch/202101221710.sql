1245b402304d05c12d3348d92b91a95d8b717a5e
Fabio Mosti <fabio.mosti@gmail.com>
2021-01-22 17:10:55 +0100

    distribuzione patch database 202101221710.sql

---
+++ _usr/_database/_patch/202101221710.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `attivita` DROP FOREIGN KEY `attivita_ibfk_6_nofollow`; ALTER TABLE `attivita` ADD CONSTRAINT `attivita_ibfk_6_nofollow` FOREIGN KEY (`id_pratica`) REFERENCES `pratiche`(`id`) ON DELETE SET NULL ON UPDATE SET NULL;
