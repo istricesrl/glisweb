d7f269d3e27240a4a08749ae6f5cc34df026399d
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-01-22 18:15:53 +0100

    eliminata patch errata

--- _usr/_database/_patch/202101221710.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `attivita` DROP FOREIGN KEY `attivita_ibfk_6_nofollow`; ALTER TABLE `attivita` ADD CONSTRAINT `attivita_ibfk_6_nofollow` FOREIGN KEY (`id_pratica`) REFERENCES `pratiche`(`id`) ON DELETE SET NULL ON UPDATE SET NULL;
