f3dea95bea1838e3ba803a8251dd10fbee770cc8
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-01-25 18:47:51 +0100

    aggiunta autorizzazione a tabella todo nel modulo produzione

--- _mod/_1000.produzione/_src/_config/_250.auth.php
+++ _mod/_1000.produzione/_src/_config/_250.auth.php
@@ -22,6 +22,10 @@
 		'task' => array(
 		    CONTROL_FULL => array( 'roots' ),
             CONTROL_FILTERED => array( 'staff' )
+        ),
+        'todo' => array(
+		    CONTROL_FULL => array( 'roots' ),
+            CONTROL_FILTERED => array( 'staff' )
         )
         )
 	);
