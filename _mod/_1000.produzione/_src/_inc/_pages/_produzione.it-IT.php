856197915ba0872c24eefac565e3dca611a3137e
silvia-df <silvia.defalco@istricesrl.it>
2021-02-22 20:50:29 +0100

    aggiunto progetti.produzione.form.todo

--- _mod/_1000.produzione/_src/_inc/_pages/_produzione.it-IT.php
+++ _mod/_1000.produzione/_src/_inc/_pages/_produzione.it-IT.php
@@ -43,8 +43,34 @@
 	    'template'		=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'progetti.produzione.form.html' ),
 	    'macro'			=> array( $m.'_src/_inc/_macro/_progetti.produzione.form.php' ),
 	    'auth'			=> array( 'groups'	=> array(	'roots', 'staff' ) ),
+		'etc'			=> array( 'tabs'	=> array(	'progetti.produzione.form', 
+														'progetti.produzione.form.todo' ) )
+	);
+
+	// gestione todo progetti
+	$p['progetti.produzione.form.todo'] = array(
+	    'sitemap'		=> false,
+	    'title'			=> array( $l		=> 'todo' ),
+	    'h1'			=> array( $l		=> 'to-do' ),
+	    'parent'		=> array( 'id'		=> 'progetti.produzione.view' ),
+	    'template'		=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'progetti.produzione.form.todo.html' ),
+	    'macro'			=> array( $m.'_src/_inc/_macro/_progetti.produzione.form.todo.php' ),
+	    'auth'			=> array( 'groups'	=> array(	'roots', 'staff' ) ),
+		'etc'			=> array( 'tabs'	=> $p['progetti.produzione.form']['etc']['tabs'] )
+	);
+
+	// gestione attività progetti
+/*	$p['progetti.produzione.form.attivita'] = array(
+	    'sitemap'		=> false,
+	    'title'			=> array( $l		=> 'attivita' ),
+	    'h1'			=> array( $l		=> 'attività' ),
+	    'parent'		=> array( 'id'		=> 'progetti.produzione.view' ),
+	    'template'		=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'progetti.produzione.form.attivita.html' ),
+	    'macro'			=> array( $m.'_src/_inc/_macro/_progetti.produzione.form.attivita.php' ),
+	    'auth'			=> array( 'groups'	=> array(	'roots', 'staff' ) ),
 		'etc'			=> array( 'tabs'	=> array(	'progetti.produzione.form' ) )
 	);
+*/
 
 	
 
