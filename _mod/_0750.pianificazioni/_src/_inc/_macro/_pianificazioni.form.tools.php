ba43b1fd20421dcd93f1d5dda3789acb4518b2b4
silvia-df <silvia.defalco@istricesrl.it>
2021-02-23 20:08:58 +0100

    concluso tools pianificazioni

--- _mod/_0750.pianificazioni/_src/_inc/_macro/_pianificazioni.form.tools.php
+++ _mod/_0750.pianificazioni/_src/_inc/_macro/_pianificazioni.form.tools.php
@@ -23,9 +23,18 @@
 	    'modal' => array('id' => 'pulisci', 'include' => 'inc/pianificazioni.form.tools.modal.pulisci.html' ),
         'icon' => NULL,
 	    'fa' => 'fa-eraser',
-	    'title' => 'rimuovi oggetti non conformi',
-	    'text' => 'rimuove gli oggetti già esistenti non più conformi'
+	    'title' => 'rimuovi oggetti futuri',
+	    'text' => 'rimuove gli oggetti futuri già esistenti in caso di modifica'
 	);
+
+    // modal per fermare la pianificazione originaria
+    $ct['page']['contents']['metro']['azioni'][] = array(
+        'modal' => array('id' => 'ferma', 'include' => 'inc/pianificazioni.form.tools.modal.ferma.html' ),
+        'icon' => NULL,
+        'fa' => 'fa-archive',
+        'title' => 'ferma pianificazione',
+        'text' => 'interrompe la pianificazione'
+    );
 
     // gestione default
 	require DIR_SRC_INC_MACRO . '_default.tools.php';
