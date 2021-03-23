582e349265579c8c4c796438177000c557330482
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-03-23 17:12:49 +0100

    creata scheda di gestione documenti_articoli con dettaglio riga

--- _mod/_6200.documenti/_src/_inc/_macro/_documenti.articoli.form.php
+++ _mod/_6200.documenti/_src/_inc/_macro/_documenti.articoli.form.php
@@ -22,5 +22,97 @@
     // tabella gestita
 	$ct['form']['table'] = 'documenti_articoli';
 
+       // tendina articoli
+	$ct['etc']['select']['id_articoli'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM articoli_view'
+	);
+
+    // tendina udm
+	$ct['etc']['select']['id_udm'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM udm_view'
+	);
+
+    // tendina iva
+	$ct['etc']['select']['id_iva'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM iva_view '
+	);
+
+    // tendina valute
+	$ct['etc']['select']['id_valute'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM valute_view '
+	);
+
+	// tendina listini
+	$ct['etc']['select']['id_listini'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM listini_view '
+	);
+
+	// tendina mastri
+	$ct['etc']['select']['id_mastri'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM mastri_view '
+	);
+
+	// tendina progetti
+	$ct['etc']['select']['id_progetti'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM progetti_view '
+	);
+
+        // tendina tipologie anagrafica
+	$ct['etc']['select']['tipologie_documenti_articoli'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM tipologie_documenti_articoli_view'
+	);
+
+    // tendina mittenti
+	$ct['etc']['select']['id_emittenti'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM anagrafica_view WHERE se_azienda_gestita = 1'
+	);
+
+    // tendina destinatari
+	$ct['etc']['select']['id_destinatari'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM anagrafica_view WHERE se_cliente = 1'
+	);
+
+    if( isset( $_REQUEST[ $ct['form']['table'][ 'id_emittente' ] ] ) && ! empty( $_REQUEST[ $ct['form']['table'][ 'id_emittente' ] ] ) ){
+        $ct['etc']['select']['id_documentii'] = mysqlCachedIndexedQuery(
+            $cf['memcache']['index'],
+            $cf['memcache']['connection'],
+            $cf['mysql']['connection'],
+            'SELECT id, __label__ FROM documenti_view'
+        );
+    } else {
+
+    }
+
+
 	// macro di default
 	require DIR_SRC_INC_MACRO . '_default.form.php';
