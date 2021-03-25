813a3afb3e8a22f486d61aa4f39d20d754369f17
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-03-25 10:56:06 +0100

    aggiunta a subview categorie_prodotti creazione nuovo oggetto e apertura oggetto esistente

--- _mod/_4000.catalogo/_src/_inc/_macro/_categorie.prodotti.form.prodotti.php
+++ _mod/_4000.catalogo/_src/_inc/_macro/_categorie.prodotti.form.prodotti.php
@@ -17,24 +17,33 @@
     // tabella della vista
 	$ct['view']['table'] = 'prodotti_categorie';
 
-    // pagina per la gestione degli oggetti esistenti
-#	$ct['view']['open']['page'] = 'prodotti.form';
-#   $ct['view']['open']['table'] = 'prodotti';
-#    $ct['view']['open']['field'] = 'id_prodotto';
-
     // campi della vista
 	$ct['view']['cols'] = array(
 	    'id' => '#',
-	    '__label__' => 'prodotto',
-        'id_categoria' => 'id_categoria'
+	    'prodotto' => 'prodotto',
+        'id_categoria' => 'id_categoria',
+        'id_prodotto' => 'id_prodotto'
 	);
 
     // stili della vista
 	$ct['view']['class'] = array(
 	    'id' => 'd-none d-md-table-cell',
-	    '__label__' => 'text-left',
-	    'id_categoria' => 'd-none'
+	    'prodotto' => 'text-left',
+	    'id_categoria' => 'd-none',
+        'id_prodotto' => 'd-none'
 	);
+
+    // pagina per la gestione degli oggetti esistenti
+	$ct['view']['open']['page'] = 'prodotti.form';
+    $ct['view']['open']['table'] = 'prodotti';
+    $ct['view']['open']['field'] = 'id_prodotto';
+
+    // pagina per l'inserimento di un nuovo oggetto
+	$ct['view']['insert']['page'] = 'prodotti.form';
+
+        // campo per il preset di apertura
+	$ct['view']['open']['preset']['field'] = 'id_categoria';
+	$ct['view']['open']['preset']['subform'] = 'prodotti_categorie';
 
     // preset filtro custom progetti aperti
 	$ct['view']['__restrict__']['id_categoria']['EQ'] = $_REQUEST[ $ct['form']['table'] ]['id'];
