db2dcdb7444403aacff79a618adb9c42d9c0104c
silvia-df <silvia.defalco@istricesrl.it>
2021-03-25 12:55:54 +0100

    varie

--- _mod/_1140.variazioni/_src/_inc/_macro/_variazioni.form.approvazione.php
+++ _mod/_1140.variazioni/_src/_inc/_macro/_variazioni.form.approvazione.php
@@ -22,16 +22,21 @@
     // tabella gestita
 	$ct['form']['table'] =  'variazioni_attivita';
 
-    print_r( $_REQUEST[ $ct['form']['table'] ] );
+//    print_r( $_REQUEST[ $ct['form']['table'] ] );
 
     // se ho un operatore e dei range devo andare a vedere tutte le attività che entrano nei range
     if( isset( $_REQUEST[ $ct['form']['table'] ]['id_anagrafica'] ) && !empty( $_REQUEST[ $ct['form']['table'] ]['periodi_variazioni_attivita'] ) ) {
         
         // per ogni riga di periodo devo vedere se ci sono attività che hanno data e ora programmazione in quel range, completamente o parzialmente
         // se sì aggiungo il cantiere e la data all'array dei risultati da mostrare in dashboard
-        // nota: come gestiamo le date?
+        // nota: come gestiamo le date? la dashboard deve mostrarmi solo le date interessate? se sono troppe (es. uno va in aspettativa o in maternità 3 mesi) e non ci stanno?
+        
         foreach( $_REQUEST[ $ct['form']['table'] ]['periodi_variazioni_attivita'] as $p ){
-
+            // 1- prendo la data
+            // 2- vado a vedere se c'è un turno attivo per quella data
+            // 3- vado nel contratto attivo e vedo per quel turno che orari sono previsti
+            // 4- creo una riga di attività con la tipologia inps indicata per ogni fascia di orari_contratti trovata
+            // 5- vedo se ci sono attività già pianificate per quella fascia di data e ora e setto id_anagrafica NULL
         }
 
 
