2f27fb371498e50e5c5f42f31e812935e250d9a5
silvia-df <silvia.defalco@istricesrl.it>
2021-03-29 11:57:42 +0200

    creato vista sostituzioni.view

---
+++ _mod/_1140.variazioni/_src/_inc/_macro/_sostituzioni.view.php
@@ -0,0 +1,51 @@
+<?php
+
+    /**
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     * @todo finire di documentare
+     *
+     * @file
+     *
+     */
+
+
+    // tabella della vista
+    $ct['view']['table'] = 'sostituzioni_attivita';
+    
+    // id della vista
+    $ct['view']['id'] = md5( $ct['view']['table'] );
+
+
+    // tendina operatori
+	$ct['etc']['select']['operatori'] = mysqlCachedQuery(
+        $cf['memcache']['connection'], 
+        $cf['mysql']['connection'], 
+        'SELECT id_anagrafica as id, anagrafica as __label__ FROM sostituzioni_attivita_view'
+    );
+
+    if( isset( $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['id_anagrafica']['EQ'] ) && !empty( $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['id_anagrafica']['EQ'] ) ) 
+	{
+        $ct['etc']['sostituzioni'] = mysqlQuery(
+            $cf['mysql']['connection'],
+            'SELECT * FROM sostituzioni_attivita_view WHERE id_anagrafica = ?',
+            array(
+                array( 's' => $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['id_anagrafica']['EQ'] )
+            )
+        );
+
+    }
+
+    
+    // macro di default
+#	require DIR_SRC_INC_MACRO . '_default.view.php';
+
+   
