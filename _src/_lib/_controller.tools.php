1130d5e4af9faf09f8c5e6b3050b26d9029aaa5d
Fabio Mosti <fabio.mosti@istricesrl.it>
2021-04-18 16:52:22 +0200

    debug

--- _src/_lib/_controller.tools.php
+++ _src/_lib/_controller.tools.php
@@ -510,7 +510,7 @@
 				    foreach( $d as $k => $v ) {
 					if( is_array( $v ) ) {
 					    foreach( $v as $x => $y ) {
-						controller( $c, $mc, $d[$k][$x], $k, $a, $d['id'], $e, $i[$k][$x], $i['__auth__'], $pi, $ci );
+						controller( $c, $mc, $d[$k][$x], $k, $a, $d['id'], $e, $i[$k][$x], $i['__auth__'] );
 					    }
 					}
 				    }
@@ -519,6 +519,7 @@
 #				default:
 				    if( in_array( 'id', $ks ) ) {
 					$x = mysqlCachedQuery( $mc, $c, 'SELECT * FROM information_schema.key_column_usage WHERE referenced_table_name = ? AND constraint_name NOT LIKE "%_nofollow" AND table_schema = database()', array( array( 's' => $t ) ) );
+#C					$x = mysqlQuery( $c, 'SELECT * FROM information_schema.key_column_usage WHERE referenced_table_name = ? AND constraint_name NOT LIKE "%_nofollow" AND table_schema = database()', array( array( 's' => $t ) ) );
 #echo "cerco le referenze a $t" . PHP_EOL;
 #print_r( $x );
 					foreach( $x as $ref ) {
@@ -547,7 +548,7 @@
 						    $e[ $ref['TABLE_NAME'] ][ $ix ] = array();
 						    $i[ $ref['TABLE_NAME'] ][ $ix ] = array();
 //						    controller( $c, $d[ $ref['TABLE_NAME'] ][ $ix ], $ref['TABLE_NAME'], $a, NULL, $r, $e[ $ref['TABLE_NAME'] ][ $ix ], $i[ $ref['TABLE_NAME'] ][ $ix ] );
-						    controller( $c, $mc, $d[ $ref['TABLE_NAME'] ][ $ix ], $ref['TABLE_NAME'], $a, NULL, $e[ $ref['TABLE_NAME'] ][ $ix ], $i[ $ref['TABLE_NAME'] ][ $ix ], $i['__auth__'], $pi, $ci );
+						    controller( $c, $mc, $d[ $ref['TABLE_NAME'] ][ $ix ], $ref['TABLE_NAME'], $a, NULL, $e[ $ref['TABLE_NAME'] ][ $ix ], $i[ $ref['TABLE_NAME'] ][ $ix ], $i['__auth__'] );
 						    $ix++;
 						}
 					    }
