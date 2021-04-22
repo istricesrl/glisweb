78674a1cac42968529897f2a845135214d831662
silvia-df <silvia.defalco@istricesrl.it>
2021-04-22 21:36:42 +0200

    url encode back e rollback su cancellazione

--- _src/_api/_pages.php
+++ _src/_api/_pages.php
@@ -24,6 +24,7 @@
 	require '../_config.php';
 
     // debug
+	// die('inizio api pages');
 	// ini_set( 'display_errors', 1 );
 	// ini_set( 'display_startup_errors', 1 );
 	// error_reporting( E_ALL );
