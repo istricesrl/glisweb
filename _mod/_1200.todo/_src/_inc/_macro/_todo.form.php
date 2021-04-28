e23513fce8c1bc923572affd4ab0129cea9570db
silvia-df <silvia.defalco@istricesrl.it>
2021-04-28 13:00:25 +0200

    tendina progetti in todo form legge tutti i progetti

--- _mod/_1200.todo/_src/_inc/_macro/_todo.form.php
+++ _mod/_1200.todo/_src/_inc/_macro/_todo.form.php
@@ -53,7 +53,7 @@
 	    $cf['cache']['index'],
 	    $cf['memcache']['connection'],
         $cf['mysql']['connection'], 
-        'SELECT id, __label__ FROM progetti_produzione_view' );
+        'SELECT id, __label__ FROM progetti_view' );
 
      // tendina anni
 	foreach( range( date( 'Y' ) + 1, 2017 ) as $y ) {
