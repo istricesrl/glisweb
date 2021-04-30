8f19687c9a0db2f60b4bc138d1dc42c054774025
Francesca Petrachi <francesca.petrachi@istricesrl.it>
2021-04-30 16:55:48 +0200

    aggiunta sezione prodotti recenti, stile etichette

--- _mod/_6500.casse/_src/_inc/_macro/_terminale.php
+++ _mod/_6500.casse/_src/_inc/_macro/_terminale.php
@@ -1,1 +1,51 @@
 <?php
+
+    /**
+     * macro form anagrafica
+     *
+     *
+     *
+     * -# definizione della tabella del modulo
+     * -# popolazione delle tendine
+     *
+     *
+     *
+     *
+     *
+     *
+     * @todo documentare
+     *
+     * @file
+     *
+     */
+
+    // tabella gestita
+	$ct['form']['table'] = 'documenti';
+
+    // tendina tipologie documenti
+	$ct['etc']['select']['tipologie_documenti'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM tipologie_documenti_view'
+	);
+
+
+    // tendina  anagrafica
+	$ct['etc']['select']['anagrafica'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM anagrafica_view'
+	);
+
+    // articoli recenti
+	$ct['etc']['articoli_frequenti'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT articoli_view.id, articoli_view.__label__, contenuti.testo FROM articoli_view '.
+        'LEFT JOIN contenuti ON contenuti.id_articolo = articoli_view.id AND contenuti.id_lingua = 1'
+	);
+    
+    
