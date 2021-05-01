d5b2c07eeeaed329b0a7eba031e54f7d47a31ba6
silvia-df <silvia.defalco@istricesrl.it>
2021-05-01 14:41:24 +0200

    filtro vista progetti produzione

--- _mod/_1000.produzione/_src/_inc/_macro/_progetti.produzione.view.php
+++ _mod/_1000.produzione/_src/_inc/_macro/_progetti.produzione.view.php
@@ -21,6 +21,9 @@
     // tabella della vista
 	$ct['view']['table'] = 'progetti_produzione';
 
+    // id della vista
+    $ct['view']['id'] = md5( $ct['view']['table'] );
+
     // tabella per la gestione degli oggetti esistenti
 	$ct['view']['open']['table'] = 'progetti';
 
@@ -40,6 +43,26 @@
         'cliente' => 'text-left d-none d-md-table-cell',
         '__label__' => 'text-left'
     );
+
+    // inclusione filtri speciali
+	$ct['etc']['include']['filters'] = 'inc/progetti.produzione.view.filters.html';
+
+    // tendina clienti
+	$ct['etc']['select']['id_cliente'] = mysqlCachedQuery(
+        $cf['memcache']['connection'], 
+        $cf['mysql']['connection'], 
+        'SELECT id, __label__ FROM anagrafica_view WHERE se_interno = 1 OR se_cliente = 1');
+
+	// tendina tipologie
+	$ct['etc']['select']['tipologie'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+        $cf['mysql']['connection'], 'SELECT id, __label__ FROM tipologie_progetti_view' );
+
+    // preset filtro aperti
+	if( ! isset( $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['chiuso']['EQ'] ) ) {
+	    $_REQUEST['__view__'][ $ct['view']['id'] ]['__filters__']['chiuso']['EQ'] = 0;
+    }
  
 
     // macro di default
