99270d6a7acdfb1c1bdba8e4486528d0f2413ca0
RLeontine <83805081+RLeontine@users.noreply.github.com>
2021-05-10 15:17:07 +0200

    Update _000001000999.patch.sql
    aggiornamento tabelle fino a articoli assicurazioni_montaggio

--- _usr/_database/_patch/_000001000999.patch.sql
+++ _usr/_database/_patch/_000001000999.patch.sql
@@ -139,4 +139,126 @@ CREATE TABLE IF NOT EXISTS `anagrafica_cittadinanze` (
   `data_fine` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--| 000001000008
+
+-- anagrafica_condizioni_pagamento
+-- tipologia: tabella gestita
+CREATE TABLE IF NOT EXISTS `anagrafica_condizioni_pagamento` (
+`id` int(11) NOT NULL,
+  `id_anagrafica` int(11) NOT NULL,
+  `id_condizione` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--| 000001000009
+
+-- anagrafica_indirizzi
+-- tipologia: tabella gestita
+CREATE TABLE IF NOT EXISTS `anagrafica_indirizzi` (
+`id` int(11) NOT NULL,
+  `id_indirizzo` int(11) NOT NULL,
+  `id_anagrafica` int(11) NOT NULL,
+  `id_tipologia` int(11) DEFAULT NULL,
+  `interno` char(8) DEFAULT NULL,
+  `descrizione` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--| 000001000010
+
+-- anagrafica_modalita_pagamento
+-- tipologia: tabella gestita
+CREATE TABLE IF NOT EXISTS `anagrafica_modalita_pagamento` (
+`id` int(11) NOT NULL,
+  `id_anagrafica` int(11) NOT NULL,
+  `id_modalita_pagamento` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--| 000001000011
+
+-- anagrafica_provenienze
+-- tipologia: tabella gestita
+CREATE TABLE IF NOT EXISTS `anagrafica_provenienze` (
+`id` int(11) NOT NULL,
+  `id_anagrafica` int(11) NOT NULL,
+  `id_provenienza` int(11) NOT NULL,
+  `testo` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--| 000001000012
+
+-- anagrafica_ruoli
+-- tipologia: tabella gestita
+CREATE TABLE IF NOT EXISTS `anagrafica_ruoli` (
+`id` int(11) NOT NULL,
+  `id_genitore` int(11) DEFAULT NULL,
+  `ordine` int(11) DEFAULT NULL,
+  `id_ruolo` int(11) DEFAULT NULL,
+  `specifica_ruolo` char(255) DEFAULT NULL,
+  `id_anagrafica` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--| 000001000013
+
+-- anagrafica_servizi_contatto
+-- tipologia: tabella gestita
+CREATE TABLE IF NOT EXISTS `anagrafica_servizi_contatto` (
+`id` int(11) NOT NULL,
+  `id_anagrafica` int(11) NOT NULL,
+  `id_servizio_contatto` int(11) NOT NULL,
+  `testo` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--| 000001000014
+
+-- anagrafica_settori
+-- tipologia: tabella gestita
+CREATE TABLE IF NOT EXISTS `anagrafica_settori` (
+`id` int(11) NOT NULL,
+  `id_anagrafica` int(11) NOT NULL,
+  `id_settore` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--| 000001000015
+
+-- articoli
+-- tipologia: tabella gestita
+CREATE TABLE IF NOT EXISTS `articoli` (
+  `id` char(32) NOT NULL,
+  `id_prodotto` char(32) NOT NULL,
+  `nome` char(128) DEFAULT NULL,
+  `ordine` int(11) DEFAULT NULL,
+  `id_taglia` int(11) DEFAULT NULL,
+  `id_colore` int(11) DEFAULT NULL,
+  `id_udm` int(11) DEFAULT NULL,
+  `se_disponibile` int(1) DEFAULT '1',
+  `quantita_disponibile` int(11) DEFAULT NULL,
+  `id_account_inserimento` int(11) DEFAULT NULL,
+  `timestamp_inserimento` int(11) DEFAULT NULL,
+  `id_account_aggiornamento` int(11) DEFAULT NULL,
+  `timestamp_aggiornamento` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--| 000001000016
+
+-- articoli_caratteristiche
+-- tipologia: tabella gestita
+CREATE TABLE IF NOT EXISTS `articoli_caratteristiche` (
+`id` int(11) NOT NULL,
+  `id_articolo` char(32) NOT NULL,
+  `id_caratteristica` int(11) DEFAULT NULL,
+  `ordine` int(11) DEFAULT NULL,
+  `testo` text,
+  `se_non_presente` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--| 000001000017
+
+-- assicurazioni_montaggio
+-- tipologia: tabella gestita
+CREATE TABLE IF NOT EXISTS `assicurazioni_montaggio` (
+`id` int(11) NOT NULL,
+  `nome` char(255) NOT NULL,
+  `suggerimento` char(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 --| FINE FILE
