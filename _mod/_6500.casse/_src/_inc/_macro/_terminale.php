28c07f33bef751e7da881994da111a58b1def083
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-05-18 16:18:22 +0200

    aggiunta indicazione reparto; aggiunta articolo nel documento dagli articoli frequenti

--- _mod/_6500.casse/_src/_inc/_macro/_terminale.php
+++ _mod/_6500.casse/_src/_inc/_macro/_terminale.php
@@ -24,9 +24,17 @@
     // tabella gestita
 	$ct['form']['table'] = 'documenti';
 
-    $_REQUEST[ $ct['form']['table'] ]['id'] = 12;
+  //  $_REQUEST[ $ct['form']['table'] ]['id'] = 12;
 
    // print_r($_REQUEST);
+
+    if(  isset( $_REQUEST[ $ct['form']['table'] ]['id'] ) && isset( $_REQUEST['__art__'] ) && !empty( $_REQUEST['__art__'] ) ){
+        
+        $_REQUEST[ $ct['form']['table'] ]['__comando__']  = $_REQUEST['__art__'];
+        $_REQUEST[ $ct['form']['table'] ]['__operazione__'] = 1;
+        $_REQUEST[ $ct['form']['table'] ]['__reparto__'] = 0;
+        print_r( "trovato articolo da aggiungere codice ".$_REQUEST[ $ct['form']['table'] ]['__comando__'] );
+    }
 
     if( isset( $_REQUEST[ $ct['form']['table'] ]['id'] ) && isset( $_REQUEST[ $ct['form']['table'] ]['__comando__'] ) && !empty( $_REQUEST[ $ct['form']['table'] ]['__comando__']  ) ){
 
@@ -34,16 +42,20 @@
         if( true ){
 
             // verifico se esiste l'atricolo e se ha un prezzo associato
-            $articolo = mysqlCachedIndexedQuery(
-                $cf['memcache']['index'],
-                $cf['memcache']['connection'],
+            $articolo = mysqlSelectRow(
                 $cf['mysql']['connection'],
 #                'SELECT * FROM articoli LEFT JOIN prezzi ON prezzi.id_articolo = articoli.id AND prezzi.id_listino = ? WHERE articoli.id = ? LIMIT 1',
 #                array( array( 's' => 1 ), array( 's' => '`'.$_REQUEST[ $ct['form']['table'] ]['__comando__'].'`' ) )
                 "SELECT * FROM articoli LEFT JOIN prezzi ON prezzi.id_articolo = articoli.id AND prezzi.id_listino = 1 WHERE articoli.id = \"".$_REQUEST[ $ct['form']['table'] ]['__comando__']."\" LIMIT 1"
-                )[0];
+                );
 
             if( $articolo ){    
+
+                if( $_REQUEST[ $ct['form']['table'] ]['__reparto__'] == 0 ){ $reparto = $articolo['id_reparto']; }
+                else { $reparto = $_REQUEST[ $ct['form']['table'] ]['__reparto__']; }
+
+                if( $reparto > 0 ){
+
                 // verifico se l'articolo è già nel documento
                 $in_doc = mysqlSelectRow(
                     $cf['mysql']['connection'],
@@ -71,16 +83,18 @@
 
 
                 } elseif ( $_REQUEST[ $ct['form']['table'] ]['__operazione__'] != '-1' ) {
-                   // print_r( "inserisco ".$_REQUEST[ $ct['form']['table'] ]['__comando__'] );
                     $insert = mysqlQuery( 
                                 $cf['mysql']['connection'], 
-                                "INSERT INTO documenti_articoli ( id_articolo, id_documento, data_lavorazione, importo_netto_totale, quantita )  VALUES ( \"".$_REQUEST[ $ct['form']['table'] ]['__comando__']."\", ?, ?, ?, 1 )",
+                                "INSERT INTO documenti_articoli ( id_articolo, id_documento, data_lavorazione, importo_netto_totale, quantita, id_reparto )  VALUES ( \"".$_REQUEST[ $ct['form']['table'] ]['__comando__']."\", ?, ?, ?, 1, ? )",
                                 array( 
                                     array( 's' => $_REQUEST[ $ct['form']['table'] ]['id'] ),
                                     array( 's' => date("Y-m-d") ),
-                                    array( 's' => $articolo['prezzo'] )
+                                    array( 's' => $articolo['prezzo'] ),
+                                    array( 's' => $reparto )
                                 ) );
                 }
+            }
+
             }
         }
 
@@ -134,12 +148,14 @@
 	    'SELECT id FROM anagrafica_view WHERE se_azienda_gestita = 1 LIMIT 1'
 	);
     
+    if( isset( $_REQUEST[ $ct['form']['table'] ]['id'] )  ){
    // righe del documento
 	$ct['etc']['righe'] = mysqlQuery(
 	    $cf['mysql']['connection'],
-	    'SELECT * FROM documenti_articoli WHERE id_documento = ?',
+	    'SELECT * FROM documenti_articoli_view WHERE id_documento = ?',
         array( array( 's' =>  $_REQUEST[ $ct['form']['table'] ]['id'] ) ) 
 	);
+
 
     if( sizeof( $ct['etc']['righe'] ) > 0 ){
 
@@ -153,3 +169,5 @@
         }
 
     }
+
+}
