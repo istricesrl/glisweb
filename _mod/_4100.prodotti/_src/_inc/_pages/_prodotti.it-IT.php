bfd7f333b578a55916fbe1de9b6fed2267c15199
Rleontine <rleonie.m@gmail.com>
2021-05-28 17:59:03 +0200

    creazione vista reparti e form gestione reparti

--- _mod/_4100.prodotti/_src/_inc/_pages/_prodotti.it-IT.php
+++ _mod/_4100.prodotti/_src/_inc/_pages/_prodotti.it-IT.php
@@ -383,3 +383,29 @@
 		'auth'		=> array( 'groups'	=> array(	'roots' ) ),
 		'etc'		=> array( 'tabs'	=> $p['listini.form']['etc']['tabs'] )
 	);
+    
+	// vista reparti
+	$p['reparti.view'] = array(
+	    'sitemap'		=> false,
+	    'title'			=> array( $l		=> 'reparti' ),
+	    'h1'			=> array( $l		=> 'reparti' ),
+	    'parent'		=> array( 'id'		=> 'catalogo' ),
+	    'template'		=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'default.view.html' ),
+		'macro'			=> array( $m . '_src/_inc/_macro/_reparti.view.php' ),
+		'etc'			=> array( 'tabs'	=> array( 'reparti.view' ) ),
+		'auth'			=> array( 'groups'	=> array(	'roots', 'staff' ) ),
+		'menu'			=> array( 'admin'	=> array(	'label'		=> array( $l => 'reparti' ),
+								'priority'	=> '025' ) )
+	);
+
+	// gestione reparti
+	$p['reparti.form'] = array(
+	    'sitemap'		=> false,
+	    'title'		=> array( $l		=> 'gestione' ),
+	    'h1'		=> array( $l		=> 'gestione' ),
+	    'parent'		=> array( 'id'		=> 'reparti.view' ),
+	    'template'		=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'reparti.form.html' ),
+	    'macro'		=> array( $m . '_src/_inc/_macro/_reparti.form.php' ),
+	    'auth'		=> array( 'groups'	=> array(	'roots', 'staff' ) ),
+	    'etc'		=> array( 'tabs'	=> array(	'reparti.form' ) )
+	);
