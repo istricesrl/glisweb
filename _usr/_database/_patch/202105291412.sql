b19b265a070c41a31c36608f87fa5c5d353e6498
Fabio Mosti <fabio.mosti@gmail.com>
2021-05-29 14:12:31 +0200

    distribuzione patch database 202105291412.sql

---
+++ _usr/_database/_patch/202105291412.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE VIEW `progetti_view` AS
+	SELECT
+	progetti.*,
+	tipologie_progetti.nome AS tipologia,
+	categorie_progetti.se_ordinario,
+	categorie_progetti.se_straordinario,
+	group_concat( DISTINCT categorie_progetti_path( categorie_progetti.id ) SEPARATOR ' | ' ) AS categorie,
+	if( progetti.timestamp_chiusura IS NOT NULL, 1, 0 ) AS chiuso,
+	mastri.nome AS mastro_attivita_default,
+	coalesce( anagrafica_cliente.soprannome, anagrafica_cliente.denominazione , concat( anagrafica_cliente.cognome, ' ', anagrafica_cliente.nome ), '' ) AS cliente,
+	concat( 
+		if(indirizzi.descrizione not like '', concat (indirizzi.descrizione, ' - '), ''),
+		indirizzi.indirizzo,
+		if(indirizzi.civico not like '', concat (' ', indirizzi.civico), ''),
+		if(indirizzi.cap not like '', concat (', ', indirizzi.cap), ''),
+		if(indirizzi.localita not like '', concat (' ', indirizzi.localita), ''),
+		if(indirizzi.id_comune is not null, concat (' ', comuni.nome, ' (', provincie.sigla, ')'), '')
+	) AS indirizzo,
+	progetti.nome AS __label__
+	FROM progetti
+	LEFT JOIN task ON task.id_progetto = progetti.id
+	LEFT JOIN anagrafica AS anagrafica_cliente ON anagrafica_cliente.id = progetti.id_cliente
+	LEFT JOIN tipologie_progetti ON tipologie_progetti.id = progetti.id_tipologia
+	LEFT JOIN indirizzi ON indirizzi.id = progetti.id_indirizzo
+	LEFT JOIN comuni ON comuni.id = indirizzi.id_comune
+	LEFT JOIN provincie ON provincie.id = comuni.id_provincia
+	LEFT JOIN progetti_categorie ON progetti_categorie.id_progetto = progetti.id
+	LEFT JOIN categorie_progetti ON categorie_progetti.id = progetti_categorie.id_categoria
+	LEFT JOIN mastri ON progetti.id_mastro_attivita_default = mastri.id
+	WHERE progetti.se_cancellare IS NULL
+	GROUP BY progetti.id
+	ORDER BY __label__
+;
