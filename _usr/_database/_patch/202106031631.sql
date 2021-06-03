0adcc598930700184dc9286d22b88aac852ee0e9
Fabio Mosti <fabio.mosti@gmail.com>
2021-06-03 16:48:15 +0200

    distribuzione patch database 202106031631.sql

---
+++ _usr/_database/_patch/202106031631.sql
@@ -0,0 +1,26 @@
+DELIMITER //
+DROP PROCEDURE IF EXISTS `anagrafica_view_static`//
+CREATE DEFINER=`root`@`localhost`
+PROCEDURE `anagrafica_view_static`( IN `i` INT(11) )
+BEGIN
+
+	IF @TRIGGER_LAZY_ANAGRAFICA IS NULL THEN
+	  IF i IS NULL THEN
+
+		DELETE FROM anagrafica_view_static;
+		
+		REPLACE INTO anagrafica_view_static SELECT * FROM anagrafica_view;
+
+	  ELSE
+	  
+		DELETE FROM anagrafica_view_static WHERE anagrafica_view_static.id = i;
+
+		INSERT INTO anagrafica_view_static
+		  SELECT * FROM anagrafica_view WHERE anagrafica_view.id = i;
+	  
+	  END IF;
+	END IF;
+
+END//
+DELIMITER ;
+
