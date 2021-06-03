50481f1726cafd96615bc38ddb823034cbcb2b9d
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-06-03 13:04:54 +0200

    aggiunta gestione e salvataggio elementi principali della gestione assistenza

--- _mod/_6500.casse/_src/_inc/_macro/_assistenza.php
+++ _mod/_6500.casse/_src/_inc/_macro/_assistenza.php
@@ -12,7 +12,10 @@ if( isset( $_REQUEST['__unset__'] ) ){
 
     unset( $_SESSION['assistenza']['id_cliente'] );
     unset( $_SESSION['assistenza']['id_progetto'] );
+    unset( $_SESSION['assistenza']['id_todo']  );
+    unset( $_SESSION['assistenza']['id_attivita']  );
     unset( $_SESSION['__view__'][ 'clienti' ]['__search__'] );
+
 }
 
 // id della vista
@@ -20,6 +23,7 @@ $ct['view']['id'] = md5( $ct['view']['table'] );
 
 if( isset( $_SESSION['assistenza']['id_cliente'] ) && !isset($ct['etc']['progetto'])  ){
     $ct['etc']['cliente'] = mysqlSelectRow( $cf['mysql']['connection'], 'SELECT * FROM anagrafica_view_static WHERE id = ?', array( array( 's' => $_SESSION['assistenza']['id_cliente']) ));
+    
 }
 
 if( isset( $_SESSION['assistenza']['id_progetto'] ) && !isset($ct['etc']['progetto']) ){
@@ -77,6 +81,48 @@ if( !( isset( $_SESSION['assistenza']['id_cliente'] ) ) ){
 
 }
 
+if( isset( $_SESSION['assistenza']['id_cliente'] ) && isset( $_SESSION['assistenza']['id_progetto'] ) && !isset( $_SESSION['assistenza']['id_todo'] ) ){
+
+    $ct['form']['table'] = 'todo';
+}
+
+    if( isset( $_REQUEST['todo']['id'] ) && !empty($_REQUEST['todo']['id']) ){
+
+        $_SESSION['assistenza']['id_todo'] = $_REQUEST['todo']['id'];
+ //       $ct['form']['table'] = 'attivita';
+   //     $_REQUEST['todo'] = mysqlSelectRow( $cf['mysql']['connection'], 'SELECT * FROM todo_view_static WHERE id = ?', array( array( 's' => $_SESSION['assistenza']['id_todo']) ));
+    }
+
+    if( isset( $_SESSION['assistenza']['id_todo'] ) ){
+
+     //   $_SESSION['assistenza']['id_todo'] = $_REQUEST['todo']['id'];
+        $ct['form']['table'] = 'attivita';
+        $_REQUEST['todo'] = mysqlSelectRow( $cf['mysql']['connection'], 'SELECT * FROM todo_view_static WHERE id = ?', array( array( 's' => $_SESSION['assistenza']['id_todo']) ));
+    
+    }
+
+    if( isset( $_REQUEST['attivita']['id'] ) && !empty( $_REQUEST['attivita']['id'] ) ){
+        $_SESSION['assistenza']['id_attivita'] = $_REQUEST['attivita']['id'];
+
+    }
+
+    if( isset( $_SESSION['assistenza']['id_attivita'] ) ){
+
+           $_REQUEST['attivita'] = mysqlSelectRow( $cf['mysql']['connection'], 'SELECT * FROM attivita_view WHERE id = ?', array( array( 's' => $_SESSION['assistenza']['id_attivita']) ));   
+    }
+
+
+    $ct['etc']['select']['id_anagrafica_collaboratori'] = mysqlCachedIndexedQuery(
+        $cf['cache']['index'],
+        $cf['memcache']['connection'],
+        $cf['mysql']['connection'], 
+        'SELECT id, __label__ FROM anagrafica_view WHERE se_collaboratore = 1 '  );
+
+        $ct['etc']['select']['indirizzi'] = mysqlCachedIndexedQuery(
+            $cf['cache']['index'],
+            $cf['memcache']['connection'],
+            $cf['mysql']['connection'], 
+            'SELECT id, __label__ FROM indirizzi_view' );
 
    // macro di default
    require DIR_SRC_INC_MACRO . '_default.view.php';
