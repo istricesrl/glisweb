c0c2581069002ab7e17c43cb95b00644ca1a99b2
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-06-04 16:00:39 +0200

    debug _760.job aggiunto controllo sulla presenza di un account prima di effettuare la query per la selezione di job in foreground

--- _src/_config/_760.job.php
+++ _src/_config/_760.job.php
@@ -11,14 +11,17 @@
      *
      */
 
-    // seleziono i job a cui ho applicato il lock
-    $cf['jobs']['foreground'] = mysqlQuery(
-        $cf['mysql']['connection'],
-        'SELECT * FROM job WHERE se_foreground IS NOT NULL AND ( id_account_inserimento = ? OR id_account_inserimento IS NULL ) AND timestamp_completamento IS NULL',
-        array(
-            array( 's' => $cf['session']['account']['id'] )
-        )
-    );
+    // seleziono i job a cui ho applicato il lock se un account è loggato
+    if( isset( $cf['session']['account']['id'] ) ){
+        $cf['jobs']['foreground'] = mysqlQuery(
+            $cf['mysql']['connection'],
+            'SELECT * FROM job WHERE se_foreground IS NOT NULL AND ( id_account_inserimento = ? OR id_account_inserimento IS NULL ) AND timestamp_completamento IS NULL',
+            array(
+                array( 's' => $cf['session']['account']['id'] )
+            )
+        );
+    }
+
 
     // debug
         // print_r( $cf['jobs']['foreground'] );
