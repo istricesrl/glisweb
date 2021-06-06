9bd782e56b1644e77f6e0800eef56357ca969d76
Fabio Mosti <fabio.mosti@istricesrl.it>
2021-06-06 16:49:31 +0200

    prima implementazione

---
+++ _src/_api/_task/_static.view.refresh.php
@@ -0,0 +1,71 @@
+<?php
+
+    /**
+     *
+     *
+     *
+     *
+     * @todo commentare
+     *
+     * @file
+     *
+     */
+
+    // inclusione del framework
+	if( ! defined( 'CRON_RUNNING' ) ) {
+	    require '../../_config.php';
+	}
+
+    // inizializzo l'array del risultato
+	$status = array();
+
+    // chiave di lock
+	$status['token'] = getToken( __FILE__ );
+
+    // blocco una riga
+    $status['row'] = mysqlSelectRow(
+        $cf['mysql']['connection'],
+        'UPDATE refresh_view_statiche SET token = ? WHERE token IS NULL ORDER BY timestamp_prenotazione ASC LIMIT 1',
+        array(
+            array( 's' => $status['token'])
+        )
+    );
+
+    // seleziono le view statiche da aggiornare
+    $status['row'] = mysqlSelectRow(
+        $cf['mysql']['connection'],
+        'SELECT * FROM refresh_view_statiche WHERE token = ?'
+        array(
+            array( 's' => $status['token'])
+        )
+    );
+
+    // se c'è una riga da chiamare
+    if( ! empty( $status['row'] ) ) {
+
+        // aggiorno la vista
+        $status['refresh'] = mysqlSelectRow(
+            $cf['mysql']['connection'],
+            'CALL ' . $status['entita'] . '_view_static()'
+        );
+
+        // elimino la richiesta
+        $status['remove'] = mysqlSelectRow(
+            $cf['mysql']['connection'],
+            'DELETE FROM refresh_view_statiche WHERE token = ?',
+            array(
+                array( 's' => $status['token'])
+            )
+        );
+    
+    } else {
+
+        // output
+        $status['row'] = 'nessuna riga da elaborare';
+
+    }
+
+    // output
+	if( ! defined( 'CRON_RUNNING' ) ) {
+	    buildJson( $status );
+	}
