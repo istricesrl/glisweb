5f04f0f86407243cbdbee9249aa4a21c12beab1d
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-07-01 16:57:44 +0200

    aggiunte schede per la stama articoli del catalogo

---
+++ _mod/_4100.prodotti/_src/_inc/_macro/_articoli.stampe.php
@@ -0,0 +1,42 @@
+<?php
+
+    /**
+     * macro form anagrafica
+     *
+     *
+     *
+     * -# definizione della tabella del modulo
+     * -# popolazione delle tendine
+     *
+     *
+     *
+     *
+     *
+     *
+     * @todo documentare
+     *
+     * @file
+     *
+     */
+
+    // tabella gestita
+	$ct['form']['table'] = 'articoli';
+
+    $ct['page']['contents']['metros'] = array(
+	    'general' => array(
+		'label' => NULL
+	    )
+	);
+
+    if(file_exists(DIR_MOD.'4100.prodotti/src/api/print/manuale.barcode.pdf.php')  ){$file =  $cf['site']['url'].'4100.prodotti/src/api/print/cartellini.prezzo.articoli.pdf.php';}
+    else {$file = $cf['site']['url'].'_mod/_4100.prodotti/_src/_api/_print/_cartellini.prezzo.articoli.pdf.php';  }
+
+	$ct['page']['contents']['metro']['general'][] = array(
+        'target' => '_blank' ,
+		'url' => $file ,
+		'icon' => NULL,
+		'fa' => 'fa-file-pdf-o',
+		'title' => 'etichette prezzo',
+		'text' => 'stampa l\'etichetta prezzo di tutti gli articol in pdf'
+	    );
+
