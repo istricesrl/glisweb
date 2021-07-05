b4b6f474d54be4956417cdfe2b4396afafe1672e
silvia-df <silvia.defalco@istricesrl.it>
2021-07-05 12:04:10 +0200

    modal conferma creazione cartellini

--- _mod/_1120.cartellini/_src/_inc/_macro/_attivita.tools.php
+++ _mod/_1120.cartellini/_src/_inc/_macro/_attivita.tools.php
@@ -12,17 +12,29 @@
     // gruppi di controlli
 	$ct['page']['contents']['metros'] = array(
 	    'esportazioni' => array(
-		'label' => 'esportazioni'
-	    )
+			'label' => 'esportazioni'
+		),
+		'azioni' => array(
+			'label' => 'azioni'
+		)
 	);
 
-    // esportazione contatti anagrafica
+    // esportazione ore zucchetti
 	$ct['page']['contents']['metro']['esportazioni'][] = array(
 	    'modal' => array( 'id' => 'esporta_zucchetti', 'include' => 'inc/attivita.tools.modal.zucchetti.html' ),
 	    'icon' => NULL,
 	    'fa' => 'fa-file-excel-o',
 	    'title' => 'esportazione XML per Zucchetti',
 	    'text' => 'esporta le ore in formato XML per il software Zucchetti'
+	);
+
+	// calcolo cartellini
+	$ct['page']['contents']['metro']['azioni'][] = array(
+	    'modal' => array( 'id' => 'cartellini', 'include' => 'inc/attivita.tools.modal.cartellini.html' ),
+	    'icon' => NULL,
+	    'fa' => 'fa-id-card-o',
+	    'title' => 'calcolo cartellini',
+	    'text' => 'calcola i cartellini di un determinato mese e anno'
 	);
 
     // tendina mesi
