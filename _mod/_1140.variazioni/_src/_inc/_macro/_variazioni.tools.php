dc61fff75249a4235ee1b51819e82a7c7e9251d6
silvia-df <silvia.defalco@istricesrl.it>
2021-07-23 10:53:49 +0200

    richiesta e sostituzione per operatore e periodo

--- _mod/_1140.variazioni/_src/_inc/_macro/_variazioni.tools.php
+++ _mod/_1140.variazioni/_src/_inc/_macro/_variazioni.tools.php
@@ -12,7 +12,10 @@
      // gruppi di controlli
 	$ct['page']['contents']['metros'] = array(
 	    'azioni' => array(
-		'label' => 'azioni'
+			'label' => 'azioni'
+		),
+		'sostituzioni' => array(
+			'label' => 'sostituzioni'
 	    )
 	);
 
@@ -40,6 +43,65 @@
 	    'title' => 'libera operatore',
 	    'text' => 'rimuove un operatore dalle attività di un progetto'
 	);
+
+	// richiesta sostituzione per operatore
+	$ct['page']['contents']['metro']['sostituzioni'][] = array(
+        'modal' => array('id' => 'richiesta', 'include' => 'inc/variazioni.tools.modal.operatore.richiesta.html' ),
+	    'icon' => NULL,
+	    'fa' => 'fa-files-o',
+	    'title' => 'richiedi sostituzione operatore',
+	    'text' => 'invia la richiesta di sostituzione di un operatore con un altro'
+	);
+	
+	// sostituzione per operatore
+	$ct['page']['contents']['metro']['sostituzioni'][] = array(
+        'modal' => array('id' => 'sostituisci', 'include' => 'inc/variazioni.tools.modal.operatore.sostituisci.html' ),
+	    'icon' => NULL,
+	    'fa' => 'fa-files-o',
+	    'title' => 'sostituisci operatore',
+	    'text' => 'effettua la sostituzione di un operatore con un altro'
+	);
+
+	// se ho una richiesta di sostituzione creo il job relativo
+	if( !empty( $_REQUEST['__sostituzione__']['id_assente'] ) && !empty( $_REQUEST['__sostituzione__']['id_sostituto'] ) ){
+
+		$assente = mysqlSelectValue(
+			$cf['mysql']['connection'],
+			'SELECT __label__ FROM anagrafica_view_static WHERE id = ?',
+			array( array( 's' => $_REQUEST['__sostituzione__']['id_assente'] ) )
+		);
+
+		$sostituto = mysqlSelectValue(
+			$cf['mysql']['connection'],
+			'SELECT __label__ FROM anagrafica_view_static WHERE id = ?',
+			array( array( 's' => $_REQUEST['__sostituzione__']['id_sostituto'] ) )
+		);
+
+		$nome =  ( !isset( $_REQUEST['__sostituzione__']['hard'] ) ) ? 'richiesta ' : '';
+		$nome .= 'sostituzione operatore ' . $assente . ' con ' . $sostituto;
+		$nome .=  ( isset( $_REQUEST['__sostituzione__']['data_inizio'] ) && isset( $_REQUEST['__sostituzione__']['data_fine'] ) ) ? ' dal ' . $_REQUEST['__sostituzione__']['data_inizio'] . ' al ' . $_REQUEST['__sostituzione__']['data_fine'] : '';
+
+		$job = mysqlQuery(
+			$cf['mysql']['connection'],
+			'INSERT INTO job ( nome, job, iterazioni, workspace, se_foreground, delay ) VALUES ( ?, ?, ?, ?, ?, ? )',
+			array(
+				array( 's' => $nome ),
+				array( 's' => '_mod/_1140.variazioni/_src/_api/_job/_sostituzioni.operatore.request.php' ),
+				array( 's' => 10 ),
+				array( 's' => json_encode(
+					array(
+						'id_assente' => $_REQUEST['__sostituzione__']['id_assente'],
+						'id_sostituto' => $_REQUEST['__sostituzione__']['id_sostituto'],
+						'data_inizio' => isset( $_REQUEST['__sostituzione__']['data_inizio'] ) ? $_REQUEST['__sostituzione__']['data_inizio'] : NULL,
+						'data_fine' => isset( $_REQUEST['__sostituzione__']['data_fine'] ) ? $_REQUEST['__sostituzione__']['data_fine'] : NULL,
+						'hard' => ( isset( $_REQUEST['__sostituzione__']['hard'] ) ) ? $_REQUEST['__sostituzione__']['hard'] : NULL
+					)
+				) ),
+				array( 's' => 1 ),
+				array( 's' => 3 )
+			)
+		);	
+	}
 
 
 	// macro di default
