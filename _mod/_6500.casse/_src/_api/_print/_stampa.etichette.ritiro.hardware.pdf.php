f6f5d89a6bea237b541142132cb194c680a09bdf
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-08-10 12:59:17 +0200

    nascosto rettangolo guida stampa

--- _mod/_6500.casse/_src/_api/_print/_stampa.etichette.ritiro.hardware.pdf.php
+++ _mod/_6500.casse/_src/_api/_print/_stampa.etichette.ritiro.hardware.pdf.php
@@ -143,7 +143,7 @@
 
        
         // rettangolo guida
-        $pdf-> Rect( $x, $y, $wBox, $hBox );	
+        //$pdf-> Rect( $x, $y, $wBox, $hBox );	
         //$pdf-> Rect( $x , $y + $hBox , $wBox, $hBox );
         $pdf -> setXY( $x + $litsp, $y + $litsp );
         
