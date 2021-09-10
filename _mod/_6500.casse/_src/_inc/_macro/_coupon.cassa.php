78997ec7f235f784b31c3864d5aa3bf55093eb48
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-09-10 17:55:21 +0200

    aggiunta funzione stampa coupon

--- _mod/_6500.casse/_src/_inc/_macro/_coupon.cassa.php
+++ _mod/_6500.casse/_src/_inc/_macro/_coupon.cassa.php
@@ -1,10 +1,18 @@
 <?php
 
 
-    $ct['form']['table'] = '';
+    $ct['form']['table'] = 'coupon';
 
-
+    if( isset( $_REQUEST[$ct['form']['table']]['id'] ) ){
+        $_SESSION['coupon'] = $_REQUEST[$ct['form']['table']]['id'];
+    }
     
+    if( empty( $_REQUEST[$ct['form']['table']] ) ){
+        $_REQUEST[$ct['form']['table']] = $_SESSION['coupon'];
+        $_REQUEST[$ct['form']['table']] =  mysqlSelectRow( $cf['mysql']['connection'], 'SELECT * FROM coupon_view  WHERE id = ?', array( array( 's' =>  $_SESSION['coupon'] ) ) );
+
+
+    }
 
     // macro di default
 	require DIR_SRC_INC_MACRO . '_default.form.php';
