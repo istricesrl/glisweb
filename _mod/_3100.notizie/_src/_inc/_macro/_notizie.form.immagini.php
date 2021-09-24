d5e9dfd56dffab7cedf83023482e2e31105f8962
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-09-24 17:18:31 +0200

    aggiunta modulo notizie ed inizio implementazione mosulo categorie notizie

---
+++ _mod/_3100.notizie/_src/_inc/_macro/_notizie.form.immagini.php
@@ -0,0 +1,37 @@
+<?php
+
+    /**
+     * macro form anagrafica
+     *
+     *
+     *
+     * -# definizione della tabella del modulo
+     * -# popolazione delle tendine
+     *
+     *
+     *
+     *
+     *
+     *
+     * @todo documentare
+     *
+     * @file
+     *
+     */
+
+    // tabella gestita
+	$ct['form']['table'] = 'notizie';
+
+    // sotto tabella gestita
+	$ct['form']['subtable'] = 'immagini';
+
+    // tendina ruolo immagini
+	$ct['etc']['select']['ruoli_immagini'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM ruoli_immagini_view WHERE se_contenuti = 1'
+	);
+
+	// macro di default
+	require DIR_SRC_INC_MACRO . '_default.form.php';
---
+++ _mod/_3100.notizie/_src/_inc/_macro/_notizie.form.php
@@ -0,0 +1,50 @@
+<?php
+
+    /**
+     * macro form anagrafica
+     *
+     *
+     *
+     * -# definizione della tabella del modulo
+     * -# popolazione delle tendine
+     *
+     *
+     *
+     *
+     *
+     *
+     * @todo documentare
+     *
+     * @file
+     *
+     */
+
+    // tabella gestita
+	$ct['form']['table'] =  'notizie';
+
+    // tendina tipologie pubblicazione
+	$ct['etc']['select']['tipologie_pubblicazione'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM tipologie_pubblicazione_view'
+	);
+
+    // tendina tipologie pubblicazione
+	$ct['etc']['select']['tipologie'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM tipologie_notizie_view'
+	);
+
+    // tendina categorie
+	$ct['etc']['select']['categorie'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM categorie_notizie_view'
+	);
+
+	// macro di default
+	require DIR_SRC_INC_MACRO . '_default.form.php';
