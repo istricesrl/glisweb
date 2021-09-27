e740db8a0037d38df151ccc499b6d2e068c399a5
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-09-27 17:57:30 +0200

    aggiunte nel config

---
+++ _mod/_3100.notizie/_src/_config/_030.common.php
@@ -0,0 +1,12 @@
+<?php
+
+    /**
+     * 
+     * 
+     * 
+     * @todo documentare
+     * 
+     */
+
+    // costanti che descrivono lo stato di funzionamento del framework
+	define( 'PREFX_CATEGORIE_NOTIZIE'					, 'CATEGORIE.NOTIZIE.' );
