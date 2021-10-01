85af78449bb4752352cee388e23567c51467bf7e
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-10-01 17:44:30 +0200

    debug

--- _mod/_3100.notizie/_src/_config/_030.common.php
+++ _mod/_3100.notizie/_src/_config/_030.common.php
@@ -10,3 +10,4 @@
 
     // costanti che descrivono lo stato di funzionamento del framework
 	define( 'PREFX_CATEGORIE_NOTIZIE'					, 'CATEGORIE.NOTIZIE.' );
+    define( 'PREFX_NOTIZIE'					            , 'NOTIZIE.' );
