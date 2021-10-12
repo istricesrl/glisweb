62e08329003d45ccf3176b7f5f28f194c47d54df
silvia-df <silvia.defalco@istricesrl.it>
2021-10-12 16:37:20 +0200

    creato modulo ticket

---
+++ _mod/_1250.ticket/_src/_config/_250.auth.php
@@ -0,0 +1,31 @@
+<?php
+
+    /**
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     * @todo documentare
+     *
+     * @file
+     *
+     */
+
+    // array dei permessi
+	$cf['auth']['permissions'] = array_merge_recursive( 
+	    $cf['auth']['permissions'],
+	    array(
+            'ticket' => array(
+                CONTROL_FULL => array( 'roots' ),
+                CONTROL_FILTERED => array( 'staff' )
+            ),
+            'todo' => array(
+                CONTROL_FULL => array( 'roots' ),
+                CONTROL_FILTERED => array( 'staff' )
+            )
+        )
+	);
