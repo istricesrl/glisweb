62b6825e100f7c240a27c29ac192068d50b3d6be
Fabio Mosti <fabio.mosti@istricesrl.it>
2021-10-26 18:36:52 +0200

    bugfix > corretto chiamata restCall su funzione reCaptchaVerifyV3

--- _src/_lib/_recaptcha.tools.php
+++ _src/_lib/_recaptcha.tools.php
@@ -33,7 +33,7 @@
             'response' => $t
         );
         
-        $r = restCall( 'https://www.google.com/recaptcha/api/siteverify', METHOD_POST, $dati, 'query', MIME_APPLICATION_JSON, $status );
+        $r = restCall( 'https://www.google.com/recaptcha/api/siteverify', METHOD_GET, $dati, 'query', MIME_APPLICATION_JSON, $status );
 
         if( isset( $r['score'] ) ){
             appendToFile(  $r['score']  . PHP_EOL, 'var/log/google_recaptcha_score.log' );
