824849755241980d338dab991b6e6f46c682bdec
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-11-16 12:27:49 +0100

    debug campo json software

--- _usr/_database/_patch/_010000999999.tables.sql
+++ _usr/_database/_patch/_010000999999.tables.sql
@@ -1883,7 +1883,7 @@ CREATE TABLE IF NOT EXISTS `software` (
   `id` int(11) NOT NULL,
   `id_genitore` int(11) DEFAULT NULL,
   `id_articolo` char(32) DEFAULT NULL,
-  `json` text(256) DEFAULT NULL, 
+  `json` text DEFAULT NULL, 
   `nome` char(128) DEFAULT NULL,
   `note` text,
   `id_account_inserimento` int(11) DEFAULT NULL,
--- _usr/_database/_patch/_030000999999.indexes.sql
+++ _usr/_database/_patch/_030000999999.indexes.sql
@@ -2046,10 +2046,10 @@ ALTER TABLE `software`
 	ADD UNIQUE KEY `unica` (`id_genitore`,`nome`),
 	ADD KEY `id_genitore` (`id_genitore`),
 	ADD KEY `id_articolo` (`id_articolo`),
-	ADD KEY `json` (`json`),
+	ADD KEY `json` (`json`(255) ),
 	ADD KEY `id_account_inserimento` (`id_account_inserimento`),
 	ADD KEY `id_account_aggiornamento` (`id_account_aggiornamento`),
-	ADD KEY `indice` (`id`,`id_genitore`,`id_articolo`,`nome`,`json`);
+	ADD KEY `indice` (`id`,`id_genitore`,`id_articolo`,`nome`,`json`(255));
 
 --| 030000041401
 
