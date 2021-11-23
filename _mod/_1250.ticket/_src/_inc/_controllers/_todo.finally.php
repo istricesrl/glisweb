ad25d55ea4c5524016c4cf1f37b144344c7f9815
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-11-23 16:28:27 +0100

    debug

--- _mod/_1250.ticket/_src/_inc/_controllers/_todo.finally.php
+++ _mod/_1250.ticket/_src/_inc/_controllers/_todo.finally.php
@@ -24,8 +24,38 @@
         case METHOD_PUT:
 
             $todo = mysqlSelectRow( $c, 'SELECT * FROM todo_view WHERE id = ?', array( array('s' => $d['id'] ) ) );
+            
+            // log
+	        logWrite( "inserito ticket #".$todo['id'], 'ticket', LOG_CRIT );
+            // recupero la mail del responsabile
+            $mail = mysqlSelectValue( $c, 'SELECT indirizzo FROM mail WHERE id_anagrafica = ?', array( array('s' => $d['id_responsabile'] ) ) );
+
+            // accodao la mail per il responsabile
+            if( !empty( $mail ) ){
+
                 // log
-	        logWrite( "è stato inserito il ticket ".$todo['id'].' etichetta  '.$todo['__label__'], 'controller', LOG_CRIT );
+	            logWrite( "accodo la mail per il responsabile".$d['id_responsabile'], 'ticket', LOG_CRIT );
+               
+                // invio la mail con i dati della todo
+                queueMailFromTemplate(
+                    $c,
+                    $cf['mail']['tpl'][ 'DEFAULT_TICKET_RESPONSABILE'],
+                    array( 'dati' => array( 'todo' => $todo ) ),
+                    strtotime( '-5 minutes' ),
+                    array( $mail => $mail ),
+                    NULL,
+                    array(),
+                    array(),
+                    array(),
+                    NULL,
+                    10
+                );
+                print_r($cf);
+            }
+            // recupero la mail cliente
+
+            // accodo la mail per il cliente
+
 
         break;
 
