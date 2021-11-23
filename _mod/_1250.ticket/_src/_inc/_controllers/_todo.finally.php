594763d59242a20b37a6d1c76888cdbdfbe6d518
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-11-23 17:38:14 +0100

    completato debug controller finally per ticket

--- _mod/_1250.ticket/_src/_inc/_controllers/_todo.finally.php
+++ _mod/_1250.ticket/_src/_inc/_controllers/_todo.finally.php
@@ -15,7 +15,7 @@
      */
 
     // log
-	logWrite( "controller finally per ${t}/${a}", 'controller' );
+	logWrite( "controller finally per ticket ${t}/${a}", 'controller' );
 
     // elaborazioni di default dei dati
 	switch( strtoupper( $a ) ) {
@@ -23,10 +23,10 @@
 	    case METHOD_POST:
         case METHOD_PUT:
 
-            $todo = mysqlSelectRow( $c, 'SELECT * FROM todo_view WHERE id = ?', array( array('s' => $d['id'] ) ) );
+            $todo = mysqlSelectRow( $c, 'SELECT progetti.id_cliente,todo_view.id,todo_view.data_ora_apertura, todo_view.nome, todo_view.testo, todo_view.id_responsabile, todo_view.responsabile FROM todo_view LEFT join progetti ON progetti.id = todo_view.id_progetto WHERE todo_view.id = ?', array( array('s' => $d['id'] ) ) );
             
             // log
-	        logWrite( "inserito ticket #".$todo['id'], 'ticket', LOG_CRIT );
+	        logWrite( "inserito ticket #".$todo['id'].' per cliente '.$todo['id_cliente'], 'ticket', LOG_CRIT );
             // recupero la mail del responsabile
             $mail = mysqlSelectValue( $c, 'SELECT indirizzo FROM mail WHERE id_anagrafica = ?', array( array('s' => $d['id_responsabile'] ) ) );
 
@@ -36,26 +36,38 @@
                 // log
 	            logWrite( "accodo la mail per il responsabile".$d['id_responsabile'], 'ticket', LOG_CRIT );
                
+                $template = mailGetTemplateByRuolo('DEFAULT_TICKET_RESPONSABILE');
                 // invio la mail con i dati della todo
                 queueMailFromTemplate(
                     $c,
-                    $cf['mail']['tpl'][ 'DEFAULT_TICKET_RESPONSABILE'],
+                    $template,
                     array( 'dati' => array( 'todo' => $todo ) ),
                     strtotime( '-5 minutes' ),
-                    array( $mail => $mail ),
-                    NULL,
-                    array(),
-                    array(),
-                    array(),
-                    NULL,
-                    10
+                    array( $mail => $mail )
                 );
-                print_r($cf);
+
             }
+
             // recupero la mail cliente
+            $mail_cliente = mysqlSelectValue( $c, 'SELECT indirizzo FROM mail WHERE id_anagrafica = ?', array( array('s' => $todo['id_cliente'] ) ) );
 
             // accodo la mail per il cliente
+           if( !empty( $mail_cliente ) ){
 
+                // log
+	            logWrite( "accodo la mail per il cliente".$todo['id_cliente'], 'ticket', LOG_CRIT );
+               
+                $template = mailGetTemplateByRuolo('DEFAULT_TICKET_CLIENTE');
+                // invio la mail con i dati della todo
+                queueMailFromTemplate(
+                    $c,
+                    $template,
+                    array( 'dati' => array( 'todo' => $todo ) ),
+                    strtotime( '-5 minutes' ),
+                    array( $mail_cliente => $mail_cliente )
+                );
+
+            }
 
         break;
 
--- _src/_lib/_mail.utils.php
+++ _src/_lib/_mail.utils.php
@@ -5,19 +5,17 @@
         global $cf;
 
         // recupero dei template dal database
-        $tpl = mysqlCachedIndexedQuery(
-            $cf['memcache']['index'],
-            $cf['memcache']['connection'],
+        $tpl = mysqlSelectRow(
             $cf['mysql']['connection'],
-            'SELECT * FROM template WHERE ruolo = ?',
+            'SELECT * FROM template_mail WHERE ruolo = ?',
             array( array( 's' => $ruolo ) )
         );
 
-        // se ci sono template
+        // se individuo il template
         if( ! empty( $tpl ) ) {
 
                 // inizializzo l'oggetto
-                $cf['mail']['tpl'][ $tpl['ruolo'] ] = array(
+               $template = array(
                     'type' => $tpl['type'],
                     'nome' => $tpl['nome']
                 );
@@ -28,13 +26,13 @@
                     $cf['mysql']['connection'],
                     'SELECT contenuti.*,lingue.ietf FROM contenuti '.
                     'INNER JOIN lingue ON lingue.id = contenuti.id_lingua '.
-                    'WHERE contenuti.id_template = ?',
+                    'WHERE contenuti.id_template_mail = ?',
                     array( array( 's' => $tpl['id'] ) )
                 );
 
                 // ciclo sui contenuti
                 foreach( $cnts as $cnt ) {
-                    $cf['mail']['tpl'][ $tpl['ruolo'] ][ $cnt['ietf'] ] = array(
+                   $template[ $cnt['ietf'] ] = array(
                     'from' => array( $cnt['mittente_nome'] => $cnt['mittente_mail'] ),
                     'to' => array( $cnt['destinatario_nome'] => $cnt['destinatario_mail'] ),
                     'to_cc' => array( $cnt['destinatario_cc_nome'] => $cnt['destinatario_cc_mail'] ),
@@ -50,17 +48,21 @@
                     $cf['mysql']['connection'],
                     'SELECT file.*,lingue.ietf FROM file '.
                     'INNER JOIN lingue ON lingue.id = file.id_lingua '.
-                    'WHERE file.id_template = ?',
+                    'WHERE file.id_template_mail = ?',
                     array( array( 's' => $tpl['id'] ) )
                 );
 
                 // ciclo sugli allegati
                 foreach( $files as $file ) {
-                    $cf['mail']['tpl'][ $tpl['ruolo'] ][ $file['ietf'] ]['attach'][ basename( $file['path'] ) ] = DIRECTORY_BASE . $file['path'];
+                   $template[ $file['ietf'] ]['attach'][ basename( $file['path'] ) ] = DIRECTORY_BASE . $file['path'];
                 }
 
         } else {
+             
+            $template = $cf['mail']['tpl']['DEFAULT'];
 
         }
+
+        return $template;
 
     }
