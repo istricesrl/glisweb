e9df119441178844588fc518a2b5d27354c92a7d
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-12-13 17:01:49 +0100

    aggiunta possibilità di aggiungere allegati alle mail

--- _src/_api/_task/_mail.send.php
+++ _src/_api/_task/_mail.send.php
@@ -15,7 +15,7 @@
     // inclusione del framework
 	if( ! defined( 'CRON_RUNNING' ) ) {
 	    require '../../_config.php';
-	}
+	}  
 
     // inizializzo l'array del risultato
 	$status = array();
@@ -39,30 +39,29 @@
 		    )
 		);
 
-		if( isset( $_REQUEST['__to_cc__'] ) ){
+		if( isset( $_REQUEST['__to_cc__'] ) && !empty( $_REQUEST['__to_cc__'] ) ){
 			$template[ $cf['localization']['language']['ietf'] ]['to_cc'] =  array( $_REQUEST['__to_cc__'] => $_REQUEST['__to_cc__'] );
 		} else {
 			$template[ $cf['localization']['language']['ietf'] ]['to_cc'] = array();
 		}
 
-		if( isset( $_REQUEST['__to_bcc__'] ) ){
+		if( isset( $_REQUEST['__to_bcc__'] ) && !empty( $_REQUEST['__to_bcc__'] ) ){
 			$template[ $cf['localization']['language']['ietf'] ]['to_bcc'] =  array( $_REQUEST['__to_bcc__'] => $_REQUEST['__to_bcc__'] );
 		} else {
 			$template[ $cf['localization']['language']['ietf'] ]['to_bcc'] = array();
 		}
 
+		if( isset( $_REQUEST['__attach__']) && !empty( $_REQUEST['__attach__'] ) ){
+			$template[ $cf['localization']['language']['ietf'] ]['attach'] = json_decode($_REQUEST['__attach__']);
+		} else {
+			$template[ $cf['localization']['language']['ietf'] ]['attach'] = array();
+		}
+
 	// istanzio il template
 	    $twig = new Twig_Environment( new Twig_Loader_Array( $template[ $cf['localization']['language']['ietf'] ] ) );
-/* function queueMail( 
-	$c, 
-	$timestamp_invio, 
-	$mittente, 
-	$destinatari, 
-	$oggetto, 
-	$corpo, $destinatari_cc = array(), $destinatari_bcc = array(), $allegati = array(), $headers = array(), $server = NULL ) {
-*/
+
 	// accodo la mail
-	    queueMail(
+	    $result = queueMail(
 		$cf['mysql']['connection'],
 		strtotime( '+1 minutes' ),
 		$template[ $cf['localization']['language']['ietf'] ]['from'],
@@ -70,12 +69,16 @@
 		$template[ $cf['localization']['language']['ietf'] ]['oggetto'],
 		$template[ $cf['localization']['language']['ietf'] ]['testo'],
 		$template[ $cf['localization']['language']['ietf'] ]['to_cc'],
-		$template[ $cf['localization']['language']['ietf'] ]['to_bcc']
+		$template[ $cf['localization']['language']['ietf'] ]['to_bcc'],
+		$template[ $cf['localization']['language']['ietf'] ]['attach']
 		);
 
-	    // log
-//	    logWrite( 'inserisco nella coda mail la mail inviata da indirizzo '.$template[ $cf['localization']['language']['ietf'] ]['from'], 'invio mail', LOG_DEBUG );
+		if( $result ){ $status['__status__'] = 'OK'; }
+		else { $status['__status__'] = 'NO'; }
 
+		buildJson( $status );
+	    // log
+	   
 
 	} else {
 
