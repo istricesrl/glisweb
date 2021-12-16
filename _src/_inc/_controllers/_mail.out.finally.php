82ebc5bdadfce96a0e9f945d4aec0e27ab044212
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-12-16 10:53:53 +0100

    debug

--- _src/_inc/_controllers/_mail.out.finally.php
+++ _src/_inc/_controllers/_mail.out.finally.php
@@ -30,11 +30,13 @@
 						if( in_array( $vKey, array( 'allegati' ) ) && !empty( $d[ 'allegati' ]) ) {
 							
 							if( !isset( $d['file']) || ( isset( $d['file'] ) && count(  unserialize( $vVal ) ) != count( $d['file'] ) ) ){
+								
 								$file =  unserialize( $vVal );
 							
 								if( !isset($d['file']) ){ $d['file'] = array(); }
 								
 								$counter = 1;
+								
 								foreach( $file as $f ){
 
 										$d['file'][] =  array( 'path' => $f ,'ordine' => $counter * 5, 'nome' => 'allegato mail #'.$counter++, 'id_ruolo' => 1 ) ;	
