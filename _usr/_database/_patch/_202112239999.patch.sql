c17488165a4dd2e5d08e6fe882c35c8a0c557f01
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2021-12-23 11:44:11 +0100

    aggiunti campidestinatario_numero e mittente_numero nella tabella contenuti

--- _usr/_database/_patch/_202112239999.patch.sql
+++ _usr/_database/_patch/_202112239999.patch.sql
@@ -14,4 +14,9 @@ ADD UNIQUE KEY `unica_account` (`id_entita`,`id_account`);
 ALTER TABLE `__acl_pagine__` CHANGE `id_gruppo` `id_gruppo` int NULL AFTER `id_entita`,
 ADD UNIQUE KEY `unica_account` (`id_entita`,`id_account`);
 
+--| 202112230040
+ALTER TABLE `contenuti`
+ADD `mittente_numero` char(128) COLLATE 'utf8_general_ci' NULL AFTER `mittente_nome`,
+ADD `destinatario_numero` char(128) COLLATE 'utf8_general_ci' NULL AFTER `destinatario_nome`;
+
 --| FINE FILE
