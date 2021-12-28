7f2a315aef8617011ca7fa2d394018508f1caff0
silvia-df <silvia.defalco@istricesrl.it>
2021-12-28 10:56:54 +0100

    previsto cartellini.form.sub per modifica righe

--- _mod/_1120.cartellini/_src/_inc/_macro/_cartellini.form.php
+++ _mod/_1120.cartellini/_src/_inc/_macro/_cartellini.form.php
@@ -29,6 +29,17 @@
         $cf['mysql']['connection'], 
         'SELECT id, __label__ FROM anagrafica_view_static' );
 
+    if( isset( $_REQUEST[ $ct['form']['table'] ]['id_anagrafica'] ) ){
+        // tendina contratti
+        $ct['etc']['select']['id_contratto'] = mysqlCachedIndexedQuery(
+            $cf['memcache']['index'],
+            $cf['memcache']['connection'],
+            $cf['mysql']['connection'], 
+            'SELECT id, __label__ FROM contratti_completa_view WHERE id_anagrafica = ?',
+        array( array( 's' => $_REQUEST[ $ct['form']['table'] ]['id_anagrafica'] ) ) );
+    }
+    
+
     // tendina tipologia inps
 	$ct['etc']['select']['id_tipologia_inps'] = mysqlCachedIndexedQuery(
 	    $cf['memcache']['index'],
