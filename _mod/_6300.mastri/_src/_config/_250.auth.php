57d8714614ce0e6eeb230e19e2fc0a07474d77d9
Fabio Mosti <fabio.mosti@istricesrl.it>
2022-02-01 18:41:51 +0100

    impostazione modulo magazzini

--- _mod/_6300.mastri/_src/_config/_250.auth.php
+++ _mod/_6300.mastri/_src/_config/_250.auth.php
@@ -19,18 +19,6 @@
 	$cf['auth']['permissions'] = array_merge_recursive( 
 	    $cf['auth']['permissions'],
 	    array(
-            'ddt' => array(
-                CONTROL_FULL => array( 'roots' ),
-                CONTROL_FILTERED => array( 'staff' )
-            ),
-            'ddt_passivi' => array(
-                CONTROL_FULL => array( 'roots' ),
-                CONTROL_FILTERED => array( 'staff' )
-            ),
-            'magazzini' => array(
-                CONTROL_FULL => array( 'roots' ),
-                CONTROL_FILTERED => array( 'staff' )
-            ),
             'conti' => array(
                 CONTROL_FULL => array( 'roots' ),
                 CONTROL_FILTERED => array( 'staff' )
