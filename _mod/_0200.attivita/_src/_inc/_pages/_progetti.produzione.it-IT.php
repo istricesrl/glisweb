9b5a5bac8a0f4be1501397d4b41bc1d1e43961e6
Fabio Mosti <fabio.mosti@istricesrl.it>
2022-02-08 17:56:37 +0100

    aggiunte attività alle fatture form

--- _mod/_0200.attivita/_src/_inc/_pages/_progetti.produzione.it-IT.php
+++ _mod/_0200.attivita/_src/_inc/_pages/_progetti.produzione.it-IT.php
@@ -6,20 +6,25 @@
     // modulo di questo file
 	$m = DIR_MOD . '_0200.attivita/';
 
-	// RELAZIONI CON IL MODULO PRODUZIONE
-	if( in_array( "1000.produzione", $cf['mods']['active']['array'] ) ) {
+	// RELAZIONI CON IL MODULO PROGETTI
+	if( in_array( "0900.progetti", $cf['mods']['active']['array'] ) ) {
 
-		// gestione attività progetti
-		$p['progetti.produzione.form.attivita'] = array(
-			'sitemap'		=> false,
-			'icon'			=> '<i class="fa fa-pencil-square-o" aria-hidden="true"></i>',
-			'title'			=> array( $l		=> 'attivita' ),
-			'h1'			=> array( $l		=> 'attività' ),
-			'parent'		=> array( 'id'		=> 'progetti.produzione.view' ),
-			'template'		=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'progetti.produzione.form.attivita.html' ),
-			'macro'			=> array( $m.'_src/_inc/_macro/_progetti.produzione.form.attivita.php' ),
-			'auth'			=> array( 'groups'	=> array(	'roots', 'staff' ) ),
-			'etc'			=> array( 'tabs'	=> 'progetti.produzione.form' )
-		);
+		// RELAZIONI CON IL MODULO PRODUZIONE
+		if( in_array( "1000.produzione", $cf['mods']['active']['array'] ) ) {
+
+			// gestione attività progetti
+			$p['progetti.produzione.form.attivita'] = array(
+				'sitemap'		=> false,
+				'icon'			=> '<i class="fa fa-pencil-square-o" aria-hidden="true"></i>',
+				'title'			=> array( $l		=> 'attivita' ),
+				'h1'			=> array( $l		=> 'attività' ),
+				'parent'		=> array( 'id'		=> 'progetti.produzione.view' ),
+				'template'		=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'progetti.produzione.form.attivita.html' ),
+				'macro'			=> array( $m.'_src/_inc/_macro/_progetti.produzione.form.attivita.php' ),
+				'auth'			=> array( 'groups'	=> array(	'roots', 'staff' ) ),
+				'etc'			=> array( 'tabs'	=> 'progetti.produzione.form' )
+			);
+
+		}
 
 	}
