27f617017cd21506d737cc3c5371153e972f6ec4
Fabio Mosti <fabio.mosti@istricesrl.it>
2022-02-20 18:24:42 +0100

    aggiunta funzione numOfWorkingDaysInMonth()

--- _src/_lib/_cron.utils.php
+++ _src/_lib/_cron.utils.php
@@ -27,6 +27,18 @@
 
     }
 
+    function numOfWorkingDaysInMonth($year, $month, $ignore = array( 0, 6 ) ) {
+        $count = 0;
+        $counter = mktime(0, 0, 0, $month, 1, $year);
+        while (date("n", $counter) == $month) {
+            if (in_array(date("w", $counter), $ignore) == false) {
+                $count++;
+            }
+            $counter = strtotime("+1 day", $counter);
+        }
+        return $count;
+    }
+
     /**
      * genera attività in base ai parametri
      *
