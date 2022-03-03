8168cb3d8489a8bb7c3546803133335c42aed96c
federicoistrice <federico.pierantoni@istricesrl.it>
2022-03-03 16:51:49 +0100

    aggiunto form

---
+++ _mod/_3500.risorse/_src/_inc/_macro/_risorse.form.php
@@ -0,0 +1,20 @@
+<?php
+
+    /**
+     *
+     *
+     *
+     * @todo documentare
+     * 
+     *
+     * @file
+     *
+     */
+
+    // tabella gestita
+    $ct['form']['table'] = 'risorse';
+
+    
+
+    // macro di default
+	require DIR_SRC_INC_MACRO . '_default.form.php';
