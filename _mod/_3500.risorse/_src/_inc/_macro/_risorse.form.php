702e4b88a6e0d649c3845e47968a2bebdf09bc13
federicoistrice <federico.pierantoni@istricesrl.it>
2022-03-04 18:00:51 +0100

    debug

--- _mod/_3500.risorse/_src/_inc/_macro/_risorse.form.php
+++ _mod/_3500.risorse/_src/_inc/_macro/_risorse.form.php
@@ -30,5 +30,15 @@
         'SELECT id, __label__ FROM categorie_risorse_view' 
     );
 
+    // tendina mesi
+	foreach( range( 1, 12 ) as $mese ) {
+	    $ct['etc']['select']['mesi'][$mese] = int2month( $mese ) ;
+	}
+
+    // tendina anni
+	foreach( range( date( 'Y' ) - 5,  date( 'Y' ) ) as $y ) {
+	    $ct['etc']['select']['anni'][$y] = $y ;
+	}
+
     // macro di default
 	require DIR_SRC_INC_MACRO . '_default.form.php';
