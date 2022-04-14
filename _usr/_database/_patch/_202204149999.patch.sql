dd6e5c32cc2d35f60d88117c0a9bd7c84b271364
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2022-04-14 17:52:05 +0200

    aggiunta tabella recensioni in patch

--- _usr/_database/_patch/_202204149999.patch.sql
+++ _usr/_database/_patch/_202204149999.patch.sql
@@ -226,4 +226,86 @@ CREATE OR REPLACE VIEW `progetti_articoli_view` AS
 		LEFT JOIN articoli ON articoli.id = progetti_articoli.id_articolo
 		LEFT JOIN prodotti ON prodotti.id = articoli.id_prodotto;
 
+--| 202204140160
+CREATE TABLE IF NOT EXISTS `recensioni` (
+`id` int(11) NOT NULL,
+  `id_lingua` int(11) NOT NULL,
+  `id_prodotto` char(32) DEFAULT NULL,
+  `id_articolo` char(32) DEFAULT NULL,
+  `id_risorsa` int(11) NULL DEFAULT NULL,
+  `id_pagina` int(11) NULL DEFAULT NULL,
+  `autore` char(128) NULL NULL,
+  `valutazione` int(11) NULL NULL,
+  `titolo` char(255) NULL DEFAULT NULL,
+  `testo` text,
+  `se_approvata` tinyint(1) NULL DEFAULT NULL,
+  `id_account_inserimento` int(11) NULL DEFAULT NULL,
+  `timestamp_inserimento` int(11) NULL DEFAULT NULL,
+  `id_account_aggiornamento` int(11) NULL DEFAULT NULL,
+  `timestamp_aggiornamento` int(11) NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--| 202204140170
+ALTER TABLE `recensioni`
+	ADD PRIMARY KEY (`id`), 
+	ADD KEY `id_lingua` (`id_lingua`), 
+	ADD KEY `id_prodotto` (`id_prodotto`), 
+	ADD KEY `id_articolo` (`id_articolo`), 
+	ADD KEY `id_risorsa` (`id_risorsa`), 
+	ADD KEY `id_account_inserimento` (`id_account_inserimento`), 
+	ADD KEY `id_account_aggiornamento` (`id_account_aggiornamento`), 
+	ADD KEY `indice` (`id`,`id_lingua`,`id_prodotto`,`id_articolo`, `id_risorsa` ,`autore`,`valutazione`,`se_approvata`), 
+	ADD KEY `id_pagina` (`id_pagina`);
+
+--| 202204140180
+ALTER TABLE `recensioni` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--| 202204140190
+ALTER TABLE `recensioni`
+ADD CONSTRAINT `recensioni_ibfk_01` FOREIGN KEY (`id_pagina`) REFERENCES `pagine` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
+ADD CONSTRAINT `recensioni_ibfk_02_nofollow` FOREIGN KEY (`id_account_inserimento`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
+ADD CONSTRAINT `recensioni_ibfk_03_nofollow` FOREIGN KEY (`id_account_aggiornamento`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
+ADD CONSTRAINT `recensioni_ibfk_04_nofollow` FOREIGN KEY (`id_prodotto`) REFERENCES `prodotti` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
+ADD CONSTRAINT `recensioni_ibfk_05_nofollow` FOREIGN KEY (`id_lingua`) REFERENCES `lingue` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+ADD CONSTRAINT `recensioni_ibfk_06_nofollow` FOREIGN KEY (`id_articolo`) REFERENCES `articoli` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
+ADD CONSTRAINT `recensioni_ibfk_07_nofollow` FOREIGN KEY (`id_risorsa`) REFERENCES `risorse` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
+;
+
+--| 202204140200
+CREATE OR REPLACE VIEW recensioni_view AS
+    SELECT
+		recensioni.id_lingua ,
+		recensioni.id_prodotto,
+		prodotti.nome AS prodotto,
+		recensioni.id_articolo,
+		concat_ws( ' ', p2.nome, articoli.nome ) AS articolo,
+		recensioni.id_risorsa,
+		risorse.nome AS risorsa,
+		recensioni.id_pagina,
+		recensioni.autore,
+		recensioni.valutazione,
+		recensioni.titolo ,
+		recensioni.id_account_inserimento ,
+		recensioni.id_account_aggiornamento ,
+		from_unixtime( recensioni.timestamp_inserimento, '%Y-%m-%d %H:%i' ) AS data_ora_recensione,
+		concat_ws( '|', recensioni.autore, recensioni.titolo ) AS __label__
+    FROM recensioni
+    	LEFT JOIN articoli ON articoli.id = recensioni.id_articolo
+    	LEFT JOIN prodotti AS p2 ON p2.id = articoli.id_prodotto
+		LEFT JOIN prodotti ON prodotti.id = recensioni.id_prodotto
+		LEFT JOIN risorse ON risorse.id = recensioni.id_risorsa
+;
+
+--| 202204140210
+ALTER TABLE `risorse`
+ADD `id_sito` INT NULL DEFAULT NULL AFTER `note`,
+ADD `template` CHAR(255) NULL DEFAULT NULL AFTER `id_sito`,
+ADD `schema_html` CHAR(128) NULL DEFAULT NULL AFTER `template`,
+ADD `tema_css` CHAR(32) NULL DEFAULT NULL AFTER `schema_html`,
+ADD `se_sitemap` INT(1) NULL DEFAULT NULL AFTER `tema_css`,
+ADD `se_cacheable` INT(1) NULL DEFAULT NULL AFTER `se_sitemap`,
+ADD KEY `id_sito` (`id_sito`),
+ADD KEY `se_sitemap` (`se_sitemap`),
+ADD KEY `se_cacheable` (`se_cacheable`);
+
 -- FINE
