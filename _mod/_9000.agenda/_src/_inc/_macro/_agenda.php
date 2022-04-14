f2e7e4e4b4c9aeaef8957ad0f099c6b5fa5ab796
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2022-04-14 14:55:42 +0200

    debug e migliorie

--- _mod/_9000.agenda/_src/_inc/_macro/_agenda.php
+++ _mod/_9000.agenda/_src/_inc/_macro/_agenda.php
@@ -24,14 +24,14 @@
 			if( !empty($ct['etc']['attivita']) ){
 				$todo = array();
 				foreach( $ct['etc']['attivita'] as $a ){
-					$todo[] = $a['id_todo'];
+					if(!empty( $a['id_todo'])){$todo[] = $a['id_todo'];}	
 				}		
 			}
 
 		$ct['etc']['todo'] = mysqlQuery(
 			$cf['mysql']['connection'],
 			'SELECT \'t\' AS entita, todo.testo, todo.id,todo.id_tipologia,tipologie_todo.nome AS tipologia,todo.id_anagrafica,coalesce( a1.denominazione, concat( a1.cognome, \' \', a1.nome ), \'\' ) AS anagrafica,todo.id_cliente,coalesce( a2.denominazione, concat( a2.cognome, \' \', a2.nome ), \'\' ) AS cliente,todo.id_indirizzo,concat_ws(\'\',indirizzo,indirizzi.civico,indirizzi.cap,indirizzi.localita,comuni.nome,provincie.sigla) AS indirizzo,todo.id_luogo,luoghi_path( todo.id_luogo ) AS luogo,todo.data_scadenza,todo.ora_scadenza,todo.data_programmazione,todo.ora_inizio_programmazione,todo.ora_fine_programmazione,todo.anno_programmazione,todo.settimana_programmazione,todo.ore_programmazione,todo.data_chiusura,todo.nome,todo.note_programmazione,todo.id_contatto,todo.id_progetto,todo.id_pianificazione,todo.data_archiviazione,todo.id_account_inserimento,todo.id_account_aggiornamento,concat(todo.nome,\' per \',coalesce( a2.denominazione, concat( a2.cognome, \' \', a2.nome ), \'\' ),\' su \',todo.id_progetto) AS __label__,count(attivita.id) AS n_attivita FROM todo LEFT JOIN attivita ON attivita.id_todo = todo.id LEFT JOIN anagrafica AS a1 ON a1.id = todo.id_anagrafica LEFT JOIN anagrafica AS a2 ON a2.id = todo.id_cliente LEFT JOIN indirizzi ON indirizzi.id = todo.id_indirizzo LEFT JOIN comuni ON comuni.id = indirizzi.id_comune LEFT JOIN provincie ON provincie.id = comuni.id_provincia LEFT JOIN tipologie_todo ON tipologie_todo.id = todo.id_tipologia '.
-			'WHERE attivita.data_attivita IS NULL AND (todo.id_anagrafica = ? AND attivita.id_anagrafica_programmazione <> ?) AND todo.data_chiusura IS NULL'.( isset($todo) ? ' AND todo.id NOT IN ('.implode(',',$todo).')' : '' ).'GROUP BY todo.id  HAVING n_attivita > 0',
+			'WHERE attivita.data_attivita IS NULL AND (todo.id_anagrafica = ? AND attivita.id_anagrafica_programmazione <> ?) AND todo.data_chiusura IS NULL '.( isset($todo) && count($todo)>0 ? ' AND todo.id NOT IN ('.implode(',',$todo).') ' : ' ' ).' GROUP BY todo.id  HAVING n_attivita > 0',
 			array( array( 's' => $_SESSION['account']['id_anagrafica'] ),  array( 's' => $_SESSION['account']['id_anagrafica'] )  ) );
 
 		} else {
@@ -40,7 +40,7 @@
 		$ct['etc']['attivita'] = mysqlQuery(
 			$cf['mysql']['connection'],
 			'SELECT \'a\' AS entita,a2.telefoni, a2.mail,  concat( todo.id_progetto, \" \" ,todo.nome ) AS todo, todo.testo AS testo_todo, attivita.note_programmazione,  attivita.id, attivita.id_tipologia, tipologie_attivita.nome AS tipologia, attivita.id_cliente, coalesce( a2.denominazione , concat( a2.cognome, \' \', a2.nome ), \'\' ) AS cliente, attivita.id_indirizzo, indirizzi.indirizzo AS indirizzo, attivita.id_luogo, luoghi_path( attivita.id_luogo ) AS luogo, attivita.data_programmazione, attivita.ora_inizio_programmazione, attivita.ora_fine_programmazione, attivita.id_anagrafica_programmazione, coalesce( a3.denominazione , concat( a3.cognome, \' \', a3.nome ), \'\' ) AS anagrafica_programmazione, attivita.ore_programmazione, attivita.data_attivita, day( data_attivita ) as giorno_attivita, month( data_attivita ) as mese_attivita, year( data_attivita ) as anno_attivita, attivita.ora_inizio, attivita.ora_fine, attivita.id_anagrafica, coalesce( a1.denominazione , concat( a1.cognome, \' \', a1.nome ), \'\' ) AS anagrafica, attivita.ore, attivita.nome, attivita.id_progetto, progetti.nome AS progetto, attivita.id_todo, attivita.id_account_inserimento, attivita.id_account_aggiornamento, concat( attivita.nome, \' / \', attivita.ore, \' / \', coalesce( a1.denominazione , concat( a1.cognome, \' \', a1.nome ), \'\' ) ) AS __label__ FROM attivita LEFT JOIN tipologie_attivita ON tipologie_attivita.id = attivita.id_tipologia LEFT JOIN anagrafica AS a1 ON a1.id = attivita.id_anagrafica LEFT JOIN anagrafica_view_static AS a2 ON a2.id = attivita.id_cliente LEFT JOIN anagrafica AS a3 ON a3.id = attivita.id_anagrafica_programmazione LEFT JOIN progetti_categorie ON progetti_categorie.id_progetto = attivita.id_progetto LEFT JOIN categorie_progetti ON categorie_progetti.id = progetti_categorie.id_categoria LEFT JOIN progetti ON progetti.id = attivita.id_progetto LEFT JOIN todo ON todo.id = attivita.id_todo LEFT JOIN indirizzi ON indirizzi.id = attivita.id_indirizzo '.
-			'WHERE attivita.data_attivita IS NULL ORDER BY attivita.data_programmazione, attivita.ora_inizio_programmazione',
+			'WHERE attivita.data_attivita IS NULL ORDER BY attivita.data_programmazione, attivita.ora_inizio_programmazione'
 		);
 	
 		$ct['etc']['agenda_da_fissare']['todo'] = mysqlQuery(
