1c0f27bbf1de418d2db77231494174f72b2b2c71
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2022-04-19 11:07:31 +0200

    debug form inserimento contratti

--- _mod/_0600.contratti/_src/_inc/_macro/_contratti.form.php
+++ _mod/_0600.contratti/_src/_inc/_macro/_contratti.form.php
@@ -19,47 +19,32 @@
 	    $cf['memcache']['index'],
 	    $cf['memcache']['connection'],
 	    $cf['mysql']['connection'],
-	    'SELECT id, __label__ FROM anagrafica_view_static WHERE se_collaboratore = 1 OR se_dipendente = 1 OR se_interinale = 1'
+	    'SELECT id, __label__ FROM anagrafica_view_static'
     );
 
-    // tendina agenzia
+    // tendina emittenti
 	$ct['etc']['select']['agenzia'] = mysqlCachedIndexedQuery(
 	    $cf['memcache']['index'],
 	    $cf['memcache']['connection'],
 	    $cf['mysql']['connection'],
-	    'SELECT id, __label__ FROM anagrafica_view_static WHERE se_agenzia_interinale = 1'
+	    'SELECT id, __label__ FROM anagrafica_view_static '
     );
     
+    // tendina progetti
+	$ct['etc']['select']['progetti'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM progetti_view '
+    );
+    
+
     // tendina per le tipologie di contratto
     $ct['etc']['select']['tipologie_contratti'] = mysqlCachedIndexedQuery(
 	    $cf['memcache']['index'],
 	    $cf['memcache']['connection'],
 	    $cf['mysql']['connection'],
 	    'SELECT id, __label__ FROM tipologie_contratti_view'
-    );
-    
-    // tendina per le tipologie di qualifiche inps
-    $ct['etc']['select']['tipologie_qualifiche_inps'] = mysqlCachedIndexedQuery(
-	    $cf['memcache']['index'],
-	    $cf['memcache']['connection'],
-	    $cf['mysql']['connection'],
-	    'SELECT id, __label__ FROM tipologie_qualifiche_inps_view'
-    );
-
-    // tendina per le tipologie di durate inps
-    $ct['etc']['select']['tipologie_durate_inps'] = mysqlCachedIndexedQuery(
-	    $cf['memcache']['index'],
-	    $cf['memcache']['connection'],
-	    $cf['mysql']['connection'],
-	    'SELECT id, __label__ FROM tipologie_durate_inps_view'
-    );
-
-    // tendina per le tipologie di orari inps
-    $ct['etc']['select']['tipologie_orari_inps'] = mysqlCachedIndexedQuery(
-	    $cf['memcache']['index'],
-	    $cf['memcache']['connection'],
-	    $cf['mysql']['connection'],
-	    'SELECT id, __label__ FROM tipologie_orari_inps_view'
     );
 
     // macro di default per l'entità contratti
