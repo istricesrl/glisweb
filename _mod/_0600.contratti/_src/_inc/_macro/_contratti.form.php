b36665314bb7653b9b4e6a98939fa9df2ead05dc
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2022-04-21 14:35:20 +0200

    completato debug sezione immobili

--- _mod/_0600.contratti/_src/_inc/_macro/_contratti.form.php
+++ _mod/_0600.contratti/_src/_inc/_macro/_contratti.form.php
@@ -19,7 +19,7 @@
 	    $cf['memcache']['index'],
 	    $cf['memcache']['connection'],
 	    $cf['mysql']['connection'],
-	    'SELECT id, __label__ FROM anagrafica_view_static WHERE se_collaboratore = 1 OR se_dipendente = 1 OR se_interinale = 1'
+	    'SELECT id, __label__ FROM anagrafica_view_static '
     );
 
     // tendina agenzia
@@ -27,7 +27,7 @@
 	    $cf['memcache']['index'],
 	    $cf['memcache']['connection'],
 	    $cf['mysql']['connection'],
-	    'SELECT id, __label__ FROM anagrafica_view_static WHERE se_agenzia_interinale = 1'
+	    'SELECT id, __label__ FROM anagrafica_view_static '
     );
     
     // tendina per le tipologie di contratto
@@ -36,30 +36,6 @@
 	    $cf['memcache']['connection'],
 	    $cf['mysql']['connection'],
 	    'SELECT id, __label__ FROM tipologie_contratti_view'
-    );
-    
-    // tendina per le tipologie di qualifiche inps
-    $ct['etc']['select']['tipologie_qualifiche_inps'] = mysqlCachedIndexedQuery(
-	    $cf['memcache']['index'],
-	    $cf['memcache']['connection'],
-	    $cf['mysql']['connection'],
-	    'SELECT id, __label__ FROM tipologie_qualifiche_inps_view'
-    );
-
-    // tendina per le tipologie di durate inps
-    $ct['etc']['select']['tipologie_durate_inps'] = mysqlCachedIndexedQuery(
-	    $cf['memcache']['index'],
-	    $cf['memcache']['connection'],
-	    $cf['mysql']['connection'],
-	    'SELECT id, __label__ FROM tipologie_durate_inps_view'
-    );
-
-    // tendina per le tipologie di orari inps
-    $ct['etc']['select']['tipologie_orari_inps'] = mysqlCachedIndexedQuery(
-	    $cf['memcache']['index'],
-	    $cf['memcache']['connection'],
-	    $cf['mysql']['connection'],
-	    'SELECT id, __label__ FROM tipologie_orari_inps_view'
     );
 
     // macro di default per l'entità contratti
