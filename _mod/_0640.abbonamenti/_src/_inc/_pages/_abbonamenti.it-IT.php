7a84741c2a491fd79cc0c7286a94feb76f78426a
federicoistrice <federico.pierantoni@istricesrl.it>
2022-04-29 11:58:06 +0200

    moduli spostati sotto corsi

--- _mod/_0640.abbonamenti/_src/_inc/_pages/_abbonamenti.it-IT.php
+++ _mod/_0640.abbonamenti/_src/_inc/_pages/_abbonamenti.it-IT.php
@@ -18,45 +18,49 @@
 	    'etc'				=> array( 'tabs'	=> 'anagrafica.form' )
 	);
 
-    // vista abbonamenti
-	$p['abbonamenti.view']	= array(
-	    'sitemap'			=> false,
-	    'title'				=> array( $l		=> 'abbonamenti' ),
-	    'h1'				=> array( $l		=> 'abbonamenti' ),
-	    'parent'			=> array( 'id'		=> 'amministrazione' ),
-	    'template'			=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'default.view.html' ),
-	    'macro'				=> array( '_src/_inc/_macro/_abbonamenti.view.php' ),
-	    'auth'				=> array( 'groups'	=> array(	'roots', 'staff' ) ),
-	    'etc'				=> array( 'tabs'	=> array(	'abbonamenti.view',
-															'abbonamenti.archivio.view',
-															'abbonamenti.stampe',
-															'abbonamenti.tools' ) ),
-	    'menu'				=> array( 'admin'	=> array(	'' => 	array(	'label'		=> array( $l => 'abbonamenti' ),
-																			'priority'	=> '050' ) ) )
-	);
+	// RELAZIONI CON IL MODULO CORSI
+	if( in_array( "0920.corsi", $cf['mods']['active']['array'] ) ) {
 
-    // vista archivio abbonamenti
-	$p['abbonamenti.archivio.view'] = array(
-	    'sitemap'			=> false,
-		'icon'				=> '<i class="fa fa-archive" aria-hidden="true"></i>',
-		'title'				=> array( $l		=> 'archivio' ),
-	    'h1'				=> array( $l		=> 'archivio' ),
-	    'parent'			=> array( 'id'		=> 'abbonamenti.view' ),
-	    'template'			=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'default.view.html' ),
-	    'macro'				=> array( '_src/_inc/_macro/_abbonamenti.archivio.view.php' ),
-	    'auth'				=> array( 'groups'	=> array(	'roots', 'staff' ) ),
-	    'etc'				=> array( 'tabs'	=> $p['abbonamenti.view']['etc']['tabs'] )
-	);
+		// vista abbonamenti
+		$p['abbonamenti.view']	= array(
+			'sitemap'			=> false,
+			'title'				=> array( $l		=> 'abbonamenti' ),
+			'h1'				=> array( $l		=> 'abbonamenti' ),
+			'parent'			=> array( 'id'		=> 'corsi' ),
+			'template'			=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'default.view.html' ),
+			'macro'				=> array( $m . '_src/_inc/_macro/_abbonamenti.view.php' ),
+			'auth'				=> array( 'groups'	=> array(	'roots', 'staff' ) ),
+			'etc'				=> array( 'tabs'	=> array(	'abbonamenti.view',
+																'abbonamenti.archivio.view',
+																'abbonamenti.stampe',
+																'abbonamenti.tools' ) ),
+			'menu'				=> array( 'admin'	=> array(	'' => 	array(	'label'		=> array( $l => 'abbonamenti' ),
+																				'priority'	=> '050' ) ) )
+		);
 
-	// stampe abbonamenti
-	$p['abbonamenti.stampe'] = array(
-	    'sitemap'			=> false,
-		'icon'				=> '<i class="fa fa-print" aria-hidden="true"></i>',
-		'title'				=> array( $l		=> 'stampe' ),
-	    'h1'				=> array( $l		=> 'stampe' ),
-	    'parent'			=> array( 'id'		=> 'abbonamenti.view' ),
-	    'template'			=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'default.tools.html' ),
-	    'macro'				=> array( '_src/_inc/_macro/_abbonamenti.stampe.php' ),
-	    'auth'				=> array( 'groups'	=> array(	'roots', 'staff' ) ),
-	    'etc'				=> array( 'tabs'	=> $p['abbonamenti.view']['etc']['tabs'] )
-	);
+		// vista archivio abbonamenti
+		$p['abbonamenti.archivio.view'] = array(
+			'sitemap'			=> false,
+			'icon'				=> '<i class="fa fa-archive" aria-hidden="true"></i>',
+			'title'				=> array( $l		=> 'archivio' ),
+			'h1'				=> array( $l		=> 'archivio' ),
+			'parent'			=> array( 'id'		=> 'abbonamenti.view' ),
+			'template'			=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'default.view.html' ),
+			'macro'				=> array( $m . '_src/_inc/_macro/_abbonamenti.archivio.view.php' ),
+			'auth'				=> array( 'groups'	=> array(	'roots', 'staff' ) ),
+			'etc'				=> array( 'tabs'	=> $p['abbonamenti.view']['etc']['tabs'] )
+		);
+
+		// stampe abbonamenti
+		$p['abbonamenti.stampe'] = array(
+			'sitemap'			=> false,
+			'icon'				=> '<i class="fa fa-print" aria-hidden="true"></i>',
+			'title'				=> array( $l		=> 'stampe' ),
+			'h1'				=> array( $l		=> 'stampe' ),
+			'parent'			=> array( 'id'		=> 'abbonamenti.view' ),
+			'template'			=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'default.tools.html' ),
+			'macro'				=> array( $m . '_src/_inc/_macro/_abbonamenti.stampe.php' ),
+			'auth'				=> array( 'groups'	=> array(	'roots', 'staff' ) ),
+			'etc'				=> array( 'tabs'	=> $p['abbonamenti.view']['etc']['tabs'] )
+		);
+	}
