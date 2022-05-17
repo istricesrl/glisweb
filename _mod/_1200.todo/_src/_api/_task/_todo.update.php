abfeb73e251e5d0aee528a3cb57cba58b226e151
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2022-05-17 13:08:02 +0200

    completato codice per modifica todo in caso non sia specificato il giorno

--- _mod/_1200.todo/_src/_api/_task/_todo.update.php
+++ _mod/_1200.todo/_src/_api/_task/_todo.update.php
@@ -181,7 +181,7 @@
 
                     $status['action'][] = 'sostituzione istruttore';
                     $status['date'] = $restult;
-                    
+
                     // aggiornamento todo ed attivita
                     foreach( $restult as $data ){
 
@@ -245,6 +245,210 @@
 
             } else {
                 // tutti i giorni
+                if( ! empty( $_REQUEST['__l__'] ) ){
+
+                        $status['action'][] = 'modifica luogo';
+
+                        $where = array();
+                        $params = array();
+
+                        $params[] = array( 's' => $_REQUEST['__l__'] );
+                        $params[] = array( 's' => $_REQUEST['__l__'] );
+                        $params[] = array( 's' => $_REQUEST['progetto'] );
+
+                        
+                        if(! empty( $_REQUEST['__o_i__'] )){
+                            $where[] = 'todo.ora_inizio_programmazione = ?';
+                            $params[] = array( 's' => $_REQUEST['__o_i__'] );
+                        }  
+                        if(! empty( $_REQUEST['__o_f__'] )){
+                            $where[] = 'todo.ora_fine_programmazione = ?';
+                            $params[] = array( 's' => $_REQUEST['__o_f__'] );
+                        }  
+
+                        if(!empty($where)){$where = ' AND '.implode(' AND ', $where);}
+                        else{ $where = '';}
+
+                        $params[] = array( 's' => $_REQUEST['__d_i__'] );
+                        $params[] = array( 's' => $_REQUEST['__d_f__'] );
+
+                        mysqlQuery( $cf['mysql']['connection'],
+                            'UPDATE attivita, todo SET todo.id_luogo = ?, attivita.id_luogo = ? WHERE todo.id = attivita.id_todo AND todo.id_progetto = ? '.$where.' AND  todo.data_programmazione BETWEEN ? AND ?  ',
+                            $params
+                        );
+
+
+                    } 
+                
+                // se devo aggiungere un istruttore
+                if( ! empty( $_REQUEST['__addi__'] ) ){
+
+                    $status['action'][] = 'aggiunta istruttore';
+
+                    // aggiornamento todo ed attivita
+                    foreach( $restult as $data ){
+
+                        $where = array();
+                        $params = array();
+
+                        $params[] = array( 's' => $_REQUEST['progetto'] );
+                        $params[] = array( 's' => $_REQUEST['__d_i__'] );
+                        $params[] = array( 's' => $_REQUEST['__d_f__'] );
+                        
+                        if(! empty( $_REQUEST['__o_i__'] )){
+                            $where[] = 'ora_inizio_programmazione = ?';
+                            $params[] = array( 's' => $_REQUEST['__o_i__'] );
+                        }  
+                        if(! empty( $_REQUEST['__o_f__'] )){
+                            $where[] = 'ora_fine_programmazione = ?';
+                            $params[] = array( 's' => $_REQUEST['__o_f__'] );
+                        }  
+
+                        if(!empty($where)){$where = ' AND '.implode(' AND ', $where);}
+                        else{ $where = '';}
+
+
+
+                        $t = mysqlQuery( $cf['mysql']['connection'],
+                        'SELECT * FROM todo WHERE id_progetto = ? AND (data_programmazione BETWEEN ? AND ?) '.$where,
+                        $params
+                        );
+                        
+                        if( $t ){
+                            foreach($t as $todo){
+                                mysqlQuery( $cf['mysql']['connection'], 
+                                'INSERT INTO attivita (id_anagrafica_programmazione, id_luogo, id_todo, id_progetto, ora_inizio_programmazione, ora_fine_programmazione, data_programmazione, id_tipologia) VALUES ( ?, ?, ?, ?, ?, ?, ?, ? )',
+                                    array(  
+                                        array( 's' => $_REQUEST['__addi__'] ),
+                                        array( 's' => $todo['id_luogo'] ), 
+                                        array( 's' => $todo['id'] ),                             
+                                        array( 's' => $todo['id_progetto'] ), 
+                                        array( 's' => $todo['ora_inizio_programmazione'] ), 
+                                        array( 's' => $todo['ora_fine_programmazione'] ), 
+                                        array( 's' => $data ),
+                                        array( 's' => 1 )
+                                    )
+                                );
+    
+                            }
+
+                        }
+
+                    }
+
+                }
+
+                // se devo eliminare un istruttore
+                if( ! empty( $_REQUEST['__removei__'] ) ){
+
+                    // aggiornamento todo ed attivita
+                    foreach( $restult as $data ){
+
+                        $where = array();
+                        $params = array();
+
+                        $params[] = array( 's' => $_REQUEST['progetto'] );
+                        $params[] = array( 's' => $_REQUEST['__d_i__'] );
+                        $params[] = array( 's' => $_REQUEST['__d_f__'] );
+
+                        
+                        if(! empty( $_REQUEST['__o_i__'] )){
+                            $where[] = 'ora_inizio_programmazione = ?';
+                            $params[] = array( 's' => $_REQUEST['__o_i__'] );
+                        }  
+                        if(! empty( $_REQUEST['__o_f__'] )){
+                            $where[] = 'ora_fine_programmazione = ?';
+                            $params[] = array( 's' => $_REQUEST['__o_f__'] );
+                        }  
+
+                        if(!empty($where)){$where = ' AND '.implode(' AND ', $where);}
+                        else{ $where = '';}
+
+                        $todo = mysqlQuery( $cf['mysql']['connection'],
+                        'SELECT id FROM todo WHERE id_progetto = ? AND (data_programmazione BETWEEN ? AND ?) '.$where,
+                        $params
+                        );
+                        
+                        if( $todo ){
+
+                            foreach($todo as $t){
+                                $params[0] = array( 's' => $_REQUEST['__removei__'] );
+                                mysqlQuery(  $cf['mysql']['connection'],
+                                'DELETE FROM attivita WHERE id_anagrafica_programmazione = ?  AND (data_programmazione BETWEEN ? AND ?)  '.$where.' AND id_todo = ?',
+                                $params
+                                );
+                                $params[count($params)] = array( 's' => $t['id'] );
+
+                            }
+
+                            //ora_inizio_programmazione, ora_fine_programmazione, data_programmazione
+                        }
+
+                    }
+
+                }
+
+                // se devo sostituire un instruttore
+                if( ! empty( $_REQUEST['__deletei__'] ) && ! empty( $_REQUEST['__newi__'] ) ){
+
+                    $status['action'][] = 'sostituzione istruttore';
+                    $status['date'] = $restult;
+                    
+
+                        $where = array();
+                        $params = array();
+
+                        if(! empty( $_REQUEST['__o_i__'] )){
+                            $where[] = 'ora_inizio_programmazione = ?';
+                            $params[] = array( 's' => $_REQUEST['__o_i__'] );
+                        }  
+                        if(! empty( $_REQUEST['__o_f__'] )){
+                            $where[] = 'ora_fine_programmazione = ?';
+                            $params[] = array( 's' => $_REQUEST['__o_f__'] );
+                        }  
+
+                        $params[] = array( 's' => $_REQUEST['progetto'] );
+                        $params[] = array( 's' => $_REQUEST['__d_i__'] );
+                        $params[] = array( 's' => $_REQUEST['__d_f__'] );
+
+                        
+
+                        if(!empty($where)){$where = implode(' AND ', $where).' AND ';}
+                        else{ $where = '';}
+
+                        $todo = mysqlQuery( $cf['mysql']['connection'],
+                        'SELECT id FROM todo WHERE '.$where.' id_progetto = ? (AND data_programmazione BETWEEN ? AND ?) ',
+                        $params
+                        );
+                        
+                        $status['todo'] = $todo;
+
+                        if( $todo ){
+
+                            foreach($todo as $t){
+    
+                                mysqlQuery(  $cf['mysql']['connection'],
+                                'UPDATE attivita SET id_anagrafica_programmazione = ? WHERE  id_anagrafica_programmazione = ?   AND id_todo = ?',
+                                array(
+                                    array( 's' => $_REQUEST['__newi__'] ),
+                                    array( 's' => $_REQUEST['__deletei__'] ),
+                                    array( 's' => $t['id'] )
+                                )
+                                );
+                            }
+
+
+                        }
+
+
+                }
+
+                // aggiornare todo_view_static
+                mysqlQuery( $cf['mysql']['connection'], 'CALL todo_view_static( ? )', array( array( 's' => NULL ) ) );
+                logWrite( 'aggiornata todo view statica per tutti i record', 'speed' );
+
+                mysqlQuery( $cf['mysql']['connection'], 'CALL attivita_view_static( ? )', array( array( 's' => NULL ) ) );
+                logWrite( 'aggiornata attivita view statica per tutti i record', 'speed' );
 
 
             }
