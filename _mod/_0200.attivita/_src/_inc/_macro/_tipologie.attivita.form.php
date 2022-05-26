a07fa6566ebbc7410bfc22e4cc35fca37ffdad84
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2022-05-26 09:48:30 +0200

    debug form tipologie_attivita

--- _mod/_0200.attivita/_src/_inc/_macro/_tipologie.attivita.form.php
+++ _mod/_0200.attivita/_src/_inc/_macro/_tipologie.attivita.form.php
@@ -13,6 +13,22 @@
     // tabella gestita
     $ct['form']['table'] = 'tipologie_attivita';
 
-  
+    if( isset( $_REQUEST[ $ct['form']['table'] ]['id'] ) && !empty( $_REQUEST[ $ct['form']['table'] ]['id'] ) ){
+        $ct['etc']['select']['genitori'] = mysqlCachedIndexedQuery(
+            $cf['memcache']['index'],
+            $cf['memcache']['connection'],
+            $cf['mysql']['connection'],
+            'SELECT id, __label__ FROM tipologie_attivita_view  WHERE id <> ?',
+            array( array( 's' => $_REQUEST[ $ct['form']['table'] ]['id'] ) )
+        );
+    } else {
+        $ct['etc']['select']['genitori'] = mysqlCachedIndexedQuery(
+            $cf['memcache']['index'],
+            $cf['memcache']['connection'],
+            $cf['mysql']['connection'],
+            'SELECT id, __label__ FROM tipologie_attivita_view'
+        );
+    }
+
     // macro di default
 	require DIR_SRC_INC_MACRO . '_default.form.php';
