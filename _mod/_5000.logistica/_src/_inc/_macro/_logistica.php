0bda020b7247946802ac04993e63dca6c2d6b1d5
federicoistrice <federico.pierantoni@istricesrl.it>
2022-05-27 14:18:26 +0200

    aggiunta azione rapida dashboard logistica

--- _mod/_5000.logistica/_src/_inc/_macro/_logistica.php
+++ _mod/_5000.logistica/_src/_inc/_macro/_logistica.php
@@ -1,1 +1,36 @@
 <?php
+
+    /**
+     * macro dashboard logistica
+     *
+     *
+     *
+     *
+     * @todo implementare
+     * @todo documentare
+     *
+     * @file
+     *
+     */
+
+    // gruppi di controlli
+	$ct['page']['contents']['metros'] = array(
+	    'notifiche' => array(
+			'label' => 'notifiche'
+		),
+	    'scorciatoie' => array(
+			'label' => 'azioni rapide'
+		)
+	);
+
+	if( in_array( "0400.documenti", $cf['mods']['active']['array'] ) ) {
+
+        // inserimento nuovo DDT
+        $ct['page']['contents']['metro']['scorciatoie'][] = array(
+            'url' => $cf['contents']['pages']['ddt.magazzini.form']['url'][ LINGUA_CORRENTE ],
+            'icon' => NULL,
+            'fa' => 'fa-bolt',
+            'title' => 'inserimento DDT',
+            'text' => 'inserisce un nuovo DDT'
+        );
+    }
