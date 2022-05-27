c16b53bc1e31936b4cb057a4147cb71a3b39d396
federicoistrice <federico.pierantoni@istricesrl.it>
2022-05-27 13:10:00 +0200

    aggiunte scorciatoie dashboard principale

--- _src/_inc/_macro/_dashboard.php
+++ _src/_inc/_macro/_dashboard.php
@@ -32,8 +32,8 @@
 	    'text' => 'inserisce una nuova anagrafica'
 	);
 
-	// RELAZIONI CON IL MODULO 
-	if( in_array( "0400.documenti", $cf['mods']['active']['array'] ) ) {
+	// RELAZIONI CON IL MODULO DOCUMENTI
+	if( in_array( "6000.amministrazione", $cf['mods']['active']['array'] ) && in_array( "0400.documenti", $cf['mods']['active']['array'] ) ) {
 
         // inserimento nuova fattura
         $ct['page']['contents']['metro']['scorciatoie'][] = array(
@@ -42,6 +42,34 @@
             'fa' => 'fa-bolt',
             'title' => 'inserimento fattura',
             'text' => 'inserisce una nuova fattura'
+        );
+
+    }
+
+    // RELAZIONI CON IL MODULO LOGISTICA
+	if( in_array( "5000.logistica", $cf['mods']['active']['array'] ) && in_array( "0400.documenti", $cf['mods']['active']['array'] ) ) {
+
+        // inserimento nuovo DDT
+        $ct['page']['contents']['metro']['scorciatoie'][] = array(
+            'url' => $cf['contents']['pages']['ddt.magazzini.form']['url'][ LINGUA_CORRENTE ],
+            'icon' => NULL,
+            'fa' => 'fa-bolt',
+            'title' => 'inserimento DDT',
+            'text' => 'inserisce un nuovo DDT'
+        );
+        
+    }
+
+    // RELAZIONI CON IL MODULO MAILING
+	if( in_array( "7000.mailing", $cf['mods']['active']['array'] ) ) {
+
+        // inserimento nuovo invio mailing
+        $ct['page']['contents']['metro']['scorciatoie'][] = array(
+            'url' => $cf['contents']['pages']['mailing.form']['url'][ LINGUA_CORRENTE ],
+            'icon' => NULL,
+            'fa' => 'fa-bolt',
+            'title' => 'crea mailing',
+            'text' => 'crea nuovo invio mailing'
         );
 
     }
