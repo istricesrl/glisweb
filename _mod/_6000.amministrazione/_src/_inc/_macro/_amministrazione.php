5ab0200dd37b6a5285b41e9c230f88651675faa1
federicoistrice <federico.pierantoni@istricesrl.it>
2022-05-27 14:18:16 +0200

    aggiunta azione rapida dashboard amministrazione

--- _mod/_6000.amministrazione/_src/_inc/_macro/_amministrazione.php
+++ _mod/_6000.amministrazione/_src/_inc/_macro/_amministrazione.php
@@ -1,1 +1,36 @@
 <?php
+
+    /**
+     * macro dashboard amministrazione
+     *
+     *
+     *
+     *
+     * @todo implementare
+     * @todo documentare
+     *
+     * @file
+     *
+     */
+
+    // gruppi di controlli
+	$ct['page']['contents']['metros'] = array(
+	    'notifiche' => array(
+			'label' => 'notifiche'
+		),
+	    'scorciatoie' => array(
+			'label' => 'azioni rapide'
+		)
+	);
+
+	if( in_array( "0400.documenti", $cf['mods']['active']['array'] ) ) {
+
+        // inserimento nuova fattura
+        $ct['page']['contents']['metro']['scorciatoie'][] = array(
+            'url' => $cf['contents']['pages']['fatture.amministrazione.form']['url'][ LINGUA_CORRENTE ],
+            'icon' => NULL,
+            'fa' => 'fa-bolt',
+            'title' => 'inserimento fattura',
+            'text' => 'inserisce una nuova fattura'
+        );
+    }
