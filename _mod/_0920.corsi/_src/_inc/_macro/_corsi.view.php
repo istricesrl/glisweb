2503bf6acdb7fd41a0368aa512a41a545074386f
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2022-06-24 12:34:15 +0200

    aggiunto filtri fasce, livelli e discipline a corsi

--- _mod/_0920.corsi/_src/_inc/_macro/_corsi.view.php
+++ _mod/_0920.corsi/_src/_inc/_macro/_corsi.view.php
@@ -32,7 +32,9 @@
         'id' => '#',
         'tipologia' => 'tipologia',
         'nome' => 'nome',
-        'categorie' => 'categorie'
+        'fasce' => 'fasce',
+        'discipline' => 'discipline',
+        'livelli' => 'livelli'      
 	);
 
     // stili della vista
@@ -43,6 +45,30 @@
         'nome' => 'text-left',
         'categorie' => 'text-left'
     );
+
+        // tendina categorie progetti
+	$ct['etc']['select']['discipline'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM categorie_progetti_view WHERE se_disciplina = 1'
+	);
+
+	$ct['etc']['select']['classi'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM categorie_progetti_view WHERE se_classe = 1'
+	);
+
+	$ct['etc']['select']['fasce'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM categorie_progetti_view WHERE se_fascia = 1'
+	);
+
+    $ct['etc']['include']['filters'] = 'inc/corsi.view.filters.html';
 
     // macro di default
 	require DIR_SRC_INC_MACRO . '_default.view.php';
