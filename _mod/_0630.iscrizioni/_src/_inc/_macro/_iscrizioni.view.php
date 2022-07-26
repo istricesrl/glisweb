7e66b9283599231061e9ceb937c5b1168793ff03
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2022-07-26 11:03:44 +0200

    miglioria vista

--- _mod/_0630.iscrizioni/_src/_inc/_macro/_iscrizioni.view.php
+++ _mod/_0630.iscrizioni/_src/_inc/_macro/_iscrizioni.view.php
@@ -30,7 +30,7 @@
     // campi della vista
 	$ct['view']['cols'] = array(
         'id' => '#',
-        'tipologia' => 'tipologia',
+   //     'tipologia' => 'tipologia',
         'destinatario' => 'anagrafica',
         'data_inizio' => 'data',
         'progetto' => 'corso'
@@ -39,10 +39,16 @@
     // stili della vista
 	$ct['view']['class'] = array(
         'id' => 'd-none d-md-table-cell',
-        'codice' => 'text-left d-none d-md-table-cell',
-        'id_destinatario' => 'text-left'
+        'destinatario' => 'text-left',
+        'data_inizio' => 'text-left',
+        'progetto' => 'text-left'
     );
 
     // macro di default
 	require DIR_SRC_INC_MACRO . '_default.view.php';
 
+    if( !empty( $ct['view']['data'] ) ){
+		foreach ( $ct['view']['data'] as &$row ){
+             if(!empty($row['data_inizio'])){$row['data_inizio'] = date('d/m/Y', strtotime($row['data_inizio']));}
+          	}
+	}
