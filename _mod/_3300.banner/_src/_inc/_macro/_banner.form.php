8ca9199150a1438e16e0adcbdfa14b2f5eb39c4c
Chiara Giardino Di Lollo <chiara.giardinodilollo@istricesrl.it>
2022-07-28 14:48:40 +0200

    aggiunta gestione banner

---
+++ _mod/_3300.banner/_src/_inc/_macro/_banner.form.php
@@ -0,0 +1,35 @@
+<?php
+
+    /**
+     *
+     *
+     *
+     * @todo documentare
+     * 
+     *
+     * @file
+     *
+     */
+
+    // tabella gestita
+    $ct['form']['table'] = 'banner';
+
+    // tendina tipologie banner
+	$ct['etc']['select']['tipologie_banner'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM tipologie_banner_view'
+    );
+
+	$ct['etc']['select']['siti'] = $cf['sites'];
+
+	$ct['etc']['select']['anagrafica'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM anagrafica_view_static'
+    );
+
+    // macro di default
+	require DIR_SRC_INC_MACRO . '_default.form.php';
