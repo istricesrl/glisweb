1b66cc211da06de1363e9ece17c02b30b1060f3d
federicoistrice <federico.pierantoni@istricesrl.it>
2022-08-17 14:43:19 +0200

    debug auth

--- _mod/_0530.crediti/_src/_config/_250.auth.php
+++ _mod/_0530.crediti/_src/_config/_250.auth.php
@@ -21,6 +21,10 @@
 	    array(
             'crediti' => array(
                 CONTROL_FULL => array( 'roots' )
+            ),
+
+            '__report_movimenti_crediti__' => array(
+                CONTROL_FULL => array( 'roots' )
             )
         )
 	);
