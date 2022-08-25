bd18e7daa1b3497d0bc1dc4c3caef8af34f94b8b
Fabio Mosti <fabio.mosti@istricesrl.it>
2022-08-25 14:48:20 +0200

    debug e refactoring

--- _mod/_1200.todo/_src/_inc/_pages/_progetti.produzione.it-IT.php
+++ _mod/_1200.todo/_src/_inc/_pages/_progetti.produzione.it-IT.php
@@ -6,19 +6,24 @@
     // modulo di questo file
 	$m = DIR_MOD . '_1200.todo/';
 
-	// RELAZIONI CON IL MODULO PRODUZIONE
-	if( in_array( "1000.produzione", $cf['mods']['active']['array'] ) ) {
+	// RELAZIONI CON IL MODULO PROGETTI
+	if( in_array( "0900.progetti", $cf['mods']['active']['array'] ) ) {
 
-		// gestione todo progetti
-		$p['progetti.produzione.form.todo'] = array(
-			'sitemap'		=> false,
-			'title'			=> array( $l		=> 'todo' ),
-			'h1'			=> array( $l		=> 'to-do' ),
-			'parent'		=> array( 'id'		=> 'progetti.produzione.view' ),
-			'template'		=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'progetti.produzione.form.todo.html' ),
-			'macro'			=> array( $m.'_src/_inc/_macro/_progetti.produzione.form.todo.php' ),
-			'auth'			=> array( 'groups'	=> array(	'roots', 'staff' ) ),
-			'etc'			=> array( 'tabs'	=> $p['progetti.produzione.form']['etc']['tabs'] )
-		);
+		// RELAZIONI CON IL MODULO PRODUZIONE
+		if( in_array( "1000.produzione", $cf['mods']['active']['array'] ) ) {
+
+			// gestione todo progetti
+			$p['progetti.produzione.form.todo'] = array(
+				'sitemap'		=> false,
+				'title'			=> array( $l		=> 'todo' ),
+				'h1'			=> array( $l		=> 'to-do' ),
+				'parent'		=> array( 'id'		=> 'progetti.produzione.view' ),
+				'template'		=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'progetti.produzione.form.todo.html' ),
+				'macro'			=> array( $m.'_src/_inc/_macro/_progetti.produzione.form.todo.php' ),
+				'auth'			=> array( 'groups'	=> array(	'roots', 'staff' ) ),
+				'etc'			=> array( 'tabs'	=> $p['progetti.produzione.form']['etc']['tabs'] )
+			);
+
+		}
 
 	}
