10848c0ed9b3f4993566e01cf3e3985472c14849
Fabio Mosti <fabio.mosti@istricesrl.it>
2022-08-26 14:34:20 +0200

    piccole modifiche all'API di download

--- _src/_api/_download.php
+++ _src/_api/_download.php
@@ -17,11 +17,34 @@
     // debug
     // print_r( $_REQUEST );
 
+    // variabile generale per il comportamento
+    $authorized = false;
+
     // determino il mime type
     $finfo = finfo_open( FILEINFO_MIME );
     $mimetype = finfo_file( $finfo, DIR_VAR . $_REQUEST['__download__'] );
     finfo_close( $finfo );
 
+    // TODO qui logiche standard di protezione dei file
+    if( true ) {
+        $authorized = true;
+    }
+
+    // logiche custom di protezione dei file
+    if( file_exists( DIR_BASE . 'src/inc/macro/download.finally.php' ) ) {
+        require DIR_BASE . 'src/inc/macro/download.finally.php';
+    }
+
     // restituzione contenuto
-    header( 'content-type: ' . $mimetype );
-    echo file_get_contents( DIR_VAR . $_REQUEST['__download__'] );
+    if( $authorized === true ) {
+
+        header( 'content-type: ' . $mimetype );
+        echo file_get_contents( DIR_VAR . $_REQUEST['__download__'] );
+    
+    } else {
+
+	    http_response_code( 403 );
+        header( 'content-type: text/plain' );
+        echo 'accesso negato';
+
+    }
