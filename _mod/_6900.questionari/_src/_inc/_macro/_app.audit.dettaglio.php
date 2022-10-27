511b41dd693f80f78ae5cf39c51dff8d8384aa2e
silvia-df <silvia.defalco@istricesrl.it>
2022-10-27 17:07:09 +0200

    audit dettaglio

--- _mod/_6900.questionari/_src/_inc/_macro/_app.audit.dettaglio.php
+++ _mod/_6900.questionari/_src/_inc/_macro/_app.audit.dettaglio.php
@@ -1,8 +1,9 @@
 <?php
 
-    // require '../../../../../_src/_config.php';
+    // tabella gestita
+	$ct['form']['table'] = 'audit';
 
-    if( isset( $_REQUEST['id_audit'] ) ){
+    if( isset( $_REQUEST[$ct['form']['table']]['id'] ) ){
 
         $ct['etc']['audit'] = mysqlSelectRow(
             $cf['mysql']['connection'], 
@@ -10,7 +11,7 @@
             'LEFT JOIN progetti AS p ON a.id_progetto = p.id '.
             'LEFT JOIN indirizzi_view AS i ON p.id_indirizzo = i.id '.
             'WHERE a.id = ?',
-            array( array( 's' => $_REQUEST['id_audit'] ) )
+            array( array( 's' => $_REQUEST[$ct['form']['table']]['id'] ) )
         );
 
         // elenco dei controlli
@@ -20,22 +21,36 @@
             'FROM controlli as c '.
             'LEFT JOIN questionari_view AS q ON c.id_questionario = q.id '.
             'WHERE c.id_audit = ? AND c.id_anagrafica IS NULL',
-            array( array( 's' => $_REQUEST['id_audit'] ) )
+            array( array( 's' => $_REQUEST[$ct['form']['table']]['id'] ) )
         );
 
         $ct['etc']['controlli']['anagrafica'] = mysqlQuery(
             $cf['mysql']['connection'], 
-            'SELECT c.*, q.nome as questionario '.
+            'SELECT c.*, q.nome as questionario, min( IFNULL( timestamp_completamento, 0 ) ) as completamento '.
             'FROM controlli as c '.
             'LEFT JOIN questionari_view AS q ON c.id_questionario = q.id '.
             'WHERE c.id_audit = ? AND c.id_anagrafica IS NOT NULL GROUP BY q.id',
-            array( array( 's' => $_REQUEST['id_audit'] ) )
+            array( array( 's' => $_REQUEST[$ct['form']['table']]['id'] ) )
         );
 
-        //print_r( $ct['etc']['controlli']['progetto'] );
-        //print_r( $ct['etc']['controlli']['anagrafica'] );
+        // questionari
+        $ct['etc']['select']['questionari'] = mysqlCachedIndexedQuery(
+            $cf['memcache']['index'],
+            $cf['memcache']['connection'],
+            $cf['mysql']['connection'], 
+            'SELECT id, __label__ FROM questionari_view' );
+
+        // anagrafiche
+        $ct['etc']['select']['anagrafica'] = mysqlCachedIndexedQuery(
+            $cf['memcache']['index'],
+            $cf['memcache']['connection'],
+            $cf['mysql']['connection'], 
+            'SELECT id, __label__ FROM anagrafica_view_static WHERE se_collaboratore = 1' );
+
+        #print_r( $ct['etc']['controlli']['progetto'] );
+        #print_r( $ct['etc']['controlli']['anagrafica'] );
     }
 
-
-
+    // macro di default
+	require DIR_SRC_INC_MACRO . '_default.form.php';
     require DIR_SRC_INC_MACRO . '_default.tools.php';
