0c7d5a27e8170750d4792424d7f87da3dfa24f40
Team Produzione Istrice <produzione@istricesrl.it>
2023-05-14 23:34:49 +0200

    migliorie form corsi scheda acquisto

--- _mod/_0620.tesseramenti/_src/_inc/_pages/_tesseramenti.it-IT.php
+++ _mod/_0620.tesseramenti/_src/_inc/_pages/_tesseramenti.it-IT.php
@@ -119,30 +119,114 @@
 			'etc'				=> array( 'tabs'	=> $p['tesseramenti.form']['etc']['tabs'] )
 		);
 
+		// vista archivio tesseramenti
+		$p['tesseramenti.archivio.view'] = array(
+			'sitemap'			=> false,
+			'icon'				=> '<i class="fa fa-archive" aria-hidden="true"></i>',
+			'title'				=> array( $l		=> 'archivio' ),
+			'h1'				=> array( $l		=> 'archivio' ),
+			'parent'			=> array( 'id'		=> 'tesseramenti.view' ),
+			'template'			=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'default.view.html' ),
+			'macro'				=> array( $m . '_src/_inc/_macro/_tesseramenti.archivio.view.php' ),
+			'auth'				=> array( 'groups'	=> array(	'roots', 'staff' ) ),
+			'etc'				=> array( 'tabs'	=> $p['tesseramenti.view']['etc']['tabs'] )
+		);
 
-    // vista archivio tesseramenti
-	$p['tesseramenti.archivio.view'] = array(
-	    'sitemap'			=> false,
-		'icon'				=> '<i class="fa fa-archive" aria-hidden="true"></i>',
-		'title'				=> array( $l		=> 'archivio' ),
-	    'h1'				=> array( $l		=> 'archivio' ),
-	    'parent'			=> array( 'id'		=> 'tesseramenti.view' ),
-	    'template'			=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'default.view.html' ),
-	    'macro'				=> array( $m . '_src/_inc/_macro/_tesseramenti.archivio.view.php' ),
-	    'auth'				=> array( 'groups'	=> array(	'roots', 'staff' ) ),
-	    'etc'				=> array( 'tabs'	=> $p['tesseramenti.view']['etc']['tabs'] )
+		// stampe tesseramenti
+		$p['tesseramenti.stampe'] = array(
+			'sitemap'			=> false,
+			'icon'				=> '<i class="fa fa-print" aria-hidden="true"></i>',
+			'title'				=> array( $l		=> 'stampe' ),
+			'h1'				=> array( $l		=> 'stampe' ),
+			'parent'			=> array( 'id'		=> 'tesseramenti.view' ),
+			'template'			=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'default.tools.html' ),
+			'macro'				=> array( $m . '_src/_inc/_macro/_tesseramenti.stampe.php' ),
+			'auth'				=> array( 'groups'	=> array(	'roots', 'staff' ) ),
+			'etc'				=> array( 'tabs'	=> $p['tesseramenti.view']['etc']['tabs'] )
+		);
+
+	}
+
+	// vista tesseramenti
+	$p['tipologie.tesseramenti.view']	= array(
+		'sitemap'			=> false,
+		'title'				=> array( $l		=> 'tesseramenti' ),
+		'h1'				=> array( $l		=> 'tesseramenti' ),
+		'parent'			=> array( 'id'		=> NULL ),
+		'template'			=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'default.view.html' ),
+		'macro'				=> array( $m . '_src/_inc/_macro/_tipologie.tesseramenti.view.php' ),
+		'auth'				=> array( 'groups'	=> array(	'roots', 'staff' ) ),
+		'etc'				=> array( 'tabs'	=> array(	'tipologie.tesseramenti.view',
+															'tipologie.tesseramenti.tools' ) ),
+		'menu'				=> array( 'admin'	=> array(	'' => 	array(	'label'		=> array( $l => 'tesseramenti' ),
+																			'priority'	=> '255' ) ) )
 	);
 
 	// stampe tesseramenti
-	$p['tesseramenti.stampe'] = array(
-	    'sitemap'			=> false,
-		'icon'				=> '<i class="fa fa-print" aria-hidden="true"></i>',
-		'title'				=> array( $l		=> 'stampe' ),
-	    'h1'				=> array( $l		=> 'stampe' ),
-	    'parent'			=> array( 'id'		=> 'tesseramenti.view' ),
-	    'template'			=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'default.tools.html' ),
-	    'macro'				=> array( $m . '_src/_inc/_macro/_tesseramenti.stampe.php' ),
-	    'auth'				=> array( 'groups'	=> array(	'roots', 'staff' ) ),
-	    'etc'				=> array( 'tabs'	=> $p['tesseramenti.view']['etc']['tabs'] )
+	$p['tipologie.tesseramenti.tools'] = array(
+		'sitemap'			=> false,
+		'icon'				=> '<i class="fa fa-cogs" aria-hidden="true"></i>',
+		'title'				=> array( $l		=> 'azioni' ),
+		'h1'				=> array( $l		=> 'azioni' ),
+		'parent'			=> array( 'id'		=> 'tipologie.tesseramenti.view' ),
+		'template'			=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'default.tools.html' ),
+		'macro'				=> array( $m . '_src/_inc/_macro/_tipologie.tesseramenti.tools.php' ),
+		'auth'				=> array( 'groups'	=> array(	'roots', 'staff' ) ),
+		'etc'				=> array( 'tabs'	=> $p['tipologie.tesseramenti.view']['etc']['tabs'] )
 	);
-}
+
+	// gestione progetti
+	$p['tipologie.tesseramenti.form'] = array(
+		'sitemap'			=> false,
+		'title'				=> array( $l		=> 'gestione' ),
+		'h1'				=> array( $l		=> 'gestione' ),
+		'parent'			=> array( 'id'		=> 'tipologie.tesseramenti.view' ),
+		'template'			=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'tipologie.tesseramenti.form.html' ),
+		'macro'				=> array( $m . '_src/_inc/_macro/_tipologie.tesseramenti.form.php' ),
+		'auth'				=> array( 'groups'	=> array(	'roots' ) ),
+		'etc'				=> array( 'tabs'	=> array(	'tipologie.tesseramenti.form',
+															'tipologie.tesseramenti.form.acquisto',	// TODO questa è una sinergia con il modulo catalogo, gestirla di conseguenza
+															'tipologie.tesseramenti.form.metadati',
+															'tipologie.tesseramenti.form.tools' 
+														) )
+	);
+
+	// tools form tesseramenti
+	// TODO questa è una sinergia con il modulo catalogo, gestirla di conseguenza
+	$p['tipologie.tesseramenti.form.acquisto'] = array(
+		'sitemap'			=> false,
+		'icon'				=> '<i class="fa fa-shopping-cart" aria-hidden="true"></i>',
+		'title'				=> array( $l		=> 'acquisto' ),
+		'h1'				=> array( $l		=> 'acquisto' ),
+		'parent'			=> array( 'id'		=> 'tipologie.tesseramenti.view' ),
+		'template'			=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'tipologie.tesseramenti.form.acquisto.html' ),
+		'macro'				=> array( $m . '_src/_inc/_macro/_tipologie.tesseramenti.form.acquisto.php' ),
+		'auth'				=> array( 'groups'	=> array(	'roots' ) ),
+		'etc'				=> array( 'tabs'	=> $p['tipologie.tesseramenti.form']['etc']['tabs'] )
+	);
+
+	// gestione anagrafica metadati
+	$p['tipologie.tesseramenti.form.metadati'] = array(
+		'sitemap'			=> false,
+		'icon'				=> '<i class="fa fa-code" aria-hidden="true"></i>',
+		'title'				=> array( $l		=> 'metadati' ),
+		'h1'				=> array( $l		=> 'metadati' ),
+		'parent'			=> array( 'id'		=> 'tipologie.tesseramenti.view' ),
+		'template'			=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'tipologie.tesseramenti.form.metadati.html' ),
+		'macro'				=> array( $m . '_src/_inc/_macro/_tipologie.tesseramenti.form.metadati.php' ),
+		'auth'				=> array( 'groups'	=> array(	'roots', 'staff' ) ),
+		'etc'				=> array( 'tabs'	=> $p['tipologie.tesseramenti.form']['etc']['tabs'] )
+	);
+
+	// tools form tesseramenti
+	$p['tipologie.tesseramenti.form.tools'] = array(
+		'sitemap'			=> false,
+		'icon'				=> '<i class="fa fa-cogs" aria-hidden="true"></i>',
+		'title'				=> array( $l		=> 'azioni abbonamento' ),
+		'h1'				=> array( $l		=> 'azioni' ),
+		'parent'			=> array( 'id'		=> 'tipologie.tesseramenti.view' ),
+		'template'			=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'default.tools.html' ),
+		'macro'				=> array( $m . '_src/_inc/_macro/_tipologie.tesseramenti.form.tools.php' ),
+		'auth'				=> array( 'groups'	=> array(	'roots' ) ),
+		'etc'				=> array( 'tabs'	=> $p['tipologie.tesseramenti.form']['etc']['tabs'] )
+	);
--- _mod/_0640.abbonamenti/_src/_inc/_macro/_anagrafica.form.abbonamenti.php
+++ _mod/_0640.abbonamenti/_src/_inc/_macro/_anagrafica.form.abbonamenti.php
@@ -24,7 +24,8 @@
         'id_anagrafica' => 'anagrafica',
         'tipologia' => 'tipologia',
         'data_inizio' => 'inizio',
-        'data_fine' => 'fine'
+        'data_fine' => 'fine',
+        NULL => 'azioni'
 	);
 
     // stili della vista
@@ -37,6 +38,11 @@
         'data_inizio' => 'text-left',
         'data_fine' => 'text-left'
 	);
+
+    // javascript della vista
+    $ct['view']['onclick'] = array(
+        NULL => 'event.stopPropagation();'
+    );
 
     // pagina per la gestione degli oggetti esistenti
 	$ct['view']['open']['page'] = 'abbonamenti.form';
@@ -65,3 +71,20 @@
 
     // macro di default
 	require DIR_SRC_INC_MACRO . '_default.form.php';
+
+    // azioni
+    foreach( $ct['view']['data'] as &$row ) {
+        if( is_array( $row ) ) {
+            $pagato = mysqlSelectValue( $cf['mysql']['connection'], 'SELECT documenti_articoli.id FROM documenti_articoli INNER JOIN documenti ON documenti.id = documenti_articoli.id_documento INNER JOIN pagamenti ON pagamenti.id_documento = documenti.id INNER JOIN rinnovi ON rinnovi.id = documenti_articoli.id_rinnovo WHERE rinnovi.id_contratto = ? AND pagamenti.timestamp_pagamento IS NOT NULL', array( array( 's' => $row['id'] ) ) );
+            if( empty( $pagato ) ) {
+                $articolo = mysqlSelectValue( $cf['mysql']['connection'], 'SELECT articoli.id FROM articoli INNER JOIN metadati ON metadati.id_articolo = articoli.id WHERE metadati.nome = "acquisto_rinnovi|id_tipologia" AND metadati.testo = ?', array( array( 's' => $row['id_tipologia'] ) ) );
+                $ordinato = mysqlSelectValue( $cf['mysql']['connection'], 'SELECT carrelli.id FROM carrelli_articoli INNER JOIN carrelli ON carrelli.id = carrelli_articoli.id_carrello WHERE id_articolo = ? AND carrelli_articoli.destinatario_id_anagrafica = ? AND carrelli.session = ?', array( array( 's' => $articolo ), array( 's' => $_REQUEST[ $ct['form']['table'] ]['id'] ), array( 's' => $cf['session']['id'] ) ) );
+# die($ordinato);
+                if( empty( $ordinato ) ) {
+#                    $row[ NULL ] =  '<a href="#" onclick="$(this).metroWs(\'/task/4170.ecommerce/aggiungi.al.carrello?__carrello__[__articolo__][id_articolo]='.$articolo.'&__carrello__[__articolo__][destinatario_id_anagrafica]='.$_REQUEST[ $ct['form']['table'] ]['id'].'\', aggiornaCarrello );"><span class="media-left"><i class="fa fa-cart-plus"></i></span></a>';
+                    $row[ NULL ] =  '<a href="' . $cf['contents']['pages']['abbonamenti.form']['url'][ LINGUA_CORRENTE ] . '?contratti[id]=' . $row['id_contratto'] . '&__backurl__='. $ct['page']['backurl'][LINGUA_CORRENTE] .'"><span class="media-left"><i class="fa fa-cart-plus"></i></span></a>';
+                }
+            }
+        }
+    }
+
