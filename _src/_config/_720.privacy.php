9e18144d58ece159ed5cefec27ded2099b38f919
Team Produzione Istrice <produzione@istricesrl.it>
2023-06-21 10:22:01 +0200

    refactoring

--- _src/_config/_720.privacy.php
+++ _src/_config/_720.privacy.php
@@ -28,33 +28,49 @@
     $cf['cookie']['index'] = array();
 
     // ...
-    foreach( $cf['privacy']['cookie'] as $l1 => $c1 ) {
+    if( is_array( $cf['privacy']['cookie'] ) ) {
 
-        // debug
-        // echo $l1 . PHP_EOL;
-
-        // ...
-        foreach( $c1 as $l2 => $c2 ) {
+        foreach( $cf['privacy']['cookie'] as $l1 => $c1 ) {
 
             // debug
-            // echo $l2 . PHP_EOL;
-
+            // echo $l1 . PHP_EOL;
+    
             // ...
-            foreach( $c2 as $l3 => $c3 ) {
+            if( is_array( $c1 ) ) {
+    
+                foreach( $c1 as $l2 => $c2 ) {
+    
+                    // debug
+                    // echo $l2 . PHP_EOL;
+        
+                    // ...
+                    if( is_array( $c2 ) ) {
 
-                // debug
-                // echo $l3 . PHP_EOL;
-                // print_r( $c3 );
+                        foreach( $c2 as $l3 => $c3 ) {
+        
+                            // debug
+                            // echo $l3 . PHP_EOL;
+                            // print_r( $c3 );
+            
+                            // ..
+                            if( is_array( $c3['id'] ) ) {
 
-                // ..
-                foreach( $c3['id'] as $id ) {
-                    $cf['cookie']['index'][ $id ] = &$cf['privacy']['cookie'][ $l1 ][ $l2 ][ $l3 ];
-                }
-
+                                foreach( $c3['id'] as $id ) {
+                                    $cf['cookie']['index'][ $id ] = &$cf['privacy']['cookie'][ $l1 ][ $l2 ][ $l3 ];
+                                }
+    
+                            }
+            
+                        }
+    
+                    }
+        
+                } 
+        
             }
-
-        } 
-
+    
+        }
+    
     }
 
 
