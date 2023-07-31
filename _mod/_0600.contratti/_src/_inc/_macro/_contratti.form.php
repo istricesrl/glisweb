c0e05bf355469efe5ed73612131a8b07c8fb82a0
Team Produzione Istrice <produzione@istricesrl.it>
2023-07-31 09:48:36 +0200

    migliorie modulo contratti

--- _mod/_0600.contratti/_src/_inc/_macro/_contratti.form.php
+++ _mod/_0600.contratti/_src/_inc/_macro/_contratti.form.php
@@ -63,9 +63,26 @@
 	    'SELECT id, __label__ FROM tipologie_contratti_view'
     );
 
+    // tendina tipologia rinnovo
+    $ct['etc']['select']['tipologie_rinnovi'] = mysqlCachedIndexedQuery(
+        $cf['memcache']['index'],
+        $cf['memcache']['connection'],
+        $cf['mysql']['connection'],
+        'SELECT id, __label__ FROM tipologie_rinnovi_view WHERE se_contratti = 1'
+    );
+
+	// ...
+	if( ! isset( $_REQUEST[ $ct['form']['table'] ]['id'] ) ) {
+		$_REQUEST[ $ct['form']['table'] ]['contratti_anagrafica'][0]['id_anagrafica'] = trovaIdAziendaGestita();
+		$_REQUEST[ $ct['form']['table'] ]['contratti_anagrafica'][0]['id_ruolo'] = 35;
+		if( isset( $_REQUEST['__preset__']['contratti']['id_anagrafica'] ) ) {
+			$_REQUEST[ $ct['form']['table'] ]['contratti_anagrafica'][1]['id_anagrafica'] = $_REQUEST['__preset__']['contratti']['id_anagrafica'];
+			$_REQUEST[ $ct['form']['table'] ]['contratti_anagrafica'][1]['id_ruolo'] = 32;
+		}
+	}
 
     // macro di default per l'entità contratti
-//	require '_contratti.form.default.php';
+	// require '_contratti.form.default.php';
 
     // macro di default
 	require DIR_SRC_INC_MACRO . '_default.form.php';
