ea31559f40c458917e03fb16113a77d67908ce3e
Team Produzione Istrice <produzione@istricesrl.it>
2023-09-07 22:36:31 +0200

    aggiunti script per minificazione e ottimizzazione risorse

---
+++ _src/_sh/_files.minimize.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## livelli per la root del sito
+RL="../../"
+
+## directory corrente
+cd $(dirname "$0")
+cd $RL
+
+## informazioni
+echo "lavoro su: $(pwd)"
+
+## pacchetti
+apt-get install yui-compressor
+
+## ottimizzo i CSS
+find . -iname "*.css" -a -not -name "*.min.*" -a -not -wholename "*/_external/*" -a -not -wholename "*/_docs/*" | while read i; do
+    yui-compressor -o "${i%.css}.min.css" "$i"
+done
+
+## ottimizzo i Javascript
+find . -iname "*.js" -a -not -name "*.min.*" -a -not -wholename "*/_external/*" -a -not -wholename "*/_docs/*" | while read i; do
+    yui-compressor -o "${i%.js}.min.js" "$i"
+done
+
+## fine elaborazione
+exit
