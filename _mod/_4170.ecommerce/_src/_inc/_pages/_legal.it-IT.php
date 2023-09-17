e3926fe1680d2f046d3fbf67415f6f25ad127120
Team Produzione Istrice <produzione@istricesrl.it>
2023-09-17 13:46:12 +0200

    aggiunta pagina condizioni di vendita

---
+++ _mod/_4170.ecommerce/_src/_inc/_pages/_legal.it-IT.php
@@ -0,0 +1,13 @@
+<?php
+
+    // lingua di questo file
+	$l = 'it-IT';
+
+    // pagina principale
+	$p['fornitura'] = array(
+	    'sitemap'		=> false,
+	    'title'		    => array( $l		=> 'condizioni di vendita' ),
+	    'h1'		    => array( $l		=> 'condizioni di vendita' ),
+	    'template'		=> array( 'path'	=> '_src/_templates/_lydia/', 'schema' => 'default.html' ),
+	    'parent'		=> array( 'id'		=> NULL )
+	);
