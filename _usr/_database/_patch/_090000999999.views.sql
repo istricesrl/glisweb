5c41d76c9a3837bb7602dd0c882754c93175c4f9
Team Produzione Istrice <produzione@istricesrl.it>
2023-10-23 14:56:17 +0200

    aggiunta recuperi_view

--- _usr/_database/_patch/_090000999999.views.sql
+++ _usr/_database/_patch/_090000999999.views.sql
@@ -1064,6 +1064,110 @@ CREATE OR REPLACE VIEW `cartellini_view` AS
 		tipologie_attivita.se_cartellini IS NOT NULL
 ;
 
+-- | 090000001804
+
+-- recuperi_view
+-- tipologia: tabella gestita
+DROP TABLE IF EXISTS `recuperi_view`;
+
+-- | 090000001805
+
+-- recuperi_view
+-- tipologia: tabella gestita
+-- verifica: 2021-05-28 13:12 Fabio Mosti
+CREATE OR REPLACE VIEW `recuperi_view` AS
+	SELECT	
+		attivita.id,
+		attivita.id_tipologia,
+		tipologie_attivita.nome AS tipologia,
+		attivita.id_cliente,
+		coalesce( a2.denominazione , concat( a2.cognome, ' ', a2.nome ), '' ) AS cliente,
+		attivita.id_contatto,
+		c1.nome AS contatto,
+		attivita.id_indirizzo,
+		indirizzi.indirizzo AS indirizzo,
+		attivita.id_luogo,
+		luoghi_path( coalesce( attivita.id_luogo, todo.id_luogo ) ) AS luogo,
+		attivita.data_scadenza,
+		attivita.ora_scadenza,
+		attivita.data_programmazione,
+		attivita.ora_inizio_programmazione,
+		attivita.ora_fine_programmazione,
+		attivita.id_anagrafica_programmazione,
+		coalesce( a3.denominazione , concat( a3.cognome, ' ', a3.nome ), '' ) AS anagrafica_programmazione,
+		attivita.ore_programmazione,
+		attivita.data_attivita,
+		day( attivita.data_attivita ) as giorno_attivita,
+		month( attivita.data_attivita ) as mese_attivita,
+		year( attivita.data_attivita ) as anno_attivita,
+		attivita.ora_inizio,
+		attivita.latitudine_ora_inizio,
+		attivita.longitudine_ora_inizio,
+		attivita.ora_fine,
+		attivita.latitudine_ora_fine,
+		attivita.longitudine_ora_fine,
+		attivita.id_anagrafica,
+		coalesce( a1.denominazione , concat( a1.cognome, ' ', a1.nome ), '' ) AS anagrafica,
+		attivita.ore,
+		recupero.id AS id_recupero,
+		recupero.data_programmazione AS data_programmazione_recupero,
+		recupero.ora_inizio_programmazione AS ora_inizio_programmazione_recupero,
+		recupero.ora_fine_programmazione AS ora_fine_programmazione_recupero,
+		attivita.nome,
+		attivita.id_documento,
+		concat(
+			tipologie_documenti.sigla,
+			' ',
+			documenti.numero,
+			'/',
+			documenti.sezionale,
+			' del ',
+			documenti.data
+		) AS documento,
+		attivita.id_progetto,
+		progetti.nome AS progetto,
+		group_concat( DISTINCT if( d.id, categorie_progetti_path( d.id ), null ) SEPARATOR ' | ' ) AS discipline,
+		attivita.id_matricola,
+        attivita.id_immobile,
+		attivita.id_pianificazione,
+		attivita.id_todo,
+		todo.nome AS todo,
+		attivita.id_mastro_provenienza,
+		m1.nome AS mastro_provenienza,
+		attivita.id_mastro_destinazione,
+		m2.nome AS mastro_destinazione,
+		attivita.codice_archivium,
+		attivita.token,
+		attivita.id_account_inserimento,
+		attivita.id_account_aggiornamento,
+		concat(
+			attivita.nome,
+			' / ',
+			attivita.ore,
+			' / ',
+			coalesce( a1.denominazione , concat( a1.cognome, ' ', a1.nome ), '' )
+		) AS __label__
+	FROM attivita
+		LEFT JOIN tipologie_attivita ON tipologie_attivita.id = attivita.id_tipologia
+		LEFT JOIN anagrafica AS a1 ON a1.id = attivita.id_anagrafica
+		LEFT JOIN anagrafica AS a2 ON a2.id = attivita.id_cliente
+		LEFT JOIN anagrafica AS a3 ON a3.id = attivita.id_anagrafica_programmazione
+		LEFT JOIN contatti AS c1 ON c1.id = attivita.id_contatto
+		LEFT JOIN progetti_categorie ON progetti_categorie.id_progetto = attivita.id_progetto
+		LEFT JOIN progetti ON progetti.id = attivita.id_progetto
+		LEFT JOIN categorie_progetti ON categorie_progetti.id = progetti_categorie.id_categoria
+		LEFT JOIN categorie_progetti AS d ON d.id = progetti_categorie.id_categoria AND d.se_disciplina = 1		
+		LEFT JOIN todo ON todo.id = attivita.id_todo
+		LEFT JOIN indirizzi ON indirizzi.id = attivita.id_indirizzo
+		LEFT JOIN mastri AS m1 ON m1.id = attivita.id_mastro_provenienza
+		LEFT JOIN mastri AS m2 ON m2.id = attivita.id_mastro_destinazione
+		LEFT JOIN documenti ON documenti.id = attivita.id_documento
+		LEFT JOIN tipologie_documenti ON tipologie_documenti.id = documenti.id_tipologia
+		LEFT JOIN attivita AS recupero ON recupero.id_genitore = attivita.id AND recupero.id_tipologia = 32
+	WHERE attivita.id_tipologia = 19
+	GROUP BY attivita.id
+;
+
 -- | 090000002100
 
 -- audio_view
