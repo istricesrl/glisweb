0fa12dbc9f09f1c052ba1a2566945c4d0112c460
Team Produzione Istrice <produzione@istricesrl.it>
2024-02-09 12:33:02 +0100

    debug e refactoring sistema privacy

--- _mod/_0350.registrazione/_src/_config/_030.common.php
+++ _mod/_0350.registrazione/_src/_config/_030.common.php
@@ -11,12 +11,5 @@
      *
      */
 
-    // configurazione extra
-	if( isset( $cx['registrazione'] ) ) {
-	    $cf['registrazione'] = array_replace_recursive( $cf['registrazione'], $cx['registrazione'] );
-	}
-
-    // configurazione extra per sito
-	if( isset( $cf['site']['registrazione'] ) ) {
-	    $cf['registrazione'] = array_replace_recursive( $cf['registrazione'], $cf['site']['registrazione'] );
-	}
+    // configurazione
+    $cf['registrazione'] = array();
