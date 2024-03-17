b9b955c616131d384871f8f18f0a867696a50238
Team Produzione Istrice <produzione@istricesrl.it>
2024-03-17 12:54:24 +0100

    aggiunto log dei deploy tramite phing

--- _src/_sh/_phing.deploy.sh
+++ _src/_sh/_phing.deploy.sh
@@ -16,6 +16,19 @@ apt-get install -y phing rsync
 ## informazioni
 echo "lavoro su: $(pwd)"
 
+## aggiungo una newline al file se non c'è già
+sed -i.bak '$a\' ../TODO.md
+
+## registro dei deploy
+echo >> ../TODO.md
+echo "$( date "+%Y-%m-%d %H:%M" ) DEPLOY TRAMITE PHING" >> ../TODO.md
+echo "=====================================" >> ../TODO.md
+echo "Questa è una registrazione automatica di deploy ($1) effettuata dallo script $0" >> ../TODO.md
+echo >> ../TODO.md
+
+## debug
+exit 0
+
 ## avvio deploy
 if [ -n "$1" ]; then
     phing -f ./_usr/_deploy/_phing/_build.xml -propertyfile ./usr/deploy/phing/$1.properties
