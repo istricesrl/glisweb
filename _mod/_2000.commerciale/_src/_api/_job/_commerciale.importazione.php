84fdd69f7f4dcffb5d9212896043eb6ced713b29
Team Produzione Istrice <produzione@istricesrl.it>
2024-03-17 19:47:39 +0100

    migliorie all'importazione attività commerciali

--- _mod/_2000.commerciale/_src/_api/_job/_commerciale.importazione.php
+++ _mod/_2000.commerciale/_src/_api/_job/_commerciale.importazione.php
@@ -1,5 +1,43 @@
 <?php
 
+    /**
+     * importazione massiva attività commerciali
+     * 
+     * questo job importa un file CSV con le attività commerciali da svolgere
+     * 
+     * introduzione
+     * ============
+     * 
+     * 
+     * 
+     * struttura del file
+     * ==================
+     * 
+     * 
+     * 
+     * campo                                        | descrizione
+     * ---------------------------------------------|---------------------------------------------------------
+     * codice_campagna                              | il codice della campagna commerciale a cui appartiene l'attività (se non esiste verrà creato)
+     * tipologia_campagna                           | la tipologia della campagna commerciale a cui appartiene l'attività (obbligatoria se si setta il codice campagna)
+     * nome_campagna                                | il nome della campagna commerciale a cui appartiene l'attività (se non esiste verrà creata)
+     * codice_cliente                               | il codice del cliente (se non esiste ed è presente la denominazione verrà creato altrimenti verrà segnalato l'errore)
+     * mail_cliente                                 | l'indirizzo mail del cliente (opzionale, è possibile specificare indirizzi multipli separati da pipe)
+     * telefoni_cliente                             | il numero di telefono del cliente (opzionale, è possibile specificare numeri multipli separati da pipe)
+     * codice_progetto                              | il codice del progetto cui si riferisce l'attività (se non esiste verrà creato)
+     * tipologia_progetto                           | la tipologia del progetto cui si riferisce l'attività (obbligatoria se si setta il codice progetto)
+     * codice_todo                                  | il codice della todo cui si riferisce l'attività (se non esiste verrà creata)
+     * tipologia_todo                               | la tipologia della todo cui si riferisce l'attività (obbligatoria se si setta il codice todo)
+     * responsabile_todo                            | nome e cognome del responsabile della todo cui si riferisce l'attività (opzionale, se non viene trovato viene ignorato)
+     * nome_todo                                    | il nome della todo cui si riferisce l'attività (obbligatorio)
+     * anagrafica_programmazione_attivita_seguente  | nome e cognome del responsabile dell'attività seguente
+     * data_programmazione_attivita_seguente        | la data di programmazione dell'attività seguente
+     * tipologia_attivita_seguente                  | la tipologia dell'attività seguente
+     * nome_attivita_seguente                       | il nome dell'attività seguente
+     * ore_programmazione_attivita_seguente         | le ore previste per l'attività seguente
+     * 
+     * 
+     */
+
     /* CODICE PRINCIPALE DEL JOB */
 
     // verifiche formali
@@ -37,6 +75,14 @@
         $job['workspace']['status']['error'][] = 'impossibile leggere il file ' . $job['workspace']['file'];
 
     } else {
+
+        /**
+         * 01. avvio del job
+         * =================
+         * 
+         * 
+         * 
+         */
 
         // attività di avvio
         if( empty( $job['corrente'] ) ) {
@@ -79,6 +125,15 @@
 
         }
 
+        /**
+         * 02. chiusura o lavorazione del job
+         * ==================================
+         * 
+         * 
+         * 
+         * 
+         */
+
         // operazioni di chiusura
         if( empty( $job['totale'] ) || $job['corrente'] > $job['totale'] ) {
 
@@ -108,6 +163,14 @@
 
             } else {
 
+                /**
+                 * 02.1. LAVORAZIONE RIGA
+                 * ======================
+                 * 
+                 * 
+                 * 
+                 */
+
                 // trovo l'ID dell'anagrafica
                 $idAnagrafica = mysqlInsertRow(
                     $cf['mysql']['connection'],
@@ -116,7 +179,12 @@
                         'codice' => ( ! empty( $job['riga']['codice_cliente'] ) ) ? $job['riga']['codice_cliente'] : NULL,
                         'denominazione' => ( ( isset( $job['riga']['denominazione_cliente'] ) ) ? $job['riga']['denominazione_cliente'] : NULL ),
                     ),
-                    'anagrafica'
+                    'anagrafica',
+                    true,
+                    false,
+                    array(
+                        'codice'
+                    )
                 );
 
                 // se è presente un ID cliente
@@ -192,6 +260,32 @@
                     // aggiornamento vista statica
                     updateAnagraficaViewStatic( $idAnagrafica );
 
+                    // campagna
+                    if( isset( $job['riga']['codice_campagna'] ) && ! empty( $job['riga']['codice_campagna'] ) ) {
+
+                        // campagna
+                        if( isset( $job['riga']['nome_campagna'] ) && ! empty( $job['riga']['nome_campagna'] ) ) {
+
+                            // cerco la tipologia di campagna
+
+                            // cerco il responsabile della campagna
+
+                            // inserisco la campagna
+
+                        } else {
+
+                            // status
+                            $job['status']['error'][] = 'codice campagna non settato per la riga ' . $job['corrente'];
+
+                        }
+
+                    } else {
+
+                        // status
+                        $job['status']['error'][] = 'codice campagna non settato per la riga ' . $job['corrente'];
+
+                    }
+
                     // progetto
                     if( isset( $job['riga']['codice_progetto'] ) && ! empty( $job['riga']['codice_progetto'] ) ) {
 
@@ -253,29 +347,106 @@
 
                     }
 
+                    // todo
+                    if( isset( $job['riga']['codice_todo'] ) && ! empty( $job['riga']['codice_todo'] ) ) {
+
+                        // todo
+                        if( isset( $job['riga']['nome_todo'] ) && ! empty( $job['riga']['nome_todo'] ) ) {
+
+                            // cerco la tipologia di todo
+                            $idTipologiaTodo = mysqlSelectCachedValue(
+                                $cf['memcache']['connection'],
+                                $cf['mysql']['connection'],
+                                'SELECT id FROM tipologie_todo_view WHERE __label__ = ?',
+                                array(
+                                    array( 's' => $job['riga']['tipologia_todo'] )
+                                )
+                            );
+
+                            // cerco il responsabile della todo
+                            $idResponsabileTodo = mysqlSelectCachedValue(
+                                $cf['memcache']['connection'],
+                                $cf['mysql']['connection'],
+                                'SELECT id FROM anagrafica_view_static WHERE __label__ = ?',
+                                array(
+                                    array( 's' => $job['riga']['responsabile_todo'] )
+                                )
+                            );
+
+                            // inserisco la todo
+                            $idTodo = mysqlInsertRow(
+                                $cf['mysql']['connection'],
+                                array(
+                                    'codice' => $job['riga']['codice_todo'],
+                                    'id_tipologia' => $idTipologiaTodo,
+                                    'nome' => $job['riga']['nome_todo'],
+                                    'id_responsabile' => $idResponsabileTodo,
+                                    'id_cliente' => $idAnagrafica
+                                ),
+                                'todo'
+                            );
+
+                        } else {
+
+                            // status
+                            $job['status']['error'][] = 'codice todo non settato per la riga ' . $job['corrente'];
+
+                        }
+
+                    } else {
+
+                        // status
+                        $job['status']['error'][] = 'codice todo non settato per la riga ' . $job['corrente'];
+
+                    }
+
                     // attività pianificata
                     if( isset( $job['riga']['data_programmazione_attivita_seguente'] ) && ! empty( $job['riga']['data_programmazione_attivita_seguente'] ) ) {
 
                         // nome attività pianificata
                         if( isset( $job['riga']['nome_attivita_seguente'] ) && ! empty( $job['riga']['nome_attivita_seguente'] ) ) {
 
+                            // cerco la tipologia di attività
+                            $idTipologiaAttivita = mysqlSelectCachedValue(
+                                $cf['memcache']['connection'],
+                                $cf['mysql']['connection'],
+                                'SELECT id FROM tipologie_attivita_view WHERE __label__ = ?',
+                                array(
+                                    array( 's' => $job['riga']['tipologia_attivita_seguente'] )
+                                )
+                            );
+
+                            // cerco il responsabile dell'attività seguente
+                            $idResponsabileAttivita = mysqlSelectCachedValue(
+                                $cf['memcache']['connection'],
+                                $cf['mysql']['connection'],
+                                'SELECT id FROM anagrafica_view_static WHERE __label__ = ?',
+                                array(
+                                    array( 's' => $job['riga']['anagrafica_programmazione_attivita_seguente'] )
+                                )
+                            );
+
                             // inserimento attività
                             $idAttivita = mysqlInsertRow(
                                 $cf['mysql']['connection'],
                                 array(
-                                    'id_tipologia' => 16,
+                                    'id_tipologia' => $idTipologiaAttivita,
                                     'data_programmazione' => $job['riga']['data_programmazione_attivita_seguente'],
+                                    'id_anagrafica_programmazione' => $idResponsabileAttivita,
                                     'nome' => $job['riga']['nome_attivita_seguente'],
                                     'id_cliente' => $idAnagrafica,
-                                    'id_progetto' => ( ( isset( $idProgetto ) ) ? $idProgetto : NULL )
+                                    'id_progetto' => ( ( isset( $idProgetto ) ) ? $idProgetto : NULL ),
+                                    'id_todo' => ( ( isset( $idTodo ) ) ? $idTodo : NULL )
                                 ),
                                 'attivita',
                                 true,
                                 false,
                                 array(
                                     'data_programmazione',
+                                    'id_anagrafica_programmazione',
                                     'nome',
-                                    'id_progetto'
+                                    'id_progetto',
+                                    'id_todo'
                                 )
                             );
 
@@ -299,6 +470,11 @@
                         $job['status']['error'][] = 'data pianificazione attività seguente non settata per la riga ' . $job['corrente'];
 
                     }
+
+                } else {
+
+                    // status
+                    $job['status']['error'][] = 'anagrafica non trovata per la riga ' . $job['corrente'];
 
                 }
 
