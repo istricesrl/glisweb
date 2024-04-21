15f65ba2484f31c4e10becff339c2c87f9c880d5
Team Produzione Istrice <produzione@istricesrl.it>
2024-04-21 22:11:54 +0200

    lavoro al modulo pagamenti

--- _mod/_F030.pagamenti/_src/_inc/_pages/_pagamenti.it-IT.php
+++ _mod/_F030.pagamenti/_src/_inc/_pages/_pagamenti.it-IT.php
@@ -6,7 +6,18 @@
     // modulo di questo file
     $m = '_mod/_F030.pagamenti/';
 
-    // pagina principale
+    // pagina di scelta
+	$p['pagamenti.scelta'] = array(
+	    'sitemap'		=> false,
+	    'cacheable'		=> false,
+	    'title'		=> array( $l		=> 'scelta pagamento' ),
+	    'h1'		=> array( $l		=> 'scelta pagamento' ),
+	    'template'		=> array( 'path'	=> '_src/_templates/_aurora/', 'schema' => 'pagamento.scelta.html' ),
+	    'parent'		=> array( 'id'		=> NULL ),
+	    'macro'		=> array( $m . '_src/_inc/_macro/_pagamenti.scelta.php' )
+	);
+
+    // pagina di riepilogo
 	$p['pagamenti.riepilogo'] = array(
 	    'sitemap'		=> false,
 	    'cacheable'		=> false,
@@ -17,18 +28,7 @@
 	    'macro'		=> array( $m . '_src/_inc/_macro/_pagamenti.riepilogo.php' )
 	);
 
-    // pagina principale
-	$p['pagamenti.checkout'] = array(
-	    'sitemap'		=> false,
-	    'cacheable'		=> false,
-	    'title'		=> array( $l		=> 'checkout pagamento' ),
-	    'h1'		=> array( $l		=> 'checkout pagamento' ),
-	    'template'		=> array( 'path'	=> '_src/_templates/_aurora/', 'schema' => 'pagamento.checkout.html' ),
-	    'parent'		=> array( 'id'		=> NULL ),
-	    'macro'		=> array( $m . '_src/_inc/_macro/_pagamenti.checkout.php' )
-	);
-
-    // pagina principale
+    // pagina di esito
 	$p['pagamenti.esito'] = array(
 	    'sitemap'		=> false,
 	    'cacheable'		=> false,
