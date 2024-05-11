a1d388a24673c20eafb2d9d2835f277b013aaf48
Team Produzione Istrice <produzione@istricesrl.it>
2024-05-11 16:27:45 +0200

    debug e refactoring

---
+++ _src/_config/_095.translation.php
@@ -0,0 +1,32 @@
+<?php
+
+    /**
+     * tabella di traduzione
+     *
+     *
+     *
+     *
+     *
+     *
+     * TODO usare glob per trovare i dizionari (non limitarsi a 'generic')
+     * TODO ogni modulo dovrebbe avere i suoi dizionari
+     * TODO finire di documentare
+     * TODO completare l'inserimento dei dizionari
+     *
+     *
+     *
+     */
+
+    // configurazione extra
+    if( isset( $cx['tr'] ) ) {
+        $cf['tr'] = array_replace_recursive( $cf['tr'], $cx['tr'] );
+    }
+
+    // rendo le tabelle di traduzione disponibili al template
+    $ct['tr'] = &$cf['tr'];
+
+    // debug
+    // print_r( $arrayDizionari );
+    // echo DIR_ETC_DICTIONARIES . '_*.{' . LINGUE_ATTIVE . '}.php' . PHP_EOL;
+    // echo DIR_MOD_ATTIVI_ETC_LOC . '_*.{' . LINGUE_ATTIVE . '}.php' . PHP_EOL;
+    // echo 'OUTPUT';
