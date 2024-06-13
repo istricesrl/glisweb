ecbdcf8acd3ceee99472b098ed6b30623b87747c
Team Produzione Istrice <produzione@istricesrl.it>
2024-06-13 15:54:45 +0200

    debug refactoring e documentazione phing

--- _src/_sh/_phing.deploy.sh
+++ _src/_sh/_phing.deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+## SCRIPT DI DEPLOY
+#
+# Questo script utilizza Phing (http://www.phing.info/) per effettuare il deploy del progetto corrente sulla destinazione
+# desiderata. Per configurare le destinazioni (è possibile crearne un numero arbitrario) è sufficiente creare un file per ognuna
+# di esse nella cartella ./usr/deploy/phing/ con estensione .properties. Il nome del file senza estensione sarà il nome con
+# cui questo script identificherà ogni specifica destinazione.
+#
+# Per creare il file di configurazione della destinazione si può partire dal file di esempio _usr/_examples/_phing/build.properties
+# e modificarlo in base alle proprie necessità seguendo le indicazioni dei commenti.
+#
+
 ## pulizia schermo
 clear
 
