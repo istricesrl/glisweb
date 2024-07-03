c41e000de42ff89bd4603bf5a042f4efbf9de3b8
Team Produzione Istrice <produzione@istricesrl.it>
2024-07-03 10:12:24 +0200

    miglioie modulo modula

--- _mod/_0500.mastri/_src/_inc/_macro/_magazzini.form.tools.php
+++ _mod/_0500.mastri/_src/_inc/_macro/_magazzini.form.tools.php
@@ -25,35 +25,19 @@
 		)
 	);
 
-    // duplica pagina
-	$ct['page']['contents']['metro']['azioni'][] = array(
-        'modal' => array('id' => 'duplica', 'include' => 'inc/articoli.form.tools.modal.duplica.html' ),
-	    'icon' => NULL,
-	    'fa' => 'fa-files-o',
-	    'title' => 'duplica articolo',
-	    'text' => 'duplica l\'articolo corrente'
-	);
-
     // aggiunta al carrello
-	if( in_array( "4170.ecommerce", $cf['mods']['active']['array'] ) ) {
+	if( in_array( "E300.modula", $cf['mods']['active']['array'] ) ) {
 
 		$ct['page']['contents']['metro']['ecommerce'][] = array(
-			'ws' => $ct['site']['url'] . 'task/4170.ecommerce/aggiungi.al.carrello?__carrello__[__articolo__][id_articolo]='.$_REQUEST[ $ct['form']['table'] ]['id'],
+			'ws' => 'http://localhost/modula?comando=' . $_REQUEST[ $ct['form']['table'] ]['prefisso_modula'] . '|' . time() . '|CALL|' . $_REQUEST[ $ct['form']['table'] ]['codice_modula'] . '|1',
 			'callback' => 'aggiornaCarrello',
 			'icon' => NULL,
-			'fa' => 'fa-cart-plus',
-			'title' => 'aggiungi al carrello',
-			'text' => 'aggiunge questo articolo al carrello corrente'
+			'fa' => 'fa-level-down',
+			'title' => 'richiama cassetto',
+			'text' => 'richiama il cassetto del Modula'
 	    );
 
 	}
-
-    // tendina prodotti
-	$ct['etc']['select']['prodotti'] = mysqlCachedIndexedQuery(
-	    $cf['memcache']['index'],
-	    $cf['memcache']['connection'],
-        $cf['mysql']['connection'], 
-        'SELECT id, __label__ FROM prodotti_view' );
 
 	// macro di default
     require DIR_SRC_INC_MACRO . '_default.tools.php';
