e86000c4f616d22f13c4a4fc070e9ef6f57cf49c
Team Produzione Istrice <produzione@istricesrl.it>
2024-07-24 18:37:31 +0200

    implementato task di aggiornamento della geografia

---
+++ _src/_api/_task/_geografia.importazione.php
@@ -0,0 +1,27 @@
+<?php
+
+    // inclusione del framework
+	if( ! defined( 'CRON_RUNNING' ) ) {
+	    require '../../_config.php';
+	}
+
+    // inizializzo l'array del risultato
+	$status = array();
+
+    // file da copiare
+    $status['files'] = array(
+        'https://dataserver.istricesrl.com/geografia/01.update.stati.csv',
+        'https://dataserver.istricesrl.com/geografia/02.update.regioni.csv',
+        'https://dataserver.istricesrl.com/geografia/03.update.province.csv',
+        'https://dataserver.istricesrl.com/geografia/04.update.comuni.csv'
+    );
+
+    // scarico l'XLS
+    foreach( $status['files'] as $file ) {
+        copyFile( $file, DIR_VAR_SPOOL_IMPORT . '/' . basename( $file ) );
+    }
+
+    // output
+	if( ! defined( 'CRON_RUNNING' ) ) {
+	    buildJson( $status );
+	}
