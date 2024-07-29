01759d27dd0e54ce708c2119e5700d6be1054dbb
Team Produzione Istrice <produzione@istricesrl.it>
2024-07-29 17:43:53 +0200

    miglioria script dkim

--- _src/_sh/_dkim.keygen.sh
+++ _src/_sh/_dkim.keygen.sh
@@ -19,21 +19,27 @@ if [ -n "$1" ]; then
 ## se la cartella non esiste
 if [ ! -d ./etc/secret/$1/ ]; then
 
-## creo la cartella
-mkdir -p ./etc/secret/$1/
+    ## creo la cartella
+    mkdir -p ./etc/secret/$1/
 
-## genero la chiave privata
-# openssl genrsa -aes256 -passout pass:$1 -out ./etc/secret/dkim.private 2048
-openssl genrsa -out ./etc/secret/$1/dkim.private.pem 2048
+    ## genero la chiave privata
+    # openssl genrsa -aes256 -passout pass:$1 -out ./etc/secret/dkim.private 2048
+    openssl genrsa -out ./etc/secret/$1/dkim.private.pem 2048
 
-## esporto la chiave pubblica
-openssl rsa -in ./etc/secret/$1/dkim.private.pem -pubout -outform der 2>/dev/null | openssl base64 -A > ./etc/secret/$1/dkim.public.pem
+    ## esporto la chiave pubblica
+    openssl rsa -in ./etc/secret/$1/dkim.private.pem -pubout -outform der 2>/dev/null | openssl base64 -A > ./etc/secret/$1/dkim.public.pem
 
 else
 
-echo "il certificato per $1 esiste già"
+    echo "il certificato per $1 esiste già"
 
 fi
+
+## output
+clear
+
+## informazioni
+echo "valore del record TXT glisweb._domainkey.$1. -> v=DKIM1;p=$(cat ./etc/secret/$1/dkim.public.pem)"
 
 else
 
