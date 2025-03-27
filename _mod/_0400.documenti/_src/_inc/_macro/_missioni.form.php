ebcdd9d975fbb51411d0b079fc452ea4fc68bce7
Team Produzione Istrice <produzione@istricesrl.it>
2025-03-27 14:59:15 +0100

    impostazione gestione missioni

---
+++ _mod/_0400.documenti/_src/_inc/_macro/_missioni.form.php
@@ -0,0 +1,99 @@
+<?php
+
+    /**
+     * macro form anagrafica
+     *
+     *
+     *
+     * -# definizione della tabella del modulo
+     * -# popolazione delle tendine
+     *
+     *
+     *
+     *
+     *
+     *
+     * @todo documentare
+     *
+     * @file
+     *
+     */
+
+    // tabella gestita
+	$ct['form']['table'] = 'documenti';
+
+    // tendina tipologie anagrafica
+	$ct['etc']['select']['tipologie_documenti'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM tipologie_documenti_view WHERE se_missione IS NOT NULL'
+	);
+
+    // tendina mittenti
+	$ct['etc']['select']['id_emittenti'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM anagrafica_view_static'
+	);
+
+	// tendina condizioni_pagamento
+	$ct['etc']['select']['condizioni_pagamento'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM condizioni_pagamento_view'
+	);
+
+	// tendina coupon
+	$ct['etc']['select']['coupon'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM coupon_view'
+	);
+
+    // tendina destinatari
+	$ct['etc']['select']['id_destinatari'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM anagrafica_view_static'
+	);
+
+	// esigibilità iva
+	$ct['etc']['select']['esigibilita'] = array(
+		array( 'id' => 'I', '__label__'=> 'I - immediata' ),
+		array( 'id' =>'D', '__label__'=> 'D - differita' ),
+		array( 'id' =>'S', '__label__'=> 'S - scissione dei pagamenti')
+	); 
+
+    // tendina indirizzi mittenti
+    if( isset( $_REQUEST[ $ct['form']['table'] ]['id_emittente'] ) && !empty( $_REQUEST[ $ct['form']['table'] ]['id_emittente'] ) ){
+	    $ct['etc']['select']['id_sedi_emittente'] = mysqlCachedIndexedQuery(
+	        $cf['memcache']['index'],
+	        $cf['memcache']['connection'],
+	        $cf['mysql']['connection'],
+	        'SELECT indirizzi_view.id, __label__ FROM indirizzi_view LEFT JOIN anagrafica_indirizzi ON anagrafica_indirizzi.id_indirizzo = indirizzi_view.id WHERE anagrafica_indirizzi.id_anagrafica = ?',
+            array( array( 's' => $_REQUEST[ $ct['form']['table'] ]['id_emittente'] ) )
+	    );
+    } 
+
+	// tendina indirizzi destinatari
+    if( isset( $_REQUEST[ $ct['form']['table'] ]['id_destinatario'] ) && !empty( $_REQUEST[ $ct['form']['table'] ]['id_destinatario'] ) ){
+	    $ct['etc']['select']['id_sedi_destinatario'] = mysqlCachedIndexedQuery(
+	        $cf['memcache']['index'],
+	        $cf['memcache']['connection'],
+	        $cf['mysql']['connection'],
+	        'SELECT indirizzi_view.id, __label__ FROM indirizzi_view LEFT JOIN anagrafica_indirizzi ON anagrafica_indirizzi.id_indirizzo = indirizzi_view.id  WHERE anagrafica_indirizzi.id_anagrafica = ?',
+            array( array( 's' => $_REQUEST[ $ct['form']['table'] ]['id_destinatario'] ) )
+	    );
+	} 
+
+#	if( !isset( $_REQUEST['__preset__'][ $ct['form']['table'] ]['id_emittente'] ) && !isset( $_REQUEST['__latest__'][ $ct['form']['table'] ]['id_emittente'] ) && !empty( $ct['etc']['select']['id_emittenti'] ) ){
+#		$_REQUEST['__preset__'][ $ct['form']['table'] ]['id_emittente'] = $ct['etc']['select']['id_emittenti'][0]['id'];
+#	}
+
+	// macro di default
+	require DIR_SRC_INC_MACRO . '_default.form.php';
---
+++ _mod/_0400.documenti/_src/_inc/_macro/_missioni.view.php
@@ -0,0 +1,80 @@
+<?php
+
+    /**
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     * @todo finire di documentare
+     *
+     * @file
+     *
+     */
+
+    // tabella della vista
+	$ct['view']['table'] = 'missioni';
+
+    // pagina per la gestione degli oggetti esistenti
+	$ct['view']['open']['page'] = 'missioni.form';
+
+    // tabella per la gestione degli oggetti esistenti
+	$ct['view']['open']['table'] = 'documenti';
+
+    // campi della vista
+    $ct['view']['cols'] = array(
+        'id' => '#',
+        'numero' => 'num.',
+        'sezionale' => 'sez.',
+        'data' => 'data',
+#        'emittente' => 'emittente',
+#        'destinatario' => 'destinatario',
+        'nome' => 'nome',
+#        '__label__' => 'nome'
+    );
+
+    // stili della vista
+    $ct['view']['class'] = array(
+        'nome' => 'text-left',
+#        'numero' => 'text-left',
+        'data' => 'no-wrap', 
+        '__label__' => 'text-left',
+        'nome' => 'text-left',
+        'destinatario' => 'text-left',
+        'emittente' => 'text-left',
+        'tipologia' => 'text-left',
+        'totale' => 'text-right' 
+    );
+
+    // tendina mittenti
+	$ct['etc']['select']['id_emittenti'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id   , __label__ FROM anagrafica_view_static WHERE se_gestita = 1 ORDER BY __label__'
+	);
+
+    // tendina destinatari
+	$ct['etc']['select']['id_destinatari'] = mysqlCachedIndexedQuery(
+	    $cf['memcache']['index'],
+	    $cf['memcache']['connection'],
+	    $cf['mysql']['connection'],
+	    'SELECT id, __label__ FROM anagrafica_view_static ORDER BY __label__'
+	);
+
+    // inclusione filtri speciali
+//	$ct['etc']['include']['filters'] = 'inc/ddt.magazzini.view.filters.html';
+
+    // macro di default
+	require DIR_SRC_INC_MACRO . '_default.view.php';
+
+    foreach( $ct['view']['data'] as $key => &$row ) {
+
+        $row['timestamp_chiusura'] = date( 'Y-m-d H:i', $row['timestamp_chiusura']);
+
+	}
