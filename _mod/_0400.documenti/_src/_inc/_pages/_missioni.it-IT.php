54343294d27cd08b11e4ba10a7f8e9db7d1c5714
Team Produzione Istrice <produzione@istricesrl.it>
2025-04-01 15:04:00 +0200

    lavoro al modulo missioni

--- _mod/_0400.documenti/_src/_inc/_pages/_missioni.it-IT.php
+++ _mod/_0400.documenti/_src/_inc/_pages/_missioni.it-IT.php
@@ -6,23 +6,21 @@
     // modulo di questo file
 	$m = DIR_MOD . '_0400.documenti/';
 
-    // debug
-    // die( print_r( $p, true ) );
-
     // RELAZIONI CON IL MODULO LOGISTICA
 	if( in_array( "5200.missioni", $cf['mods']['active']['array'] ) ) {
 
 		// dashboard logistica doc. attivi
 		$p['missioni.view'] = array(
 			'sitemap'		=> false,
-			'title'		    => array( $l		=> 'missioni' ),
+			'title'		=> array( $l		=> 'missioni' ),
 			'h1'		=> array( $l		=> 'missioni' ),
 			'template'	=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'default.view.html' ),
 			'macro'		=> array( $m . '_src/_inc/_macro/_missioni.view.php' ),
 			'parent'	=> array( 'id'		=> 'logistica' ),
 			'auth'		=> array( 'groups'	=> array(	'roots', 'staff' ) ),
 			'etc'		=> array( 'tabs'	=> array( 'missioni.view', 'missioni.tools' ) ),
-			'menu'		=> array( 'admin'	=> array(	'' => 	array(	'label'		=> array( $l => 'missioni' ), 'priority'	=> '120' ) ) )	
+			'menu'			=> array( 'admin'	=> array(	'' => 	array(	'label'		=> array( $l => 'missioni' ),
+			    'priority'	=> '120' ) ) )	
 		);
 
         // tools missioni
@@ -68,7 +66,17 @@
             'etc'			=> array( 'tabs'	=> $p['missioni.form']['etc']['tabs'] )
         );
 
-    }
+        // tools missioni
+        $p['missioni.form.tools'] = array(
+            'sitemap'			=> false,
+            'icon'				=> '<i class="fa fa-cogs" aria-hidden="true"></i>',
+            'title'				=> array( $l		=> 'azioni missione' ),
+            'h1'				=> array( $l		=> 'azioni missione' ),
+            'parent'			=> array( 'id'		=> 'missioni.view' ),
+            'template'			=> array( 'path'	=> '_src/_templates/_athena/', 'schema' => 'default.tools.html' ),
+            'macro'				=> array( $m . '_src/_inc/_macro/_missioni.form.tools.php' ),
+            'auth'				=> array( 'groups'	=> array(	'roots', 'staff' ) ),
+            'etc'				=> array( 'tabs'	=> $p['missioni.form']['etc']['tabs'] )
+        );
 
-    // debug
-    // die( print_r( $p, true ) );
+    }
