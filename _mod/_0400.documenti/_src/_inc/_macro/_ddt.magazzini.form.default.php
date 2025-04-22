c1d3ee034a35ff1de991fad23e142464a8d6a04b
Team Produzione Istrice <produzione@istricesrl.it>
2025-04-22 09:37:28 +0200

    lavoro alla packing list

--- _mod/_0400.documenti/_src/_inc/_macro/_ddt.magazzini.form.default.php
+++ _mod/_0400.documenti/_src/_inc/_macro/_ddt.magazzini.form.default.php
@@ -42,7 +42,7 @@
 			) );
 	
 			// debug
-			 echo $_REQUEST['__evasione__']['quantita'].' vs. '.$cap.PHP_EOL;
+			// echo $_REQUEST['__evasione__']['quantita'].' vs. '.$cap.PHP_EOL;
 
 			// cap della quantità
 			if( $_REQUEST['__evasione__']['quantita'] <= $cap ) {
@@ -55,6 +55,7 @@
 						'id_documento' => $_REQUEST[ $ct['form']['table'] ]['id'],
 						'id_articolo' => $evasione[1],
 						'id_matricola' => isset( $evasione[2] ) ? $evasione[2] : NULL,
+						'id_collo' => ( isset( $_REQUEST['__evasione__']['id_collo'] ) ? $_REQUEST['__evasione__']['id_collo'] : NULL ),
 						'id_udm' => 1,
 						'quantita' => $_REQUEST['__evasione__']['quantita'],
 						'id_mastro_provenienza' => $evasione[0],
@@ -71,5 +72,19 @@
 			}
 
 		}
+
+		$ct['etc']['packing'] = mysqlQuery(
+			$cf['mysql']['connection'],
+			'SELECT documenti_articoli.id, documenti_articoli.id_documento, documenti_articoli.id_articolo, documenti_articoli.id_matricola, documenti_articoli.id_collo, documenti_articoli.id_udm, sum( documenti_articoli.quantita ) as quantita,
+			colli.codice AS collo, articoli.id_prodotto, prodotti.nome AS prodotto, articoli.nome AS articolo, udm.sigla AS udm
+			FROM documenti_articoli 
+			INNER JOIN articoli ON articoli.id = documenti_articoli.id_articolo
+			INNER JOIN prodotti ON prodotti.id = articoli.id_prodotto
+			INNER JOIN udm ON udm.id = documenti_articoli.id_udm
+			INNER JOIN colli ON colli.id = documenti_articoli.id_collo
+			WHERE documenti_articoli.id_documento = ? 
+			GROUP BY documenti_articoli.id_articolo, documenti_articoli.id_matricola, documenti_articoli.id_collo, documenti_articoli.id_udm',
+			array( array( 's' => $_REQUEST[ $ct['form']['table'] ]['id'] ) )
+		);
 
 	}
