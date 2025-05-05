b133f8e0173434757de6e58c371eeb68ff42f5d2
Team Produzione Istrice <produzione@istricesrl.it>
2025-05-05 15:26:27 +0200

    documentazione

--- _src/_config/_205.auth.php
+++ _src/_config/_205.auth.php
@@ -9,6 +9,13 @@
      *
      */
 
+    /**
+     * integrazione della configurazione da file Json/Yaml
+     * ===================================================
+     * 
+     * 
+     */
+
     // configurazione extra
     if( isset( $cx['auth'] ) ) {
         $cf['auth'] = array_replace_recursive( $cf['auth'], $cx['auth'] );
