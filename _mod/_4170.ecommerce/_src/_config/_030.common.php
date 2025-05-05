ad81ec573ee941375f76db063ea188ab36c9cfd2
Team Produzione Istrice <produzione@istricesrl.it>
2025-05-05 13:46:29 +0200

    riallineamenti

--- _mod/_4170.ecommerce/_src/_config/_030.common.php
+++ _mod/_4170.ecommerce/_src/_config/_030.common.php
@@ -244,6 +244,7 @@
         'fatturazione_strategia'                => array( 'type' => 'string',   'default' => 'SINGOLA', 'values' => array( 'SINGOLA', 'MULTIPLA', NULL ) ),
         // 'spam_check'                            => array( 'type' => 'float',    'default' => NULL ),
         // 'spam_score'                            => array( 'tipe' => 'int',      'default' => NULL ),
+        'note_fatturazione'                         => array( 'type' => 'string',   'default' => NULL ),
         'codice_coupon'                         => array( 'type' => 'string',   'default' => NULL ),
         'timestamp_checkout'                    => array( 'tipe' => 'int',      'default' => NULL ),
     );
