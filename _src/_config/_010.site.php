059f45cf77cc3f0110181942bd23fac9a4ec39ec
produzione Istrice <produzione@istricesrl.it>
2025-07-15 13:22:04 +0200

    debug

--- _src/_config/_010.site.php
+++ _src/_config/_010.site.php
@@ -79,6 +79,11 @@
      *
      */
 
+    // debug
+    ini_set( 'display_errors', 1 );
+    ini_set( 'display_startup_errors', 1 );
+    error_reporting( E_ALL );
+
     /**
      * configurazione del sito di default
      * ==================================
