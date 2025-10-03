74cfe0f48526512d8dac6856728796df70ea9952
produzione Istrice <produzione@istricesrl.it>
2025-10-03 16:33:51 +0200

    riallineamento da fia

--- _mod/_0500.mastri/_src/_inc/_macro/_magazzini.view.php
+++ _mod/_0500.mastri/_src/_inc/_macro/_magazzini.view.php
@@ -17,6 +17,10 @@
      *
      */
 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     // tabella della vista
     $ct['view']['table'] = 'magazzini';
 
