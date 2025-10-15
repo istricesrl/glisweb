3771796b28ffc846964c2097620c3d55d9d6b83b
produzione Istrice <produzione@istricesrl.it>
2025-10-15 10:35:20 +0200

    riallineamento da GIMBE

--- _mod/_4170.ecommerce/_src/_inc/_macro/_carrelli.form.php
+++ _mod/_4170.ecommerce/_src/_inc/_macro/_carrelli.form.php
@@ -3,5 +3,9 @@
     // tabella gestita
 	$ct['form']['table'] = 'carrelli';
 
+	$ct['etc']['select']['articoli'] = mysqlQuery( $cf['mysql']['connection'], 'SELECT id, __label__ FROM articoli_view' );
+
+	$ct['etc']['timestamp_inserimento'] = mysqlSelectValue( $cf['mysql']['connection'], 'SELECT timestamp_inserimento FROM carrelli WHERE id = ?', array( array( 's' => $_REQUEST['carrelli']['id'] ) ) );
+
     // macro di default
 	require DIR_SRC_INC_MACRO . '_default.form.php';
