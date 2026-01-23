cfceca5d435d4186900a26350a4ba4b8f95133e8
produzione Istrice <produzione@istricesrl.it>
2026-01-23 16:37:58 +0100

    debug

--- _mod/_0920.corsi/_src/_api/_task/_report.lezioni.corsi.popolazione.php
+++ _mod/_0920.corsi/_src/_api/_task/_report.lezioni.corsi.popolazione.php
@@ -16,12 +16,15 @@
 
 	} else {
 
+		// debug
+		// die( 'Debug: in ricerca di righe da aggiornare' );
+/*
 		// trovo una riga da aggiornare
 		$status['aggiornare'] = mysqlSelectRow(
 			$cf['mysql']['connection'],
 			'SELECT todo.id FROM todo
 			INNER JOIN tipologie_todo ON tipologie_todo.id = todo.id_tipologia AND tipologie_todo.id_genitore = 6
-			INNER JOIN progetti ON progetti.id = todo.id_progetto
+			-- INNER JOIN progetti ON progetti.id = todo.id_progetto
             LEFT JOIN __report_lezioni_corsi__ ON __report_lezioni_corsi__.id = todo.id
             WHERE (
                 (
@@ -38,7 +41,15 @@
 			ORDER BY todo.id DESC
 			LIMIT 1'
 		);
+*/
+		$status['aggiornare'] = mysqlSelectRow(
+			$cf['mysql']['connection'],
+			'SELECT todo.id FROM todo ORDER BY todo.timestamp_aggiornamento_report_corsi ASC LIMIT 1'
+		);
 
+		// debug
+		// die( 'Aggiornare: ' . print_r( $status['aggiornare'], true ) );
+/*
 		// ...
 		if( empty( $status['aggiornare'] ) ) {
 
@@ -68,7 +79,6 @@
 				);
 
 			}
-
 			// ...
 			// print_r( $aggiornare );
 
@@ -76,13 +86,21 @@
 			$status['aggiornare']['id'] = max( $aggiornare );
 
 		}
+*/
 
 	}
+
+	// debug
+	// die( 'Aggiornare: ' . print_r( $status['aggiornare'], true ) );
 
 	// ...
 	if( ! empty( $status['aggiornare']['id'] ) ) {
 		updateReportLezioniCorsi(
 			$status['aggiornare']['id']
+		);
+		mysqlQuery(
+			$cf['mysql']['connection'],
+			'UPDATE todo SET timestamp_aggiornamento_report_corsi = unix_timestamp(now()) WHERE id = ' . intval( $status['aggiornare']['id'] )
 		);
 	}
 
