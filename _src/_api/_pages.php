20edee976b168b656726feb19a74d75ae8eae648
produzione Istrice <produzione@istricesrl.it>
2026-05-18 16:54:20 +0200

    debug

--- _src/_api/_pages.php
+++ _src/_api/_pages.php
@@ -854,27 +854,11 @@
     }
 
     // caching locale dei JS esterni
-    // Alcune librerie (es. CKEditor 4) all'avvio autocalcolano la propria basePath
-    // dall'URL del proprio <script src> e da lì scaricano asset accessori (config.js,
-    // lang/<lingua>.js, skins/<skin>/editor.css, plugins/...). Cachare il solo file
-    // principale rompe l'autoload: i 404 sugli asset accessori impediscono
-    // l'inizializzazione. Skip-list per i pacchetti multi-file noti: restano in
-    // page.js.external e vengono caricati direttamente dalla CDN.
-    $jsCacheSkipPrefixes = array(
-        'cdn.ckeditor.com/',
-    );
     if( isset( $ct['page']['js']['external'] ) && is_array( $ct['page']['js']['external'] ) ) {
         foreach( $ct['page']['js']['external'] as $idx => $js ) {
             // le URL contenenti espressioni Twig sono dinamiche e vengono renderizzate da _page.close.twig: non cachabili
             if( strpos( $js, '{{' ) !== false || strpos( $js, '{%' ) !== false || strpos( $js, '{#' ) !== false ) {
                 continue;
-            }
-            // skip pacchetti multi-file che si rompono se serviti da basePath diversa dalla CDN
-            $jsHost = str_replace( array( 'http://', 'https://' ), '', $js );
-            foreach( $jsCacheSkipPrefixes as $prefix ) {
-                if( strpos( $jsHost, $prefix ) === 0 ) {
-                    continue 2;
-                }
             }
             $cachefile = DIR_VAR_CACHE . 'js/' . str_replace( array( 'http://', 'https://' ), '', $js );
             if( ! file_exists( $cachefile ) ) {
